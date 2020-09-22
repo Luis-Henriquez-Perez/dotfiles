9ad22b3ad19873a71718e09843781c1a9d2d73ae
Luis Henriquez <luis@luishp.xyz>
2020-09-21 21:58:28 -0400

    Configure nyxt to use vim bindings.

--- .config/nyxt/init.lisp
+++ .config/nyxt/init.lisp
@@ -1,16 +1,26 @@
-(in-package nyxt)
+;; (in-package nyxt)
 
-(define-command org-capture (&optional (buffer (current-buffer)))
-  "Org-capture current page."
-  (eval-in-emacs
-   `(org-link-set-parameters
-     "next"
-     :store (lambda ()
-              (org-store-link-props
-               :type "next"
-               :link ,(url buffer)
-               :description ,(title buffer))))
-   `(org-capture)))
+;; (define-command org-capture (&optional (buffer (current-buffer)))
+;;   "Org-capture current page."
+;;   (eval-in-emacs
+;;    `(org-link-set-parameters
+;;      "next"
+;;      :store (lambda ()
+;;               (org-store-link-props
+;;                :type "next"
+;;                :link ,(url buffer)
+;;                :description ,(title buffer))))
+;;    `(org-capture)))
+
+; $HOME/.config/next/init.lisp
+;; set vim keys
+(in-package :next-user)
+
+(defclass my-buffer (buffer)
+  ((default-modes :initform
+     (cons 'vi-normal-mode (get-default 'buffer 'default-modes)))))
+
+(setf *buffer-class* 'my-buffer)
 
 ;; (define-key *my-keymap* "C-M-o" 'org-capture)
 
