254221196c82233751f7a4948caae1e3a5a126be
Luis Henriquez <luis@luishp.xyz>
2020-09-30 00:41:25 -0400

    Update init.lisp.

--- .config/nyxt/init.lisp
+++ .config/nyxt/init.lisp
@@ -1,19 +1,15 @@
-;; (in-package nyxt)
+(defun eval-in-emacs (&rest s-exps)
+  "Evaluate S-exps with `emacsclient'."
+  (let ((s-exps-string (cl-ppcre:regex-replace-all
+                        ;; Discard the package prefix.
+                        "next-user::?"
+                        (write-to-string
+                         `(progn ,@s-exps) :case :downcase)
+                        "")))
+    (log:debug "Sending to Emacs: ~s" s-exps-string)
+    (ignore-errors (uiop:run-program
+                    (list "emacsclient" "--eval" s-exps-string)))))
 
-;; (define-command org-capture (&optional (buffer (current-buffer)))
-;;   "Org-capture current page."
-;;   (eval-in-emacs
-;;    `(org-link-set-parameters
-;;      "next"
-;;      :store (lambda ()
-;;               (org-store-link-props
-;;                :type "next"
-;;                :link ,(url buffer)
-;;                :description ,(title buffer))))
-;;    `(org-capture)))
-
-; $HOME/.config/next/init.lisp
-;; set vim keys
 (in-package :next-user)
 
 (defclass my-buffer (buffer)
@@ -21,9 +17,3 @@
      (cons 'vi-normal-mode (get-default 'buffer 'default-modes)))))
 
 (setf *buffer-class* 'my-buffer)
-
-;; (define-key *my-keymap* "C-M-o" 'org-capture)
-
-;;
-
-;; (define-configuration buffer ((default-modes (append '(vi-normal-mode) %slot-default))))
