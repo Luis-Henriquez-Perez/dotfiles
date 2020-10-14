5216748af3ae7742deeffc21c403c5b96a798e08
Luis Henriquez <luis@luishp.xyz>
2020-10-13 23:22:03 -0400

    Update init.lisp.

--- .config/nyxt/init.lisp
+++ .config/nyxt/init.lisp
@@ -1,3 +1,11 @@
+(in-package :next-user)
+
+(defclass my-buffer (buffer)
+  ((default-modes :initform
+     (cons 'vi-normal-mode (get-default 'buffer 'default-modes)))))
+
+(setf *buffer-class* 'my-buffer)
+
 (defun eval-in-emacs (&rest s-exps)
   "Evaluate S-exps with `emacsclient'."
   (let ((s-exps-string (cl-ppcre:regex-replace-all
@@ -10,8 +18,8 @@
     (ignore-errors (uiop:run-program
                     (list "emacsclient" "--eval" s-exps-string)))))
 
-(defvar *my-keymap* (make-keymap "my-map")
-  "Keymap for `my-mode'.")
+;; (defvar *my-keymap* (make-keymap "my-map")
+;;   "Keymap for `my-mode'.")
 
 (define-command org-capture (&optional (buffer (current-buffer)))
   "Org-capture current page."
@@ -25,12 +33,4 @@
                :description ,(title buffer))))
    `(org-capture)))
 
-(define-key *my-keymap* "C-o" 'org-capture)
-
-(in-package :next-user)
-
-(defclass my-buffer (buffer)
-  ((default-modes :initform
-     (cons 'vi-normal-mode (get-default 'buffer 'default-modes)))))
-
-(setf *buffer-class* 'my-buffer)
+;; (define-key *my-keymap* "C-o" 'org-capture)
