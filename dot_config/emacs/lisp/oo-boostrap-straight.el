5939e9d909f0563f898ccbc721ed7de117105d56
Luis Henriquez-Perez <luis@luishp.xyz>
2023-09-02 10:14:20 -0400

    Sat Sep  2 10:14:20 2023
    M	dot_config/emacs/init.el
    A	dot_config/emacs/lisp/base/oo-boostrap-elpaca.el
    D	dot_config/emacs/lisp/base/oo-packages.el
    A	dot_config/emacs/lisp/oo-boostrap-straight.el
    A	dot_config/emacs/lisp/uncategoriezed.el

---
+++ dot_config/emacs/lisp/oo-boostrap-straight.el
@@ -0,0 +1,8 @@
+(oo-bootstrap-straight)
+
+;; Register all the recipes.
+;; Clone all the packages in recipes.
+;; Set all the branches to the proper branch and commit
+;; Build the packages.
+
+(provide 'oo-bootstrap-straight)
