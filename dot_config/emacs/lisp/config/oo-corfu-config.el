bb19f4b3c2c842db56ded7d96c2a7d4d3c2b58c1
Luis Henriquez-Perez <luis@luishp.xyz>
2023-09-06 21:06:22 -0400

    Wed Sep  6 21:06:22 2023
    M	dot_config/emacs/init.el
    M	dot_config/emacs/lisp/base/oo-bind-macro.el
    A	dot_config/emacs/lisp/config/oo-abbrev-config.el
    M	dot_config/emacs/lisp/config/oo-corfu-config.el

--- dot_config/emacs/lisp/config/oo-corfu-config.el
+++ dot_config/emacs/lisp/config/oo-corfu-config.el
@@ -6,4 +6,19 @@
 (set! corfu-auto t)
 (set! corfu-auto-delay 0.1)
 
+(set! corfu-quick1 "abcdefghijklmnopqrstuvxyz")
+
+(oo-add-hook 'corfu-mode #'corfu-history-mode)
+
+(bind! (:map corfu-map)
+       ("<tab>" #'corfu-next)
+       ([backtab] #'corfu-previous)
+       ("C-;" #'corfu-quick-complete)
+       ("C-j" #'corfu-next)
+       ;; Works
+       ("M-k" #'corfu-previous)
+       ;; For some reason I can't bind these.
+       (:ieg "C-k" #'corfu-previous)
+       (:ieg "C-p" #'corfu-previous))
+
 (provide 'oo-corfu-config)
