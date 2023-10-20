1b923a25b83a66010bd5bbe8f3f340db1940e9eb
Luis Henriquez-Perez <luis@luishp.xyz>
2023-10-20 15:34:23 -0400

    Fri Oct 20 15:34:23 2023
    M	dot_config/emacs/init.el

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -1,4 +1,5 @@
 ;; [[file:snapshots/emacs-logo.png]]
+;; Karl Volt argues that =org= has many advantages over =markdown=
 ;; https://karl-voit.at/2017/09/23/orgmode-as-markup-only/
 ;; * init.el
 ;; * boost =gc-cons-threshold=
@@ -83,7 +84,7 @@ HOOK-OR-ADVICE.")
 ;; (require 'oo-bind-functions)
 
 ;; * hooks
-;; `prog-mode-hook'
+;; ** prog-mode-hook
 ;; (oo-add-hook 'prog-mode-hook #'smartparens-strict-mode)
 ;; (oo-add-hook 'prog-mode-hook #'corfu-mode)
 ;; (oo-add-hook 'prog-mode-hook #'lispyville-mode)
@@ -92,20 +93,20 @@ HOOK-OR-ADVICE.")
 ;; (oo-add-hook 'prog-mode-hook #'hs-minor-mode)
 ;; (oo-add-hook 'prog-mode-hook #'auto-fill-mode)
 
-;; `emacs-startup-hook'
+;; ** emacs-startup-hook
 ;; (oo-add-hook 'emacs-startup-hook #'which-key-mode)
 ;; ;; (oo-add-hook 'emacs-startup-hook #'idle-require-mode :append t)
 ;; (oo-add-hook 'emacs-startup-hook #'gcmh-mode :depth 91)
 ;; (oo-add-hook 'emacs-startup-hook #'recentf-mode)
 
-;; `on-first-input-hook'
+;; ** on-first-input-hook
 ;; (oo-add-hook 'on-first-input-hook #'vertico-mode)
 ;; (oo-add-hook 'on-first-input-hook #'savehist-mode)
 ;; (oo-add-hook 'on-first-input-hook #'dogears-mode)
 
 ;; (oo-add-hook 'vertico-mode-hook #'vertico-buffer-mode)
 
-;; `after-init-hook'
+;; ** after-init-hook
 ;; (oo-add-hook 'after-init-hook #'require 'evil :depth 10)
 ;; (oo-add-hook 'after-init-hook #'evil-mode :depth 90)
 ;; (oo-add-hook 'after-init-hook #'oo-set-window-divider-face :depth 11)
@@ -113,18 +114,18 @@ HOOK-OR-ADVICE.")
 ;; (oo-add-hook 'after-init-hook #'load-theme 'modus-operandi)
 ;; (oo-add-hook 'after-init-hook #'oo-override-mode :depth -100)
 
-;; `on-first-file-hook'
+;; ** on-first-file-hook
 ;; (oo-add-hook 'on-first-file-hook #'save-place-mode)
 ;; (oo-add-hook 'on-first-file-hook #'super-save-mode)
 
-;; `emacs-lisp-mode-hook'
+;; ** emacs-lisp-mode-hook
 ;; (oo-add-hook 'emacs-lisp-mode-hook 'aggressive-indent-mode)
 ;; (oo-add-hook 'emacs-lisp-mode-hook #'highlight-quoted-mode)
 
 ;; (oo-add-hook 'vertico-mode-hook #'marginalia-mode)
 ;; (oo-add-hook 'auto-fill-mode-hook #'filladapt-mode)
 
-;; `text-mode-hook'
+;; ** text-mode-hook
 ;; (oo-add-hook '(prog-mode-hook text-mode-hook) #'evil-surround-mode)
 ;; (oo-add-hook 'text-mode-hook #'flyspell-mode)
 ;; (oo-add-hook 'text-mode-hook #'auto-fill-mode)
