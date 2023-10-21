c036b175e337f5a0937399d1c026ba6e8c5169b5
Luis Henriquez-Perez <luis@luishp.xyz>
2023-10-21 13:40:10 -0400

    dot_config/emacs/lisp/oo-block-macro.el

--- dot_config/emacs/lisp/oo-block-macro.el
+++ dot_config/emacs/lisp/oo-block-macro.el
@@ -3,6 +3,7 @@
 ;; edit nodes precisely--allowing me to do things that would be very difficult with
 ;; dash's [[][-tree-map-nodes]].
 (require 'treepy)
+(require 'cl-lib)
 (require 'oo-modification-macros)
 
 ;; * block! - an advanced macro
