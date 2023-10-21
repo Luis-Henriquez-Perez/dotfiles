e8aa9050afb9ab40c4291d5af2161225a5094a2d
Luis Henriquez-Perez <luis@luishp.xyz>
2023-10-21 13:45:03 -0400

    dot_config/emacs/lisp/oo-block-definers.el

--- dot_config/emacs/lisp/oo-block-definers.el
+++ dot_config/emacs/lisp/oo-block-definers.el
@@ -1,3 +1,5 @@
+(require 'oo-block)
+
 (defun oo-defun-components (arglist)
   "Return the components of defun.
 ARGLIST is the arglist of `defun' or similar macro.
@@ -8,3 +10,5 @@ The components returned are in the form of (name args (docstring declaration int
     (let! declarations (when (equal 'declare (car-safe (car body))) (pop body)))
     (let! interactive-form (when (equal 'interactive (car-safe (car body))) (pop body)))
     (list name args (list docstring declarations interactive-form) body)))
+
+(provide 'oo-block-definers)
