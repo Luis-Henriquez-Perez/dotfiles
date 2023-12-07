d009b26009b8261d87f537e29f36efc0c0f7b51b
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-07 15:04:57 -0500

    dot_config/emacs/lisp/extensions/oo-org-markdown.el

--- dot_config/emacs/lisp/extensions/oo-org-markdown.el
+++ dot_config/emacs/lisp/extensions/oo-org-markdown.el
@@ -1,6 +1,10 @@
+;; This extension adds org markup to non-nor files.
+
 (defun oo-add-org-italic-emphasis ()
   "Add Org-style italic emphasis to comments in Lisp modes."
   (font-lock-add-keywords nil '(("\\(/[^/\n]+/\\)" 1 'italic))))
 
 (add-hook 'emacs-lisp-mode-hook 'oo-add-org-italic-emphasis)
 (add-hook 'lisp-mode-hook 'oo-add-org-italic-emphasis)
+
+(provide 'oo-org-markup-minor-mode)
