3cdd2ff5f497408ed2742cd4bfede582765ed943
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-06 19:36:15 -0500

    dot_config/emacs/lisp/oo-outline-patch.el

--- dot_config/emacs/lisp/oo-outline-patch.el
+++ dot_config/emacs/lisp/oo-outline-patch.el
@@ -2,6 +2,6 @@
 ;; be working properly.  The problem is that when you try to promote
 
 (defun oo-outline-promote-advice (orig-fn &rest args)
-  ())
+  (apply orig-fn args))
 
 (provide 'oo-outline-promote-patch)
