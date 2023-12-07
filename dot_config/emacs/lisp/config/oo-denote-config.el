d0899433d25118a05eff753aff6b5648b8767cf7
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-07 13:59:45 -0500

    dot_config/emacs/lisp/config/oo-denote-config.el

--- dot_config/emacs/lisp/config/oo-denote-config.el
+++ dot_config/emacs/lisp/config/oo-denote-config.el
@@ -2,6 +2,8 @@
 
 ;; I am inclined to use denote because of a falling out with my org
 ;; configuration in favor of a pure elisp configuration.  Although org is very
-;; powerful I do not think it provides the adequate tools to 
+;; powerful I do not think it provides the adequate tools to do what I referred
+;; to as the "top-down" approach.  It is simpler to use.  That's why I'm looking
+;; into taking notes with just.
 
 (provide 'oo-denote-config)
