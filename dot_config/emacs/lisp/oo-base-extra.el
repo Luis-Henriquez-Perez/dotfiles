b819d79c548025ff6658e7f3cfe74dcdd2b26e55
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-06 21:28:03 -0500

    dot_config/emacs/lisp/oo-base-extra.el

--- dot_config/emacs/lisp/oo-base-extra.el
+++ dot_config/emacs/lisp/oo-base-extra.el
@@ -1,4 +1,4 @@
 ;; I will be honest, I had no idea what to name this file.  Here, I will load
 ;; the patches and the extensions lazily.
 
-(oo-call-after-load 'outline #'require)
+(oo-call-after-load 'outline #'require 'evil)
