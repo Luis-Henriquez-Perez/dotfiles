49b34a5e11d55deea3309231191e44b3ac9180f9
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-07 13:58:14 -0500

    dot_config/emacs/lisp/config/oo-denote-config.el

--- dot_config/emacs/lisp/config/oo-denote-config.el
+++ dot_config/emacs/lisp/config/oo-denote-config.el
@@ -1,5 +1,6 @@
 (require 'denote)
 
-;; I am inclined to use denote because of
+;; I am inclined to use denote because of a falling out with my org
+;; configuration in favor of a pure elisp configuration.
 
 (provide 'oo-denote-config)
