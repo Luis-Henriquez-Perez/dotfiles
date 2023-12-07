0c6506ff9707e94347ef8694e8a73baa5ad493c7
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-06 21:27:06 -0500

    dot_config/emacs/lisp/oo-base-extra.el

--- dot_config/emacs/lisp/oo-base-extra.el
+++ dot_config/emacs/lisp/oo-base-extra.el
@@ -1,1 +1,4 @@
-;; I will be honest, I had no idea what to name this file.
+;; I will be honest, I had no idea what to name this file.  Here, I will load
+;; the patches and the extensions lazily.
+
+(oo-call-after-load ')
