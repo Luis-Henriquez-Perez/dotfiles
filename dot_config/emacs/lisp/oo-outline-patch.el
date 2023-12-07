a516fab91631808e318f5f2b17a12cb4f612db2f
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-06 19:33:25 -0500

    dot_config/emacs/lisp/oo-outline-patch.el

--- dot_config/emacs/lisp/oo-outline-patch.el
+++ dot_config/emacs/lisp/oo-outline-patch.el
@@ -1,5 +1,5 @@
 ;; This file is intended as a patch to `outline-promote' which doesn't seem to
-;; be working properly.  The problem is that when you 
+;; be working properly.  The problem is that when you try to promote
 
 (defun oo-outline-promote-advice (orig-fn &rest args)
   ())
