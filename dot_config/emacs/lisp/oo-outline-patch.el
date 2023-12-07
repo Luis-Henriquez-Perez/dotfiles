d52a495522f6ad8f020125bd0834affebe9d3d49
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-06 19:53:31 -0500

    dot_config/emacs/lisp/oo-outline-patch.el

--- dot_config/emacs/lisp/oo-outline-patch.el
+++ dot_config/emacs/lisp/oo-outline-patch.el
@@ -1,6 +1,7 @@
 ;; This file is intended as a patch to `outline-promote' which doesn't seem to
 ;; be working properly.  The problem is that when you try to promote
 
+;; What I don't understand is why
 (defun oo-outline-promote-advice (orig-fn &rest args)
   ;; If there's an error in `outline-up-heading' then do my own
   ;; promotion or demotion.
