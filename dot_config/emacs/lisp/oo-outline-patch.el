c5095c7cbb3f3bbaf3c0fc0b8c372aee9bbc802b
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-06 19:32:31 -0500

    dot_config/emacs/lisp/oo-outline-patch.el

--- dot_config/emacs/lisp/oo-outline-patch.el
+++ dot_config/emacs/lisp/oo-outline-patch.el
@@ -1,6 +1,7 @@
 ;; This file is intended as a patch to `outline-promote' which doesn't seem to
 ;; be working properly.
 
-(defun oo-outline-promote ())
+(defun oo-outline-promote-advice (orig-fn &rest args)
+  ())
 
 (provide 'oo-outline-promote-patch)
