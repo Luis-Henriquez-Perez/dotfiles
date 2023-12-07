9d6d07b025f0ef73c30155b1751bd457b8e2ff0e
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-07 16:01:19 -0500

    dot_config/emacs/add-eldev-to-path.sh

--- dot_config/emacs/add-eldev-to-path.sh
+++ dot_config/emacs/add-eldev-to-path.sh
@@ -1,4 +1,4 @@
 # This is the script [[][provided]] on the [[][eldev website]].
 curl -fsSL https://raw.github.com/emacs-eldev/eldev/master/webinstall/eldev | sh
 
-# In addition to this the instructions.
+# In addition to this the instructions say to add in case it already isn't.
