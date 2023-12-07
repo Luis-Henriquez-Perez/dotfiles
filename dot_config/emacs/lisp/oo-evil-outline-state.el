7fbbbeda9d69767cb2802844f730af8c58d2be8a
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-06 19:29:48 -0500

    dot_config/emacs/lisp/oo-evil-outline-state.el

--- dot_config/emacs/lisp/oo-evil-outline-state.el
+++ dot_config/emacs/lisp/oo-evil-outline-state.el
@@ -4,6 +4,8 @@
 (require 'outline)
 (require 'oo-base-library)
 
+(oo-call-after-load '(evil outline) #'require 'oo-evil-outline-state)
+
 (evil-define-state outline ())
 
 (provide 'oo-evil-outline-state)
