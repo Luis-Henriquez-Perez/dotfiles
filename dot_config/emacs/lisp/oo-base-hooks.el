ad436343c768b5e69aec630b141b91fb77360df4
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-06 21:51:50 -0500

    dot_config/emacs/lisp/oo-base-hooks.el

--- dot_config/emacs/lisp/oo-base-hooks.el
+++ dot_config/emacs/lisp/oo-base-hooks.el
@@ -6,7 +6,7 @@
 ;; =oo-config-smartparens= where the file =oo-init-smartparens= has what should
 ;; be evaluated immediately on startup and =oo-config-smartparens= has the code
 ;; that should be evaluated after smartparens is loaded.  But that is a lot of
-;; files.  Two files per package being configured. Furthermore, 
+;; files.  Two files per package being configured. Furthermore, the "init"
 (require 'oo-add-hook)
 
 ;; ;;;; setup hooks
