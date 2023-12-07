26121de38d27a7b94323e47878fe725f5c0a106e
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-07 13:09:41 -0500

    dot_config/emacs/lisp/config/oo-hyperbole-config.el

--- dot_config/emacs/lisp/config/oo-hyperbole-config.el
+++ dot_config/emacs/lisp/config/oo-hyperbole-config.el
@@ -1,3 +1,7 @@
-;; Hyperbole can make texts links.
+;; Hyperbole can make texts links.  When I was using and org configuration this
+;; was done with org in a "top-down" approach.  By which I mean you try to make
+;; everything into an org file so that you can utilize org links everywhere.
+;; Hyperbole is more of a "bottom-up" approach where you try make existing
+;; patterns into links.
 
 (provide 'oo-hyperbole-config)
