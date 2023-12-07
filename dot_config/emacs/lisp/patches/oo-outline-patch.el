38583412856f6f13c966755608a402f34b7989d2
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-06 21:36:39 -0500

    Wed Dec  6 21:36:39 2023
    D	dot_config/emacs/lisp/init-abbrevs.el
    D	dot_config/emacs/lisp/oo-base-call-after-load.el
    D	dot_config/emacs/lisp/oo-bind-functions.el
    D	dot_config/emacs/lisp/oo-binding-keys.el
    D	dot_config/emacs/lisp/oo-bootstrap-elpaca.el
    D	dot_config/emacs/lisp/oo-defaults.el
    D	dot_config/emacs/lisp/oo-evil-outline-state.el
    D	dot_config/emacs/lisp/oo-leader-map.el
    D	dot_config/emacs/lisp/oo-outline-patch.el
    D	dot_config/emacs/lisp/oo-outshine-bullets.el
    A	dot_config/emacs/lisp/patches/oo-outline-patch.el
    A	dot_config/emacs/lisp/patches/oo-outshine-bullets.el
    D	dot_config/emacs/recipes.el
    M	dot_config/stumpwm/config

---
+++ dot_config/emacs/lisp/patches/oo-outline-patch.el
@@ -0,0 +1,17 @@
+;; This file is intended as a patch to `outline-promote' which doesn't seem to
+;; be working properly.  The problem is that when you try to promote
+
+;; What I don't understand is why =outline-promote= gets the appropriate level
+;; by going up a heading instead of just computing it based on the current one.
+;; See the [[][]].
+(defun oo-outline-promote-advice (orig-fn &rest args)
+  ;; If there's an error in `outline-up-heading' then do my own
+  ;; promotion or demotion.  I should be able to do it by just traversing the
+  ;; subheadings and promoting or demoting each level by 1.
+  (condition-case err
+      (apply orig-fn args)
+    ;; What 
+    ()
+    ()))
+
+(provide 'oo-outline-promote-patch)
