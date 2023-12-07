360cbac8d43ab803c79f8ed653c4ac1b118fcbce
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-06 20:38:00 -0500

    dot_config/emacs/lisp/oo-outline-patch.el

--- dot_config/emacs/lisp/oo-outline-patch.el
+++ dot_config/emacs/lisp/oo-outline-patch.el
@@ -8,31 +8,10 @@
   ;; If there's an error in `outline-up-heading' then do my own
   ;; promotion or demotion.  I should be able to do it by just traversing the
   ;; subheadings and promoting or demoting each level by 1.
-  (cond
-   ((eq which 'region)
-    (outline-map-region #'outline-promote (region-beginning) (region-end)))
-   (which
-    (outline-map-region #'outline-promote
-			            (point)
-			            (save-excursion (outline-get-next-sibling) (point))))
-   (t
-    (outline-back-to-heading t)
-    (let* ((head (match-string-no-properties 0))
-	       (level (save-match-data (funcall outline-level)))
-	       (up-head (or (outline-head-from-level (1- level) head)
-			            ;; Use the parent heading, if it is really
-			            ;; one level less.
-			            (save-excursion
-			              (save-match-data
-			                (outline-up-heading 1 t)
-			                (and (= (1- level) (funcall outline-level))
-				                 (match-string-no-properties 0))))
-                        ;; Bummer!! There is no lower level heading.
-                        (outline-invent-heading head 'up))))
-
-      (unless (rassoc level outline-heading-alist)
-	    (push (cons head level) outline-heading-alist))
-
-      (replace-match up-head nil t)))))
+  (condition-case err
+      (apply orig-fn args)
+    ;; What 
+    ()
+    ()))
 
 (provide 'oo-outline-promote-patch)
