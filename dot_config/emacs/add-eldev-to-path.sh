d3ba2f03fb3b44513a1cdbf083397a397d80154d
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-07 16:03:03 -0500

    dot_config/emacs/add-eldev-to-path.sh

--- dot_config/emacs/add-eldev-to-path.sh
+++ dot_config/emacs/add-eldev-to-path.sh
@@ -1,4 +1,6 @@
-# This is the script [[][provided]] on the [[][eldev website]].
+# This is the script [[][provided]] on the [[][eldev website]].  I want to
+# gather scripts like this together so I can run them instead of looking the
+# stuff again.
 curl -fsSL https://raw.github.com/emacs-eldev/eldev/master/webinstall/eldev | sh
 
 # In addition to this the instructions say to add in case it already isn't.
