c0d5c1eb435aad331e224454114c26c16327c197
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-07 20:41:37 -0500

    dot_config/emacs/config.el

--- dot_config/emacs/config.el
+++ dot_config/emacs/config.el
@@ -892,32 +892,6 @@ when ALT should be invoked instead of ORIG."
 ;; :ID:       20231110T074115.247845
 ;; :END:
 ;; I like knowing what prefix I typed in to get a certain key.
-(set! which-key-show-prefix 'top)
-;; **** which-key
-;; :PROPERTIES:
-;; :ID:       2ad092a3-ff63-49cd-91b9-380c91dbe9f5
-;; :END:
-;; Emacs is full of so many keybindings, that it can be difficult to keep track of
-;; them.  Especially when you're starting out, but even when you're an Emacs-pro,
-;; it's easy to forget what a particular functionality is bound to.  Typically,
-;; you'll remember the first few key strokes but struggle with the rest.  To address
-;; this [[github:][which-key]] displays key binding sequences in the minibuffer as your typing
-;; them.  By doing this you can "discover" the commands as you go along.
-;; Note that this can't work with `on-first-input-hook' because which-key
-;; doesn't happen on first keypress.  It needs to be in the startup hook.
-(oo-add-hook 'emacs-startup-hook #'which-key-mode)
-;; (set! which-key-sort-uppercase-first nil)
-(set! which-key-max-display-columns nil)
-(set! which-key-add-column-padding 1)
-(set! which-key-min-display-lines 1)
-(set! which-key-side-window-slot -10)
-(set! which-key-sort-order #'which-key-prefix-then-key-order)
-(set! which-key-popup-type 'side-window)
-(set! which-key-idle-delay 0.8)
-;; (set! line-spacing 3 :hook which-key-init-buffer-hook :local t)
-
-(set! which-key-show-transient-maps t)
-(set! which-key-show-operator-state-maps t)
 ;; *** leader map bindings
 ;; :PROPERTIES:
 ;; :ID:       20230824T072915.346305
