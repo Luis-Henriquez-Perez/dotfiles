9a28d61561be5b96a76fbfbd44700081b1979dfb
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-08 08:14:12 -0500

    dot_config/emacs/lisp/config/oo-evil-configuration.el

---
+++ dot_config/emacs/lisp/config/oo-evil-configuration.el
@@ -0,0 +1,11 @@
+;; By default =evil= displays the current state in the echo area.  I think some
+;; indicator for the current state is necessary but I don't want to do it via
+;; echoing.  Instead I plan to do it primarily via cursor colors; and possibly the
+;; modeline as well.
+(set! evil-echo-state nil)
+
+;; Whether the cursor is moved backwards when exiting insert state.
+(set! evil-move-cursor-back nil)
+
+;; It's confusing to if the cursor is allowed to move beyond the end of the line.
+(set! evil-move-beyond-eol nil)
