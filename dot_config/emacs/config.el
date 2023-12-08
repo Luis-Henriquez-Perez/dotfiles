e292ab16ca821f814229277a272045df872dee2a
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-08 12:02:32 -0500

    dot_config/emacs/config.el

--- dot_config/emacs/config.el
+++ dot_config/emacs/config.el
@@ -1604,25 +1604,6 @@ when ALT should be invoked instead of ORIG."
 ;; default. It is disruptive and can greatly slow down =consult-buffer= because it actually opens each
 ;; buffer go through. More often than not I don't need the preview anyway.
 (set! consult-preview-key nil)
-;; *** recentf
-;; :PROPERTIES:
-;; :ID:       5ab47c35-53bd-460f-ba41-6f3075bd1222
-;; :END:
-;; =recentf= is a built-in program that tracks the files you've opened recently
-;; persistently.  This is a great idea because these are the files you'll likely
-;; revisit.  In practice, I look at this list of files in addition to the buffers I
-;; already have open using a [[id:f26bedb3-a172-4543-afd0-4c47f5872d15][completion-framework]].  Because of this I rarely
-;; have to set out to look for a file with =dired=.
-(oo-add-hook 'emacs-startup-hook #'recentf-mode)
-
-(set! recentf-max-menu-items 0)
-(set! recentf-max-saved-items 700)
-(set! recentf-save-file (concat oo-cache-dir "recentf"))
-(set! recentf-auto-cleanup (* 60 10))
-(set! recentf-filename-handlers '(file-truename))
-
-(oo-add-advice #'recentf-save-list :before #'recentf-cleanup)
-(oo-add-hook 'kill-emacs-hook #'recentf-save-list)
 
 (--each (list #'recentf-mode #'recentf-cleanup #'recentf-save-list)
   (oo-add-advice it :around #'oo-funcall-silently))
