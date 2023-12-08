6ee073cd5731d8cfbd5d50650e8e9240d21d1fe2
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-08 09:50:28 -0500

    dot_config/emacs/lisp/config/oo-org-config.el

--- dot_config/emacs/lisp/config/oo-org-config.el
+++ dot_config/emacs/lisp/config/oo-org-config.el
@@ -1,6 +1,7 @@
-;;; foo
-;;; capturing
-;;;; use =completing-read= for choosing a template
+;;; org
+;;;; refiling
+;;;; capturing
+;;;;; use =completing-read= for choosing a template
 ;; Org uses its own ugly looking interface for choosing a capture template.  I'd rather keep things
 ;; consistent and use =completing-read= which is what I use for everything else.
 (defun! oo-choose-capture-template ()
@@ -12,10 +13,10 @@
   (let! selected (completing-read "capture template: " alist nil t))
   (org-capture nil (alist-get selected alist nil nil #'string=)))
 
-;;;; open capture templates at bottom
+;;;;; open capture templates at bottom
 (oo-popup-at-bottom "CAPTURE[^z-a]+")
 
-;;;; generate a generic source block headline
+;;;;; generate a generic source block headline
 (defun oo-src-block-headline (title lang)
   (let! lang (or lang "emacs-lisp"))
   (let! src-block (org-ml-build-src-block :language lang))
@@ -24,7 +25,7 @@
   (thread-last (org-ml-build-headline! :title-text "%?")
                (org-ml-headline-set-section (list property-drawer src-block))
                (org-ml-to-trimmed-string)))
-;;;; defcapture!
+;;;;; defcapture!
 ;; This is a declarative macro for conveniently defining capture templates. I'll note that the plist
 ;; arguments for the macro are evaluated. This is because I feel that having dynamic arguments is more
 ;; useful.
@@ -41,7 +42,7 @@
     (appending! pairs (list :keys (char-to-string (seq-first name)))))
   `(set! org-capture-templates (doct-add-to org-capture-templates (list ,name ,@pairs))))
 
-;;;; generic capture template
+;;;;; generic capture template
 ;; This capture template is designed for code that I write on the fly and don't know where it should be
 ;; organized yet. It will capture the headline in a headline deemed "uncategorized".
 (defcapture! emacs ()
@@ -49,7 +50,7 @@
   :olp '("emacs" "uncategorized")
   :template (-partial (function oo-src-block-headline) nil "emacs-lisp"))
 
-;;;; outshine capture template
+;;;;; outshine capture template
 ;; This is a capture template that inserts.
 ;; 1. a function that finds where to insert the template
 ;; 2. a function that generates the template
