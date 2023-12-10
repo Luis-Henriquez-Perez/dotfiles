536b74fdef5c9906c8f720fa130498bef1b9b075
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-09 23:27:45 -0500

    dot_config/emacs/lisp/oo-base-modules.el

--- dot_config/emacs/lisp/oo-base-modules.el
+++ dot_config/emacs/lisp/oo-base-modules.el
@@ -3,8 +3,8 @@
 ;; This file is for specifying information about modules.  What I call modules
 ;; differs from what would be considered modules in doom or spacemacs.  I am
 ;; hesitant to make a generic abstraction like "completion" or to group a set of
-;; packages together as one unit.  A "module" as I use it here is more of just a
-;; single package
+;; packages together as one unit.  A "module" as I use it here is more pertains
+;; to a single package and its configuration.  Specifically.
 
 (defvar oo-loaded-modules nil)
 
