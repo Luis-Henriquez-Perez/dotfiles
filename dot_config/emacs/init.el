21d54681f609775e2e9b36f1afc8f6b89c9a9a75
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-12 11:11:17 -0500

    dot_config/emacs/init.el

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -69,7 +69,8 @@
 ;; are as many tools to deal with a single large file.
 
 ;; There is a general desire to keep package information together, but with
-;; every package the information is split into two components--
+;; every package the information is split into two components--things that will
+;; be evaluated immediately and things that will be evaluated later.
 
 ;;;; set initial variables
 ;;;;;  disable garbage collection until I'm done with startup
