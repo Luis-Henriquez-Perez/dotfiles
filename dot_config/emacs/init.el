2092d1d0a3fb31e7286ee9073977d0642560bc31
Luis Henriquez-Perez <luis@luishp.xyz>
2023-12-12 12:41:24 -0500

    dot_config/emacs/init.el

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -138,7 +138,7 @@ HOOK-OR-ADVICE.")
 
 ;;;; load library
 (require 'oo-base-utils)
-;; (require 'oo-block-macro)
+(require 'oo-block-macro)
 ;; (require 'oo-call-after-load)
 ;; (require 'oo-call-after-keymap)
 ;; (require 'oo-call-after-evil-state)
