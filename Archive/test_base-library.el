d46b5c295061f5eaa469d3ac69a326d1b63b618a
Luis Henriquez-Perez <luis@luishp.xyz>
2024-01-24 11:01:58 -0500

    Wed Jan 24 11:01:58 2024
    M	Archive/test_base-library.el

--- Archive/test_base-library.el
+++ Archive/test_base-library.el
@@ -16,10 +16,6 @@
 (ert-deftest oo-snoc ()
   (should (oo-snoc '(1 2))))
 
-(should (oo-snoc '(1 2)))
-(should (oo-snoc '(1 2)))
-(should (oo-snoc '(1 2)))
-
 (ert-deftest for! ()
   ;; Works for the syntax =(repeat N)= where N is a positive integer.
   (should (= 11 (let ((n 1)) (for! (repeat 10) (cl-incf n)) n)))
