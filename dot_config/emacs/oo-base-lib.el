7893c01db1257183447eb84a3a6c7463ac6f067e
Luis Henriquez-Perez <luis@luishp.xyz>
2024-01-24 15:56:22 -0500

    Wed Jan 24 15:56:22 2024
    M	dot_config/emacs/oo-base-lib.el
    M	dot_config/emacs/tests/test_oo-base-lib.el

--- dot_config/emacs/oo-base-lib.el
+++ dot_config/emacs/oo-base-lib.el
@@ -16,7 +16,9 @@
 ;; for =oo-install-packages-with-package-dot-el.el=.
 
 ;;;; Require built-in packages
+;; I need it to help me with pattern matching.
 (require 'pcase)
+;; It has several useful functions and macros.
 (require 'subr-x)
 (require 'cl-lib)
 ;;;; aliases
@@ -135,77 +137,77 @@ symbol as in `dolist', but.  LIST can be a sequence."
                (dotimes (,elt ,list) ,@body))
               (t
                (error "Unknown list predicate: %S" ',pred)))))))
-;; ;;;; setting
-;; (cl-defmacro appending! (place list &key (setter 'setf))
-;;   "Append LIST to the end of PLACE.
-;; SETTER is the symbol of the macro or function used to do the setting."
-;;   `(,setter ,place (append ,place ,list)))
+;;;; setting
+(cl-defmacro appending! (place list &key (setter 'setf))
+  "Append LIST to the end of PLACE.
+SETTER is the symbol of the macro or function used to do the setting."
+  `(,setter ,place (append ,place ,list)))
 
-;; ;; Important to note that this macro is not as efficient as pushing because it's adding to the end
-;; ;; of the list.  So this macro should be used only in non-performance-intensive code.  In
-;; ;; performance-intensive code we need the =push-nreverse= idiom.
-;; (cl-defmacro collecting! (place item &key (setter 'setf))
-;;   "Affix ITEM to the end of PLACE.
-;; SETTER is the same as in `appending!'."
-;;   `(,setter ,place (oo-snoc ,place ,item)))
+;; Important to note that this macro is not as efficient as pushing because it's adding to the end
+;; of the list.  So this macro should be used only in non-performance-intensive code.  In
+;; performance-intensive code we need the =push-nreverse= idiom.
+(cl-defmacro collecting! (place item &key (setter 'setf))
+  "Affix ITEM to the end of PLACE.
+SETTER is the same as in `appending!'."
+  `(,setter ,place (oo-snoc ,place ,item)))
 
-;; (defalias 'snocing! 'collecting!)
-;; (defalias 'affixing! 'collecting!)
+(defalias 'snocing! 'collecting!)
+(defalias 'affixing! 'collecting!)
 
-;; ;; You might be wondering why I didn't create a macro with a setter for these.
-;; ;; Well I haven't had a case yet when I've wanted to increment or decrement the
-;; ;; value symbol used in customization.
-;; (defalias 'incrementing! 'cl-incf)
-;; (defalias 'counting! 'cl-incf)
-;; (defalias 'decrementing! 'cl-decf)
+;; You might be wondering why I didn't create a macro with a setter for these.
+;; Well I haven't had a case yet when I've wanted to increment or decrement the
+;; value symbol used in customization.
+(defalias 'incrementing! 'cl-incf)
+(defalias 'counting! 'cl-incf)
+(defalias 'decrementing! 'cl-decf)
 
-;; (cl-defmacro prepending! (place list &key (setter 'setf))
-;;   "Prepend LIST to beginning of PLACE.
-;; SETTER is the same as in `appending!'."
-;;   `(,setter ,place (append ,list ,place)))
+(cl-defmacro prepending! (place list &key (setter 'setf))
+  "Prepend LIST to beginning of PLACE.
+SETTER is the same as in `appending!'."
+  `(,setter ,place (append ,list ,place)))
 
-;; (cl-defmacro maxing! (place form &key (setter 'setf) (comparator '>))
-;;   "Set PLACE to the greater of PLACE and FORM.
-;; SETTER is the same as in `appending!'."
-;;   (cl-with-gensyms (value1 value2)
-;;     `(,setter ,place (let ((,value1 ,form)
-;;                            (,value2 ,place))
-;;                        (if (,comparator ,value1 ,value2) ,value1 ,value2)))))
+(cl-defmacro maxing! (place form &key (setter 'setf) (comparator '>))
+  "Set PLACE to the greater of PLACE and FORM.
+SETTER is the same as in `appending!'."
+  (cl-with-gensyms (value1 value2)
+    `(,setter ,place (let ((,value1 ,form)
+                           (,value2 ,place))
+                       (if (,comparator ,value1 ,value2) ,value1 ,value2)))))
 
-;; (cl-defmacro minning! (place form &key (setter 'setf) (comparator '<))
-;;   "Set PLACE to the lesser of PLACE and FORM.
-;; SETTER is the same as in `appending!'. COMPARATOR is the same as in `maxing!'."
-;;   `(maxing! ,place ,form :setter ,setter :comparator ,comparator))
+(cl-defmacro minning! (place form &key (setter 'setf) (comparator '<))
+  "Set PLACE to the lesser of PLACE and FORM.
+SETTER is the same as in `appending!'. COMPARATOR is the same as in `maxing!'."
+  `(maxing! ,place ,form :setter ,setter :comparator ,comparator))
 
-;; (cl-defmacro concating! (place string &key (setter 'setf) separator)
-;;   "Concat PLACE and STRING with SEPARATOR.
-;; SETTER is the same as in `appending!'"
-;;   `(,setter ,place (string-join (list ,place ,string) ,separator)))
+(cl-defmacro concating! (place string &key (setter 'setf) separator)
+  "Concat PLACE and STRING with SEPARATOR.
+SETTER is the same as in `appending!'"
+  `(,setter ,place (string-join (list ,place ,string) ,separator)))
 
-;; (cl-defmacro adjoining! (place item &key test test-not key (setter 'setf))
-;;   "Set PLACE to the value of `cl-adjoin'.
-;; SETTER is the same as in `appending!'."
-;;   `(,setter ,place (cl-adjoin ,item ,place :test ,test :test-not ,test-not :key ,key)))
+(cl-defmacro adjoining! (place item &key test test-not key (setter 'setf))
+  "Set PLACE to the value of `cl-adjoin'.
+SETTER is the same as in `appending!'."
+  `(,setter ,place (cl-adjoin ,item ,place :test ,test :test-not ,test-not :key ,key)))
 
-;; ;; I know =push= already exists.  But I want a variant of push that can be used
-;; ;; with the =block!= macro.
-;; (cl-defmacro pushing! (place item &key (setter 'setf))
-;;   "Cons ITEM to PLACE.
-;; SETTER is the same as in `appending!'."
-;;   `(,setter ,place (cons ,item ,place)))
+;; I know =push= already exists.  But I want a variant of push that can be used
+;; with the =block!= macro.
+(cl-defmacro pushing! (place item &key (setter 'setf))
+  "Cons ITEM to PLACE.
+SETTER is the same as in `appending!'."
+  `(,setter ,place (cons ,item ,place)))
 
-;; ;; To configure variables I don't use the standard =setq=--at least not directly.
-;; ;; Instead, I use =set!=.  Adjoining is one of the most common operations done to
-;; ;; lisp symbols when configuring Emacs.
+;; To configure variables I don't use the standard =setq=--at least not directly.
+;; Instead, I use =set!=.  Adjoining is one of the most common operations done to
+;; lisp symbols when configuring Emacs.
 
-;; ;; Something I was always confused about was why adjoin instead of just using
-;; ;; =push=.  The latter is more performant; however I don't think that's.  The best reason I could
-;; ;; think of is that sometimes you want to re-evaluate parts of your configuration
-;; ;; and in that case it is more convenient to have =adjoin= over =push=.
-;; (cl-defmacro unioning! (place list &key test test-not key (setter 'setf))
-;;   "Set PLACE to the union of PLACE and FORM.
-;; SETTER is the same as in `appending!'."
-;;   `(,setter ,place (cl-union ,place ,list :test ,test :test-not ,test-not :key ,key)))
+;; Something I was always confused about was why adjoin instead of just using
+;; =push=.  The latter is more performant; however I don't think that's.  The best reason I could
+;; think of is that sometimes you want to re-evaluate parts of your configuration
+;; and in that case it is more convenient to have =adjoin= over =push=.
+(cl-defmacro unioning! (place list &key test test-not key (setter 'setf))
+  "Set PLACE to the union of PLACE and FORM.
+SETTER is the same as in `appending!'."
+  `(,setter ,place (cl-union ,place ,list :test ,test :test-not ,test-not :key ,key)))
 
 ;; ;; To configure variables I don't use the standard =setq=--at least not directly.
 ;; ;; Instead, I use =set!=.  Adjoining is one of the most common operations done to
--- dot_config/emacs/tests/test_oo-base-lib.el
+++ dot_config/emacs/tests/test_oo-base-lib.el
@@ -71,28 +71,28 @@
   ;; Should allow me to destructure arguments.
   (should (equal '(3 9) (let (list) (for! (a b) '((1 2) (4 5)))))))
 
-;; (ert-deftest oo-block-interpret-tree ()
-;;   (should (equal '(nil ((catch 'break! (for! (n 10) (catch 'continue (+ 1 1))))))
-;;                  (oo-block-interpret-tree nil '((for! (n 10) (+ 1 1))))))
+(ert-deftest oo-block-interpret-tree ()
+  (should (equal '(nil ((catch 'break! (for! (n 10) (catch 'continue (+ 1 1))))))
+                 (oo-block-interpret-tree nil '((for! (n 10) (+ 1 1))))))
 
-;;   (should (equal '((:let ((foo nil))) ((collecting! foo 1)))
-;;                  (oo-block-interpret-tree nil '((collecting! foo 1)))))
+  (should (equal '((:let ((foo nil))) ((collecting! foo 1)))
+                 (oo-block-interpret-tree nil '((collecting! foo 1)))))
 
-;;   (should (equal (nil (cl-flet ((foo nil (+ 1 1))) (+ 2 2)))
-;;                  (oo-block-interpret-tree nil '((stub! foo () (+ 1 1)) (+ 2 2)))))
+  (should (equal (nil (cl-flet ((foo nil (+ 1 1))) (+ 2 2)))
+                 (oo-block-interpret-tree nil '((stub! foo () (+ 1 1)) (+ 2 2)))))
 
-;;   (should (equal (oo-block-interpret-tree nil '((without! a b c)))
-;;                  '((:no-let (a b c)) (nil))))
+  (should (equal (oo-block-interpret-tree nil '((without! a b c)))
+                 '((:no-let (a b c)) (nil))))
 
-;;   (should (equal (oo-block-interpret-tree nil '((let! foo 1)))
-;;                  `((:let ((foo nil))) (nil))))
+  (should (equal (oo-block-interpret-tree nil '((let! foo 1)))
+                 `((:let ((foo nil))) (nil))))
 
-;;   ;; (should (equal ((:let ((gc-cons-threshold gc-cons-threshold))) (nil)))
-;;   ;;         )
-;;   ;; (should (equal (oo-block-interpret-tree nil '((let! foo 1)))))
+  ;; (should (equal ((:let ((gc-cons-threshold gc-cons-threshold))) (nil)))
+  ;;         )
+  ;; (should (equal (oo-block-interpret-tree nil '((let! foo 1)))))
 
-;;   ;; (should (equal (oo-block-interpret-tree nil '((let! foo 1)))))
-;;   )
+  ;; (should (equal (oo-block-interpret-tree nil '((let! foo 1)))))
+  )
 
 ;; (ert-deftest block! ()
 ;;   (block! nil (+ 1 1) (maxing! foo 2))
