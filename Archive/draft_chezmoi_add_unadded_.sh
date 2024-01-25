3cb6e21915c713ec6f483928b06ef7f82010d565
Luis Henriquez-Perez <luis@luishp.xyz>
2024-01-25 09:39:59 -0500

    Thu Jan 25 09:39:59 2024
    M	Archive/draft_chezmoi_add_unadded_.sh
    M	dot_config/emacs/lisp/oo-base-lib.el

--- Archive/draft_chezmoi_add_unadded_.sh
+++ Archive/draft_chezmoi_add_unadded_.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
+# For a lot of this I took as an example =chezmoi-write= from the chezmoi
+# package.  But there is a difference: =chezmoi-write= has access to the file,
+# whereas with this command I do not.  The commands =chezmoi status= and
+# =chezmoi managed= only output partial paths.  To get the full path you combine
+# the output of =chezmoi target-path= and each of =chezmoi-managed=.  And then
+# to get the source you can apply it to the result of that.
 # Add files which have not been added.
 
-# List unapplied files (added in source state but not in target state)
-unapplied=$(chezmoi status | grep '^ \?A' | cut -c4-)
+# I am trying here to automate the easy ones.  Therefore, = A=, = M=, =DA= I
+# think I would almost always want applied. =MM= I would have to consider more
+# closely, probably having to do =chezmoi diff=.  These are the files that have
+# been modified in the source state and the target state--something that I do
+# not recommend doing.  I would almost always prefer modifing files.
 
+# = A= and = M= are do not require confirmation chezmoi.  But =DA=, signaling
+# the recreation of a file that has been deleted in the actual filesystem, but
+# would be added back
+
+# List unapplied files (added in source state but not in target state)
+# \|DA.
+
+# I do not think any of these actions would change the source state.  All of
+# them propagate the source state.
+unapplied=$(chezmoi status | grep '^\( A\| M\|DA\)' | cut -c4-)
 # Exit if no unapplied files
 if [ -z "$unapplied" ]; then
     echo "No unapplied files to process."
     exit 0
 fi
 
+target_base=$(chezmoi target-path)
+
 # Iterate through unapplied files and apply changes
 for file in $unapplied; do
-    # Apply changes to the specific file
-    # echo "Applying changes to $file."
-    source=$(chezmoi source-path "$file")
-    chezmoi -nv apply --source-path "$source"
+    # echo "-> $file"
+    chezmoi apply "$target_base/$file" --force
 done
