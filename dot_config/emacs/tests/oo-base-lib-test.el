a639e0d1492ba41576ec86a2bbf8a02bd4e741d6
Luis Henriquez-Perez <luis@luishp.xyz>
2024-01-29 13:43:11 -0500

    Mon Jan 29 13:43:11 2024
    A	dot_config/emacs/tests/oo-base-lib-test.el
    D	dot_config/emacs/tests/test_oo-base-lib.el

--- dot_config/emacs/tests/oo-base-lib-test.el
+++ dot_config/emacs/tests/oo-base-lib-test.el
@@ -1,3 +1,47 @@
+;;; oo-base-lib-test.el --- `oo-base-lib' tests   -*- lexical-binding: t; -*-
+;;
+;; Copyright (c) 2015-2022, Luis Henriquez <luis@luishp.xyz.io>
+;;
+;; Author: Luis Henriquez <luis@luishp.xyz.io>
+;; Maintainer: Luis Henriquez <luis@luishp.xyz.io>
+;;
+;; Created: 28 Jan 2024
+;;
+;;
+;; License: GPLv3
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see
+;; <http://www.gnu.org/licenses/>.
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;; This file contains tests for `lib-string'.
+;;
+;;; Code:
+;; [[https://scripter.co/quick-intro-to-emacs-lisp-regression-testing/][quick-intro-to-emacs-lisp-regression-testing]]
+(require 'oo-base-lib)
+
+;; If the directory happens to have both compiled and uncompiled
+;; version, prefer to use the newer (typically the uncompiled) version.
+(setq load-prefer-newer t)
+(setq ert-batch-print-level 10)
+(setq ert-batch-print-length 120)
+;; (add-to-list 'load-path (locate-user-emacs-file "lisp"))
+;; (add-to-list 'load-path (locate-user-emacs-file "tests"))
+
 ;; Does not need to be required because already autoloaded.
 
 (require 'oo-base-lib)
@@ -89,13 +133,17 @@
                  (let (nums) (for! (n '(1 2 3 4)) (push n nums)) nums)))
 
   ;; Should allow me to destructure arguments.
-  (should (equal '(3 9) (let (list) (for! (a b) '((1 2) (4 5)))))))
+  (should (equal '(3 9) (let ((list '((1 2) (4 5)))
+                              (result nil))
+                          (for! ((a b) list)
+                            (push (+ a b) result))
+                          (reverse result)))))
 
-(ert-deftest take-while! ()
-  (should (let ((foo '(a b 1 2 3))) (list (take-while! (symbolp foo) foo) foo))
-          '((a b) (1 2 3)))
-  ;; (should ())
-  )
+;; (ert-deftest take-while! ()
+;;   (should (let ((foo '(a b 1 2 3))) (list (take-while! (symbolp foo) foo) foo))
+;;           '((a b) (1 2 3)))
+;;   ;; (should ())
+;;   )
 
 (ert-deftest oo-block-interpret-tree ()
   (should (equal '(nil ((catch 'break! (for! (n 10) (catch 'continue (+ 1 1))))))
@@ -117,10 +165,8 @@
                  (oo-block-interpret-tree nil '((let! foo 1)))))
   
   (should (equal '((:let ((gc-cons-threshold gc-cons-threshold))) ((setq gc-cons-threshold 100)))
-                 (oo-block-interpret-tree nil '((let! gc-cons-threshold 100)))))
-
-  ;; I want to see how it works with multiple data.
-  )
+                 (oo-block-interpret-tree nil '((let! gc-cons-threshold 100))))))
+;; I want to see how it works with multiple data.
 
 ;; (ert-deftest block! ()
 ;;   (block! nil (+ 1 1) (maxing! foo 2))
@@ -131,17 +177,27 @@
 (ert-deftest oo-pcase-pattern ()
   (should (equal '`((,a (,b) ,c . ,d)) (oo-pcase-pattern '((a (b) c . d)))))
   (should (equal '`[,a ,b ,c] (oo-pcase-pattern [a b c])))
-  (should (equal '`(,a [,b ,c (,f [,g]) [,d]] ,e)
-                 (oo-pcase-pattern '(a [b c (f [g]) [d]] e)))))
+  (should (equal '`(,a [,b [,d]] ,e) (oo-pcase-pattern '(a [b [d]] e)))))
 
 (ert-deftest let! ()
-  (should (equal '(1 2) (let! ((foo 1) ((a b) '(1 2))) (list foo a b))))
-  (should (equal (let! ((foo 1) ([c d] [9 8]) ((a b) '(1 2)))
-                   (list foo a b c d))
-                 '(1 2 9 8))))
+  (should (equal '((1 . 2) 1 2)
+                 (let! (((&as foo (a . b)) '(1 . 2))) (list foo a b))))
+  (should (equal '(1 2 3) (let! (((a b . c) '(1 2 . 3))) (list a b c))))
+  (should (equal '(1 1 2) (let! ((foo 1) ((a b) '(1 2))) (list foo a b))))
+  (should (equal '(1 1 2 9 8)
+                 (let! ((foo 1) ([c d] [9 8]) ((a b) '(1 2)))
+                   (list foo a b c d)))))
+
+;; (ert-deftest with-map! ()
+;;   (should (equal 2 (with-map! '((a . 1) (b . 2)) (+ !a !b))))
+;;   (should (equal 2 (with-map! '(a 1 b 2) (+ !a !b)))))
 
 (ert-deftest oo-defun-components ()
-  (should (equal '(foo (a b c) ("foo" (interactive)) body)
-                 (oo-defun-components '(foo (a b c) "foo" (interactive) 1)))))
+  (should (equal '(foo (a b) ("foo" nil nil) ((+ 1 1)))
+                 (oo-defun-components '(foo (a b) "foo" (+ 1 1)))))
+  (should (equal '(foo (a b c) ("foo" nil (interactive)) (1))
+                 (oo-defun-components '(foo (a b c) "foo" (interactive) 1))))
+  (should (equal '(foo (a b c) (nil nil (interactive)) (1))
+                 (oo-defun-components '(foo (a b c) (interactive) 1)))))
 
 (provide 'test_oo-base-lib)
