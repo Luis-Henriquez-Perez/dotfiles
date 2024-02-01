6ecf5f2b696c21e314a582966c667d4ab710b161
Luis Henriquez-Perez <luis@luishp.xyz>
2024-02-01 11:14:15 -0500

    Thu Feb  1 11:14:15 2024
    M	dot_config/emacs/tests/oo-base-lib-test.el
    M	dot_config/stumpwm/config
    D	scripts/chezmoi_safe_apply_and_commit.sh
    D	scripts/download-and-index-email.sh

--- dot_config/emacs/tests/oo-base-lib-test.el
+++ dot_config/emacs/tests/oo-base-lib-test.el
@@ -44,7 +44,8 @@
 (ert-deftest oo--match-form-wrappers ()
   (should (equal (list nil '(,a ,b ,c ,d))
                  (oo--match-form-wrappers nil '(a b c d))))
-  (should (equal (list '(let* ((pair gsym))) '(,a ,b ,gsym ,d))
+  (should (equal (list '((let! ((pair gsym) ((a . b) gsym))))
+                       '(,a ,b ,gsym ,d))
                  (cl-letf* (((symbol-function #'cl-gensym) (lambda (&rest _) 'gsym)))
                    (oo--match-form-wrappers nil '(a b (&as pair (a . b)) d))))))
 
