bac94ef21125085855b599f66cada84124c34cb8
Luis Henriquez-Perez <luis@luishp.xyz>
2024-02-02 09:57:14 -0500

    Fri Feb  2 09:57:14 2024
    M	dot_config/emacs/init.el

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -41,24 +41,25 @@
 ;; have about compiling than having to consider loading order and
 ;; features with =require=.
 (require 'oo-base-library)
-(require 'oo-base-settings)
-(require 'oo-base-custom)
+;; (require 'oo-base-settings)
+;; (require 'oo-base-custom)
 ;; (require 'oo-base-package)
 
 ;;;; disable garbage collection until I'm done with startup
 ;; This variable controls how often.  Setting it to =most-positive-fixnum=, a very
 ;; big number, essentially disables garbage collection.  The garbage collection is
 ;; later reset to a reasonable value.
-(startup-set! gc-cons-threshold most-positive-fixnum)
+;; (startup-set! gc-cons-threshold most-positive-fixnum)
 
 ;; This is the percentage of the heap before.
-(startup-set! gc-cons-percentage 0.8)
+;; (startup-set! gc-cons-percentage 0.8)
 ;;;; don't search for whenever a package is loaded
-(startup-set! file-name-handler-alist nil)
+;; (startup-set! file-name-handler-alist nil)
 ;;;; prevent flashing of unstyled modeline
 ;; Don't render the modeline on startup.  For one thing, the startup looks
 ;; better without flashing stuff on the screen.  Additionally, the more that's
 ;; saved on rendering, the faster the startup.
-(startup-set! mode-line-format nil set-default)
+;; (startup-set! mode-line-format nil set-default)
 
+;;; init.el ends here
 (provide 'init)
