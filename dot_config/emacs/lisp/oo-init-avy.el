cb82720f56a44cc1bffb9e78f2f363782e21f04c
Luis Henriquez-Perez <luis@luishp.xyz>
2024-02-14 08:31:42 -0500

    Delete these files.

--- dot_config/emacs/lisp/oo-init-avy.el
+++
@@ -1,7 +0,0 @@
-(opt! avy-style 'pre)
-
-(opt! avy-keys (eval-when-compile (string-to-list "jfkdlsaurieowncpqmxzb")))
-
-(opt! avy-background nil)
-
-(opt! avy-timeout-seconds 0.3)
--- dot_config/emacs/tests/oo-base-custom-test.el
+++
@@ -1,6 +0,0 @@
-;; 
-;; (ert-deftest oo-add-advice ())
-
-;; (ert-deftest oo-add-hook ())
-
-(provide 'oo-base-custom-test)
