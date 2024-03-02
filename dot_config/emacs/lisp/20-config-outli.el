59a5909dab7100a224c95c61d22233ce0ed09121
Luis Henriquez-Perez <luis@luishp.xyz>
2024-03-02 10:54:52 -0500

    Update.

--- dot_config/emacs/lisp/20-config-outli.el
+++ dot_config/emacs/lisp/20-config-outli.el
@@ -1,24 +1,7 @@
-;; outli doesn't refontify headlines after theme change.  So if you change a
-;; theme, the headlines will remain in the color of the previous theme.
-(defadvice! load-theme@ARfontify-outli (fn &rest args)
-  (let (outli-buffers)
-    (dolist (buffer (buffer-list))
-      (with-current-buffer buffer
-        (when outli-mode
-          (outli-mode -1)
-          (push buffer outli-buffers))))
-    (unwind-protect (apply fn args)
-      (dolist (buffer outli-buffers)
-        (with-current-buffer buffer
-          (outli-mode 1))))))
+;; TODO: figure out how to make this a named advice.
+(advice-add 'load-theme :after (lambda (&rest _) (outli-reset-all-faces)))
 
-;; (defun oo-print-fn-and-args (orig-fn arg1 arg2)
-;;   (message "(outli-fontify-headlines %S %S)" arg1 arg2)
-;;   (funcall orig-fn arg1 arg2))
-
-;; (advice-add #'outli-fontify-headlines :around #'oo-print-fn-and-args)
-;; (advice-remove 'outli-fontify-headlines #'oo-print-fn-and-args)
-
+;; TODO: set the style for all configs.
 (setf (cl-fourth (assoc 'emacs-lisp-mode outli-heading-config)) nil)
 
 (provide '20-config-outli)
