90a7660aeb66333b1c5b9bf12e195a5a2989ee20
Luis Henriquez-Perez <luis@luishp.xyz>
2024-03-02 23:42:24 -0500

    Add ace-window config.

--- dot_config/emacs/lisp/98-init-features.el
+++ dot_config/emacs/lisp/98-init-features.el
@@ -63,6 +63,39 @@
 
 (opt! dired-recursive-copies 'always)
 (opt! dired-recursive-deletes 'always)
+;;;; ace-window
+;;;;; swap
+(set! aw-swap-invert t)
+;;;;; set the keys used by ace-window
+;; The character z conflicts.
+(set! aw-keys (eval-when-compile (string-to-list "jfkdlsaurieowncpqmxb")))
+;;;;; select a window with =w=, =j= or =o=
+;; There are commands such as.  I do not need these commands.  After moving left,
+;; right, up or down some direction once, the effort needed to traverse a window
+;; using directional window movement commands greatly increases.  The command
+;; [[file:snapshots/_helpful_command__ace-window][ace-window]] in contrast scales really well with a greater number of
+;; windows.  And it only loses slightly to directional window commands when moving
+;; one time.
+
+;; The command [[file:snapshots/_helpful_command__ace-window_.png][ace-window]] leverages [[https://github.com/abo-abo/avy][avy]] to select a window.  It assigns each window
+;; a character (I'm using [[][letters]] close to the homerow) which it displays on
+;; the upper right-hand corner of windows. I've found that
+;; ace-window is the quickest way possible to switch focus form one live window to
+;; another.
+
+;; The mnemonic bind is =w= and the quick bindings--which I will likely use most
+;; often--are =o= and =j=.
+(oo-bind 'oo-window-map "w" #'ace-window :wk "select")
+(oo-bind 'oo-window-map "j" #'ace-window :wk "select")
+(oo-bind 'oo-window-map "o" #'ace-window :wk "select")
+;;;;; swap two windows with =s=
+;; Often when I want to switch focus from my main window to one of its
+;; subsidiaries; I will want to swap buffers from the two windows.
+;; Actually, =edwina= does provide functions to do this: namely
+;; [[_helpful_command__edwina-swap-next-window_.png][edwina-swap-next-window]] and [[file:snapshots/_helpful_command__edwina-swap-previous-window_.png][edwina-swap-previous-window]].
+;; But I can do something similar, but much faster with.  This is a case where =s= is
+;; mnemonic and easy to press.
+(oo-bind 'oo-window-map "s" #'ace-swap-window :wk "swap")
 ;;;; magit
 (defmacro! defafter! (name expr &rest body)
   (declare (indent defun))
