684706f1ad79684e0c38cea11971d877a76d8640
Luis Henriquez-Perez <luis@luishp.xyz>
2024-03-18 12:59:27 -0400

    Update eshell configuration.

--- dot_config/emacs/lisp/20-config-eshell.el
+++ dot_config/emacs/lisp/20-config-eshell.el
@@ -41,6 +41,43 @@
 ;; For `eat-eshell-mode'.
 ;; (oo-add-hook 'eshell-load-hook #'eat-eshell-mode)
 ;;;;
+;;;; aliases
+;; https://olddeuteronomy.github.io/post/eshell-aliases-and-prompt/
+;; https://github.com/howardabrams/dot-files/blob/master/emacs-eshell.org#aliases
+;; (eshell/alias "e" "find-file $1")
+;; For now do this, but I just really want to scroll up.  I do not want to
+;; actually delete the buffer contents.  I mean I guess its O.K. since the
+;; contents should be saved in eshell-history, but its more secure to actuall
+;; have the physical buffer contents.
+(eshell/alias "clear" "eshell/clear t")
+(eshell/alias "ff" "find-file $1")
+;; (eshell/alias "emacs" "find-file $1")
+;; (eshell/alias "ee" "find-file-other-window $1")
+;; (eshell/alias "gd" "magit-diff-unstaged")
+;; (eshell/alias "gds" "magit-diff-staged")
+(eshell/alias "d" "dired $1")
+;;;; clear
+;; Unexpectedly for me the eshell clear scrolled to the bottom.  As seen in a
+;; stackoverflow answer as well as multiple blog posts, the solution is to use
+;; "clear 1" instead, essentually telling emacs to use "clear-scrollback".  I
+;; still do not like this though because it actually erases the contents of the
+;; buffer and I do not want to do this unnecessarily.  I just want it to scroll up.
+;; TODO: make into a snippet and/or abbrev
+;; (message "current buffer %S" (buffer-name))
+;; TODO: edit surrounding form so that it works in comments
+;; (message "var %S" var)
+;; (defun! eshell/scroll-to-top ()
+;;   ;; The function `recenter' does not seem to work in the eshell buffer.  I do
+;;   ;; not know why.
+;;   ;; (call-interactively #'recenter-top-bottom)
+;;   ;; (call-interactively #'recenter-top-bottom)
+;;   ;; Need a function of the number of lines in the window for the following to
+;;   ;; work.
+;;   ;; (set! line-number (line-number-at-pos))
+;;   ;; (goto-char (point-min))
+;;   ;; (forward-line (1- line-number))
+;;   )
+;; (eshell/alias "clear" )
 ;;; provide
 (provide '20-config-eshell)
 ;;; 20-config-eshell.el ends here
