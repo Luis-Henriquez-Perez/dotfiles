46f2ce1ecdcbddb4f3af1f6162fc428c42b12085
Luis Henriquez-Perez <luis@luishp.xyz>
2024-03-19 13:36:18 -0400

    Add a configuration file for corfu.

--- dot_config/emacs/lisp/98-init-features.el
+++ dot_config/emacs/lisp/98-init-features.el
@@ -196,50 +196,7 @@
 
 (oo-bind 'oo-miscellany-map "l" #'consult-bookmark)
 ;;;; corfu
-;; https://github.com/minad/corfu/issues/12
-(oo-bind 'corfu-map :ieg "<tab>"   #'corfu-next)
-(oo-bind 'corfu-map :ieg [backtab] #'corfu-previous)
-(oo-bind 'corfu-map :ieg "S-TAB"   #'corfu-previous)
-(oo-bind 'corfu-map :ieg "C-;"     #'corfu-quick-complete)
-(oo-bind 'corfu-map :ieg "C-j"     #'corfu-next)
-(oo-bind 'corfu-map :ieg "C-k"     #'corfu-previous)
-(oo-bind 'corfu-map :ieg "C-p"     #'corfu-previous)
-(oo-bind 'corfu-map :ieg ";"       #'corfu-quick-complete)
-(oo-bind 'corfu-map :ieg "SPC"     #'corfu-insert)
-
-;; When using evil, neither `corfu-map' nor `tempel-map' bindings will work
-;; because the maps are overridden by evil.  In order for them to work, we need
-;; to boost give the maps greater precedence.
-(defafter! make-corfu-map-an-overriding-map (evil corfu)
-  "Make `corfu-map' into an overriding map."
-  (evil-make-overriding-map corfu-map)
-  (advice-add 'corfu--setup :after 'evil-normalize-keymaps)
-  (advice-add 'corfu--teardown :after 'evil-normalize-keymaps))
-
-(defafter! make-tempel-map-an-overriding-map (evil tempel)
-  "Make `tempel-map' into an overriding map."
-  (evil-make-overriding-map tempel-map))
-
-(opt! corfu-quick1 "ajskdlghty")
-(opt! corfu-quick2 "ajskdlghty")
-
 (oo-add-hook 'prog-mode-hook #'corfu-mode)
-(oo-add-hook 'corfu-mode-hook #'corfu-history-mode)
-
-;; TODO: make it so moving on a candidate if I press espace insert that candidate.
-(opt! corfu-preview-current t)
-
-(opt! corfu-preselect-first t)
-
-(opt! corfu-quit-at-boundary nil)
-
-(opt! corfu-auto t)
-
-(opt! corfu-auto-delay 0.1)
-
-(opt! corfu-auto-prefix 1)
-
-(opt! corfu-bar-width 0)
 ;;;; dashboard
 (defhook! oo-initial-buffer-choice-hook&make-dashboard ()
   (when (require 'dashboard nil t)
