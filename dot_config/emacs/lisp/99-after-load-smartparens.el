a10a928f07fcfb62563cde2bed3e7da83baf1cfe
Luis Henriquez-Perez <luis@luishp.xyz>
2024-03-19 21:08:19 -0400

    Move smartparens code to after-load smartparens file.

--- dot_config/emacs/lisp/99-after-load-smartparens.el
+++ dot_config/emacs/lisp/99-after-load-smartparens.el
@@ -25,6 +25,20 @@
 ;; TODO: add commentary
 ;;
 ;;; Code:
+(opt! sp-highlight-wrap-tag-overlay nil)
+
+(opt! sp-highlight-pair-overlay nil)
+
+(opt! sp-highlight-wrap-overlay nil)
+
+(opt! sp-show-pair-delay 0.2)
+
+(defhook! minibuffer-setup-hook&enable-smartparens-maybe ()
+  "Enable `smartparens-mode' in the minibuffer."
+  (when (memq this-command '(eval-expression evil-ex))
+    (require 'smartparens)
+    (smartparens-strict-mode 1)))
+
 (sp-local-pair sp-lisp-modes "'" nil :actions nil)
 
 (sp-local-pair sp-lisp-modes "`" "'" :when '(sp-in-string-p sp-in-comment-p))
