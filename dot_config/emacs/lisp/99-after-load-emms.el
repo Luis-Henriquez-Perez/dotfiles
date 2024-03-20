e930a22e4f6663bdb99cb501a252519d09db876c
Luis Henriquez-Perez <luis@luishp.xyz>
2024-03-19 23:12:57 -0400

    Add new files.

---
+++ dot_config/emacs/lisp/99-after-load-emms.el
@@ -0,0 +1,5 @@
+;;;; emms
+(opt! emms-player-list '(emms-player-mpv))
+(opt! emms-source-file-default-directory (expand-file-name "Music/" "~/"))
+(opt! emms-directory (expand-file-name "emms/" oo-var-dir))
+;; (require 'emms-player-mpv)
