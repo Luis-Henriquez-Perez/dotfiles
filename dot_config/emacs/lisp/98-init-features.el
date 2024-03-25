21a5610ef6c1c8c77c57820fd7a64771d49e7e30
Luis Henriquez-Perez <luis@luishp.xyz>
2024-03-25 09:30:57 -0400

    Do not enable `cofu-mode` by default.

--- dot_config/emacs/lisp/98-init-features.el
+++ dot_config/emacs/lisp/98-init-features.el
@@ -98,7 +98,7 @@ For what buffer is displayed in the case of a boolean see
 (oo-add-hook 'prog-mode-hook 'auto-fill-mode)
 (oo-add-hook 'prog-mode-hook #'abbrev-mode)
 (oo-add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
-(oo-add-hook 'prog-mode-hook #'corfu-mode)
+;; (oo-add-hook 'prog-mode-hook #'corfu-mode)
 (oo-add-hook 'prog-mode-hook #'outli-mode)
 (oo-add-hook 'prog-mode-hook #'smartparens-mode)
 (oo-add-hook 'prog-mode-hook #'turn-on-show-smartparens-mode)
