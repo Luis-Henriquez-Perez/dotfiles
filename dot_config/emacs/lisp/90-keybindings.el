253e9d3ecd07da77b732f77497eca9c152797e99
Luis Henriquez-Perez <luis@luishp.xyz>
2024-03-25 10:16:53 -0400

    Bind the keys evilem motion keys in operator state as well.

--- dot_config/emacs/lisp/90-keybindings.el
+++ dot_config/emacs/lisp/90-keybindings.el
@@ -53,12 +53,12 @@
 ;; TODO: Do not use these bindings for keyboard macros where the originals might
 ;; be more useful.
 ;; Note: these are *not* the same as
-(oo-bind :nv "w" #'evilem-motion-beginning-of-word)
-(oo-bind :nv "W" #'evilem-motion-beginning-of-WORD)
-(oo-bind :nv "e" #'evilem-motion-end-of-word)
-(oo-bind :nv "E" #'evilem-motion-end-of-WORD)
-(oo-bind :nv "f" #'evilem-motion-char)
-(oo-bind :nv "b" #'evilem-motion-beginning-of-line)
+(oo-bind :nvo "w" #'evilem-motion-beginning-of-word)
+(oo-bind :nvo "W" #'evilem-motion-beginning-of-WORD)
+(oo-bind :nvo "e" #'evilem-motion-end-of-word)
+(oo-bind :nvo "E" #'evilem-motion-end-of-WORD)
+(oo-bind :nvo "f" #'evilem-motion-char)
+(oo-bind :nvo "b" #'evilem-motion-beginning-of-line)
 ;;;;; +/- increasing text-size
 (oo-bind :nm "+" #'text-scale-increase)
 (oo-bind :nm "-" #'text-scale-decrease)
