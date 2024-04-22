17bfc3ba4a1f63a748e32330428f9a824a53531d
Luis Henriquez-Perez <luis@luishp.xyz>
2024-04-22 04:48:53 -0400

    Add boilerplate code to gcmh.

--- dot_config/emacs/lisp/99-after-load-gcmh.el
+++ dot_config/emacs/lisp/99-after-load-gcmh.el
@@ -1,3 +1,32 @@
+;;; 99-after-load-gcmh.el --- TODO: add commentary -*- lexical-binding: t; -*-
+;;
+;; Copyright (c) 2024 Free Software Foundation, Inc.
+;;
+;; Author: Luis Henriquez-Perez <luis@luishp.xyz>
+;; Homepage: https://github.com/Luis-Henriquez-Perez/dotfiles/
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;; TODO: add commentary
+;;
+;;; Code:
+(require 'gcmh)
+
 (opt! gcmh-idle-delay 'auto)
 
 (opt! gcmh-high-cons-threshold (* 8 1024 1024))
@@ -11,16 +40,6 @@
 ;; keep things snappy I increase boost the [[helpvar:gc-cons-threshold][gc-cons-threshold]] just before I enter
 ;; the minibuffer, and restore it to it's original value a few seconds after it's closed.
 ;; (defvaralias 'minibuffer-enter-hook 'minibuffer-setup-hook)
-
-(defhook! minibuffer-setup-hook&increase-garbage-collection ()
-  "Boost garbage collection settings to `gcmh-high-cons-threshold"
-  [:depth 10]
-  (when (require 'gcmh nil t)
-    (setq gc-cons-threshold gcmh-high-cons-threshold)))
-
-(defhook! minibuffer-exit-hook&decrease-garbage-collection ()
-  "Reset garbage collection settings to `gcmh-low-cons-threshold'."
-  [:depth 90]
-  (require 'gcmh)
-  (when (require 'gcmh nil t)
-    (setq gc-cons-threshold gcmh-low-cons-threshold)))
+;;; provide
+(provide '99-after-load-gcmh)
+;;; 99-after-load-gcmh.el ends here
