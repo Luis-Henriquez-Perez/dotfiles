2af84917ecd9a565c815f48fa6ba10bdeea35a50
Luis Henriquez-Perez <luis@luishp.xyz>
2024-05-08 16:36:14 -0400

    Add a file for miscellaneous commands..

---
+++ dot_config/emacs/lisp/oo-ext-commands.el
@@ -0,0 +1,46 @@
+;;; oo-ext-commands.el --- TODO: add commentary -*- lexical-binding: t; -*-
+;;
+;; Copyright (c) 2024 Free Software Foundation, Inc.
+;;
+;; Author: Luis Henriquez-Perez <luis@luishp.xyz>
+;; Homepage: https://github.com/Luis-Henriquez-Perez/dotfiles/
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;; TODO: add commentary
+;;
+;;; Code:
+;;;; custom functions
+(defun! oo-set-font-face ()
+  "Apply an existing xfont to all graphical frames."
+  (interactive)
+  (set! font (completing-read "Choose font: " (x-list-fonts "*")))
+  (set-frame-font font nil t))
+
+(defun oo-split-window-right-and-focus ()
+  "Split window right and select the window created with the split."
+  (interactive)
+  (select-window (split-window-right)))
+
+(defun oo-split-window-below-and-focus ()
+  "Split window below and select the window created with the split."
+  (interactive)
+  (select-window (split-window-below)))
+;;; provide
+(provide 'oo-ext-commands)
+;;; oo-ext-commands.el ends here
