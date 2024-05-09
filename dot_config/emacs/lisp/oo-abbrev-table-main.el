11010db23fa4fca37ba58be14a58ab2b657e3941
Luis Henriquez-Perez <luis@luishp.xyz>
2024-05-09 07:52:28 -0400

    Thu May  9 07:52:28 2024
    M	dot_config/emacs/lisp/oo-abbrev-table-main.el

--- dot_config/emacs/lisp/oo-abbrev-table-main.el
+++ dot_config/emacs/lisp/oo-abbrev-table-main.el
@@ -1,4 +1,4 @@
-;;; oo-abbrev-table-main.el --- TODO: add commentary -*- lexical-binding: t; -*-
+;;; oo-abbrev-table-main.el -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (c) 2024 Free Software Foundation, Inc.
 ;;
