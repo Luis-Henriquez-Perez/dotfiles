0331dba07466607c02edd156ea8c339095364619
Luis Henriquez-Perez <luis@luishp.xyz>
2024-05-12 19:07:12 -0400

    Update abbrev table.

--- dot_config/emacs/lisp/oo-abbrev-table-main.el
+++ dot_config/emacs/lisp/oo-abbrev-table-main.el
@@ -28,7 +28,8 @@
 (require 'abbrev)
 
 (define-abbrev-table 'oo-abbrev-table-main
-  '(("arg" "argument")
+  '(("ques" "question")
+    ("arg" "argument")
     ("2" "two")
     ("equiv" "equivalent")
     ("mmend" "recommend")
