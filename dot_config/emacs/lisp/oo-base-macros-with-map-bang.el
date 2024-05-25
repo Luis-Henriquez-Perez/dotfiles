42b80060cc10a3703051262205dc9e490b1a5968
Luis Henriquez-Perez <luis@luishp.xyz>
2024-05-25 05:37:19 -0400

    Make name `oo-base-macros-with-map-bang`.

--- dot_config/emacs/lisp/oo-base-macros-with-map-bang.el
+++ dot_config/emacs/lisp/oo-base-macros-with-map-bang.el
@@ -1,4 +1,4 @@
-;;; oo-base-macros-with-map.el -*- lexical-binding: t; -*-
+;;; oo-base-macros-with-map-bang.el -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (c) 2024 Free Software Foundation, Inc.
 ;;
@@ -53,5 +53,5 @@ Collect symbols matching REGEXP in BODY into an alist."
   `(let* ,(oo--map-let-binds map body "!\\([^[:space:]]+\\)" nil)
      ,@body))
 ;;; provide
-(provide 'oo-base-macros-with-map)
-;;; oo-base-macros-with-map.el ends here
+(provide 'oo-base-macros-with-map-bang)
+;;; oo-base-macros-with-map-bang.el ends here
