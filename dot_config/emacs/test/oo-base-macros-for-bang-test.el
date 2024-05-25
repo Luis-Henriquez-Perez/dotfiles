6b97ceea0a5a87a3d95ab452519cdd9d84c7d7fb
Luis Henriquez-Perez <luis@luishp.xyz>
2024-05-25 08:04:34 -0400

    Update test.

--- dot_config/emacs/test/oo-base-macros-for-bang-test.el
+++ dot_config/emacs/test/oo-base-macros-for-bang-test.el
@@ -1,4 +1,4 @@
-;;; oo-base-macros-for-bang-test.el --- TODO: add commentary -*- lexical-binding: t; -*-
+;;; oo-base-macros-for-bang-test.el -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (c) 2024 Free Software Foundation, Inc.
 ;;
@@ -22,7 +22,7 @@
 ;;
 ;;; Commentary:
 ;;
-;; TODO: add commentary
+;; Test `oo-base-macros-for-bang'.
 ;;
 ;;; Code:
 (require 'ert)
