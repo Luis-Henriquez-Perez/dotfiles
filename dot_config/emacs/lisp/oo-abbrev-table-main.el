e11f6f43f1de59f7dc7df802a08891bb4ddb76c3
Luis Henriquez-Perez <luis@luishp.xyz>
2024-06-11 13:50:01 -0400

    Update abbrevs.

--- dot_config/emacs/lisp/oo-abbrev-table-main.el
+++ dot_config/emacs/lisp/oo-abbrev-table-main.el
@@ -28,7 +28,8 @@
 (require 'abbrev)
 
 (define-abbrev-table 'oo-abbrev-table-main
-  '(("sytle" "style")
+  '(("src" "source")
+    ("sytle" "style")
     ("elt" "element")
     ("elts" "elements")
     ("gens" "generates")
