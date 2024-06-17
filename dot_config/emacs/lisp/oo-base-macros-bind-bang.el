bef34def3571be79c59c441ab3dfb5d57d1a22a3
Luis Henriquez-Perez <luis@luishp.xyz>
2024-06-17 12:43:50 -0400

    Update bind macro.

--- dot_config/emacs/lisp/oo-base-macros-bind-bang.el
+++ dot_config/emacs/lisp/oo-base-macros-bind-bang.el
@@ -26,20 +26,35 @@
 ;;
 ;; I have coded and re-coded this macro countless times.  The last time I wrote
 ;; it as a function-based, but for performance ideally it would be macro based.
+;; Since getting a keybinding correct is so precise I have designed this as a
+;; set of functions that will be invoked recursively to generate a form.  The
+;; design is very similar to that of `use-package'.  The idea is to maximize
+;; flexibility while maintaining very modular and extensible code.  Unlike
+;; `use-package' bind! does not expand into all built-in forms.  That being
+;; said, this file is designed to be able to be compiled.
 ;;
 ;;; Code:
 (require 'seq)
 (require 'oo-base-macros)
+(require 'oo-base-lib)
+
 ;; The main data structure will be a metadata that I can access.  I will go through
 ;; it via `use-package' style recursion.
-(defvar oo--bind-steps '(oo--bind-step-defer-keymap
-                         oo--bind-step-evil-symbol
+(defvar oo--bind-steps '(oo--bind-step-evil-symbol
+                         oo--bind-step-evil-state
+                         oo--bind-step-defer-keymap
                          oo--bind-step-which-key
                          oo--bind-step-evil-bind
                          oo--bind-step-bind)
   "List of functions to be called one by one.")
 
+(defun oo--bind-generate-body (metadata steps)
+  "Return the body for `bind!'."
+  (and steps (funcall (car steps) metadata (cdr steps))))
+
 (defun! oo--bind-step-defer-keymap (metadata steps)
+  "Defer the evaluation of body until keymap is loaded.
+If METADATA has no keymap return."
   (with-map-keywords! metadata
     (if (or (not !!keymap) (equal !keymap 'global-map) (not (symbolp !keymap)))
         (oo--bind-generate-body metadata steps)
@@ -47,6 +62,7 @@
       `((oo-call-after-keymap ',!keymap (lambda () ,@body))))))
 
 (defun! oo--bind-step-evil-symbol (metadata steps)
+  "Return"
   (set! evil-symbol (map-elt metadata :evil-symbol))
   (nif! evil-symbol
       (oo--bind-generate-body metadata steps)
@@ -59,10 +75,13 @@
         (collecting! forms form)))))
 
 (defun oo--bind-step-evil-state (metadata steps)
+  "Wrap forms with"
   (with-map-keywords! metadata
     (nif! !!state
         (oo--bind-generate-body metadata steps)
-      `((oo-call-after-evil-state ',!state (lambda (_) ,@(oo--bind-generate-body metadata steps)))))))
+      (set! state (gensym "state"))
+      (set! metadata (map-insert metadata :state state))
+      `((oo-call-after-evil-state ',!state (lambda (,state) ,@(oo--bind-generate-body metadata steps)))))))
 
 ;; The function `which-key-add-keymap-based-replacements' already applies kbd to
 ;; the binding passed in.  This makes it tricky for me to use kbd because I need
@@ -82,81 +101,46 @@
           ,@(oo--bind-generate-body metadata steps))))))
 
 (defun oo--bind-step-evil-bind (metadata steps)
+  "Append form for defining evil binding."
   (with-map-keywords! metadata
     (nif! (and !!state !!keymap !!key !!def)
         (oo--bind-generate-body metadata steps)
       (-snoc (oo--bind-generate-body metadata steps)
-             `(evil-define-key* ',!state ,!keymap ,!key ,!def)))))
+             `(evil-define-key* ,!state ,!keymap ,!key ,!def)))))
 
 (defun oo--bind-step-bind (metadata steps)
+  ""
   (with-map-keywords! metadata
     (nif! (and !!keymap !!key !!def (not !!state))
         (oo--bind-generate-body metadata steps)
       (-snoc (oo--bind-generate-body metadata steps)
              `(define-key ,!keymap ,!key ,!def)))))
 
-;; (defun oo--bind-step-do-binding (metadata steps)
-;;   (with-map-keywords! metadata
-;;     (nif! (and !!definer !!definer-args)
-;;         (oo--bind-generate-body metadata steps)
-;;       (-snoc (oo--bind-generate-body metadata steps)
-;;              `(funcall #',!definer ,@!definer-args)))))
+;; (bind! :which-key "foo" :state normal :keymap global-map :key "d" :def #'foo)
+;; (bind! :which-key "foo" :state normal :keymap org-mode-map :key "d" :def #'foo)
+;; (bind! :which-key "foo" :evil-symbol nmi :keymap global-map :key "d" :def #'foo)
+;; (bind! :which-key "foo" :evil-symbol g :keymap global-map :key "d" :def #'foo)
 
-(defun oo--bind-generate-body (metadata steps)
-  "Return binding form."
-  (and steps (funcall (car steps) metadata (cdr steps))))
-
-(bind! :which-key "foo" :evil-state normal :keymap global-map :key "d" :def #'foo)
-(bind! :which-key "foo" :state normal :keymap org-mode-map :key "d" :def #'foo)
-(bind! :which-key "foo" :evil-symbol nmi :keymap global-map :key "d" :def #'foo)
-(bind! :which-key "foo" :evil-symbol g :keymap global-map :key "d" :def #'foo)
-;; (oo--bind-generate-body '(:keymap global-map :key "d" :def #'foo) '(oo--bind-step-bind))
-;; ((define-key global-map "d" #'foo))
-;; (oo--bind-step-bind '(:keymap global-map :key "d" :def #'foo) nil)
-(setq oo--bind-steps '(oo--bind-step-evil-symbol
-                       oo--bind-step-evil-state
-                       oo--bind-step-defer-keymap
-                       oo--bind-step-which-key
-                       oo--bind-step-evil-bind
-                       oo--bind-step-bind))
-
-(defmacro bind! (&rest args)
-  (macroexp-progn (oo--bind-generate-body args oo--bind-steps)))
-
-(defmacro! bind!! (&rest args)
-  "Bind KEY in KEYMAP to DEFINITION.
-This is the same as `define-key'.  If KEYMAP is not specified use `global-map'.
-If STATES is specified use evil binding.  If KEYMAP is a symbol."
-  (flet! plist-p (x) (or (null x) (keywordp (car x))))
-  ;; States is a list of symbols
-  (flet! states-p (x) (or (symbolp x) (and (listp x) (-all-p #'symbolp x))))
-  (flet! map-symbol-p (x) (and (symbolp x)
-                               (not (keywordp x))
-                               (string-match-p "-map\\'" (symbol-name x))))
-  (flet! keymap-p (-orfn #'keymapp #'map-symbol-p))
-  (flet! key-p (-orfn #'stringp #'vectorp))
+;; I had been considering looping through this, but for now this is fine.
+(defun! oo--standardize-args (args)
+  "Standardize arguments ARGS for `bind!'."
+  (flet! esym (keyword) (intern (seq-rest (symbol-name keyword))))
+  (flet! sharp-quoted-p (obj) (equal 'function (car-safe obj)))
   (pcase args
-    ;; Just passing in key and def, implies `global-map'.
-    (`(:alt ,key ,def . ,(and (pred plist-p) metadata))
-     (list :alt key def)
-     (apply #'oo--bind :alt t :key key :def def metadata))
-    (`(,(and (pred key-p) key) ,def . ,(and (pred plist-p) metadata))
-     `(bind! :keymap global-map :key )
-     (apply #'oo--bind :keymap global-map :key key :def def metadata))
-    (`(,(and (pred keymap-p) keymap) ,(and (pred key-p) key) ,def . ,(and (pred plist-p) metadata))
-     (apply #'oo--bind :keymap keymap :key key :def def metadata))
-    (`(,(and (pred keymap-p) keymap) ,(and (pred keywordp) state-key) ,(and (pred key-p) key) ,def . ,(and (pred plist-p) metadata))
-     (apply #'oo--bind :state-key state-key :keymap keymap :key key :def def metadata))
-    (`(,(and (pred keywordp) state-key) ,(and (pred keymap-p) keymap) ,(and (pred key-p) key) ,def . ,(and (pred plist-p) metadata))
-     (apply #'oo--bind :state-key state-key :keymap keymap :key key :def def metadata))
-    (`(,(and (pred keywordp) state-key) ,(and (pred key-p) key) ,def . ,(and (pred plist-p) metadata))
-     (apply #'oo--bind :state-key state-key :keymap 'global-map :key key :def def metadata))
-    (`(,(and (pred states-p) states) ,(and (pred keymap-p) keymap) ,(and (pred key-p) key) ,def . ,(and (pred plist-p) metadata))
-     (apply #'oo--bind :states states :keymap keymap :key key :def def metadata))
-    (`(,(and (pred states-p) states) ,(and (pred key-p) key) ,def . ,(and (pred plist-p) metadata))
-     (apply #'oo--bind :states states :keymap 'global-map :key key :def def metadata))
-    (_
-     (error "unknown binding syntax"))))
+    (`(,(and (pred symbolp) (pred (not keywordp)) keymap) ,key ,(and (pred sharp-quoted-p) def) . ,rest)
+     (cl-list* :keymap keymap :key key :def def rest))
+    (`(,(and (pred keywordp) evil-keyword) ,key ,(and (pred sharp-quoted-p) def) . ,rest)
+     (cl-list* :keymap 'global-map :evil-symbol (esym evil-keyword) :key key
+               :def def rest))
+    (`(,(and (pred symbolp) keymap) ,(and (pred keywordp) evil-keyword) ,key
+       ,(and (pred sharp-quoted-p) def) . ,rest)
+     (cl-list* :keymap keymap :evil-symbol (esym evil-keyword) :key key :def def rest))
+    (`(,key ,(and (pred sharp-quoted-p) def) . ,rest)
+     (cl-list* :key key :def def rest))
+    (_ nil)))
+
+(defmacro! bind! (&rest args)
+  (macroexp-progn (oo--bind-generate-body (oo--standardize-args args) oo--bind-steps)))
 ;;; provide
 (provide 'oo-base-macros-bind-bang)
 ;;; oo-base-macros-bind-bang.el ends here
