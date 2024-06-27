55e972b5c19fef0fd284578678556e916a5bd461
Luis Henriquez-Perez <luis@luishp.xyz>
2024-06-27 03:20:17 -0400

    Use opt! macro for dashboard.

--- dot_config/emacs/lisp/oo-init-dashboard.el
+++ dot_config/emacs/lisp/oo-init-dashboard.el
@@ -37,17 +37,17 @@
 (defun oo-dashboard-init-info (&rest _)
   (format "Emacs started in %.2f seconds" (string-to-number (emacs-init-time))))
 
-(setq dashboard-init-info #'oo-dashboard-init-info)
+(opt! dashboard-init-info #'oo-dashboard-init-info)
 
-(setq dashboard-banner-logo-title "Welcome!")
+(opt! dashboard-banner-logo-title "Welcome!")
 
-(setq dashboard-set-footer nil)
+(opt! dashboard-set-footer nil)
 
-(setq dashboard-items nil)
+(opt! dashboard-items nil)
 
-(setq dashboard-startup-banner (seq-random-elt (if (display-graphic-p) '(official logo) '(1 2 3))))
+(opt! dashboard-startup-banner (seq-random-elt (if (display-graphic-p) '(official logo) '(1 2 3))))
 
-(setq dashboard-center-content t)
+(opt! dashboard-center-content t)
 ;;; provide
 (provide 'oo-init-dashboard)
 ;;; oo-init-dashboard.el ends here
