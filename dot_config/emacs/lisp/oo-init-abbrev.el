c7a33caedb11702bfd9fb6db59586bcc5502190c
Luis Henriquez-Perez <luis@luishp.xyz>
2024-07-04 16:00:01 -0400

    Auto add period after two spaces via abbrev-mode.

--- dot_config/emacs/lisp/oo-init-abbrev.el
+++ dot_config/emacs/lisp/oo-init-abbrev.el
@@ -43,6 +43,26 @@ string or comment."
 
 (alet (-snoc (abbrev-table-get global-abbrev-table :parents) oo-abbrev-table-main oo-abbrev-table-wikipedia-misspellings)
   (abbrev-table-put global-abbrev-table :parents it))
+;;;;; automatically add period
+;; I do not like manually adding periods to the end of sentences.  Having moved
+;; from using one space after a sentence to two, I find it particularl daunting
+;; to type period, space, space whenever I am ending one sentence and starting a
+;; new one.  With this customization when I type space, space, following a word
+;; it is converted into period space space.  Additionally, if I end a sentence
+;; line with two spaces and I press ESC, the trailing two spaces are replaced
+;; with a period.
+(defadvice! abbrev--default-expand@ARauto-add-periods (expand-fn)
+  "Add a period when necessary."
+  (prog1 (funcall expand-fn)
+    (when (or (derived-mode-p 'text-mode) (oo-in-string-or-comment-p))
+      (set! eol (line-beginning-position -1))
+      (set! rx "\\([[:word:]]\\)\\([[:space:]][[:space:]]\\)\\([^[:space:]]+\\)")
+      (cond ((looking-back rx eol)
+             (replace-match "\\1.\\2\\3" nil nil nil 0))
+            ((looking-back "\\([[:word:]]\\)[[:space:]]\\{2,\\}" eol)
+             (replace-match "\\1."))))))
+;; The behavior I want is if I type two spaces then replace with period
+;; followed by two spaces.
 ;;; provide
 (provide 'oo-init-abbrev)
 ;;; oo-init-abbrev.el ends here
