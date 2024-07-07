60bb19ec192cbfc3cc0905390c06b1b8a92d2b9d
Luis Henriquez-Perez <luis@luishp.xyz>
2024-07-07 19:56:42 -0400

    Use `pop-to-buffer` which selects the buffer.

--- dot_config/emacs/lisp/oo-init-keybindings.el
+++ dot_config/emacs/lisp/oo-init-keybindings.el
@@ -190,7 +190,7 @@
 (bind! oo-find-map "n" #'oo-dwim-narrow)
 (bind! oo-find-map "o" #'find-file)
 (bind! oo-find-map "f" #'switch-to-buffer)
-(bind! oo-find-map "d" #'display-buffer)
+(bind! oo-find-map "d" #'pop-to-buffer)
 
 ;; (bind! oo-find-map "i" #'oo-open-emacs-init-file)
 ;; (bind! oo-find-map "e" #'oo-open-emacs-directory)
