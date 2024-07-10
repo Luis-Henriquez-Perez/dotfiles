c590381ea175288c5cf9a0d0d1f5b6deb86c8228
Luis Henriquez-Perez <luis@luishp.xyz>
2024-07-10 19:55:42 -0400

    Prevent compiler warnings.

--- dot_config/emacs/lisp/oo-init-keybindings.el
+++ dot_config/emacs/lisp/oo-init-keybindings.el
@@ -297,15 +297,16 @@
 (alt! describe-command helpful-command helpful)
 (alt! describe-variable helpful-variable helpful)
 (alt! describe-key helpful-key helpful)
-;;;;;; vertico 
+;;;;;; vertico
 (with-eval-after-load 'vertico
-  (define-key vertico-map "TAB" #'vertico-next)
-  (define-key vertico-map "C-k" #'vertico-previous)
-  (define-key vertico-map "C-j" #'vertico-next)
-  (define-key vertico-map ";" #'vertico-quick-exit)
-  (define-key vertico-map "C-;" #'vertico-quick-exit)
-  (define-key vertico-map [backtab] #'vertico-previous)
-  (define-key vertico-map "C-o" #'embark-act))
+  (with-no-warnings
+    (define-key vertico-map "TAB" #'vertico-next)
+    (define-key vertico-map "C-k" #'vertico-previous)
+    (define-key vertico-map "C-j" #'vertico-next)
+    (define-key vertico-map ";" #'vertico-quick-exit)
+    (define-key vertico-map "C-;" #'vertico-quick-exit)
+    (define-key vertico-map [backtab] #'vertico-previous)
+    (define-key vertico-map "C-o" #'embark-act)))
 ;;;;;; consult
 (alt! switch-to-buffer consult-buffer consult)
 (alt! yank-pop consult-yank-pop consult)
