2337ad584c2216cf8693a7c39898019265a412f9
Luis Henriquez-Perez <luis@luishp.xyz>
2024-07-20 12:54:31 -0400

    Add more abbreviations.

--- dot_config/emacs/lisp/oo-abbrev-table-main.el
+++ dot_config/emacs/lisp/oo-abbrev-table-main.el
@@ -28,7 +28,8 @@
 (require 'abbrev)
 
 (define-abbrev-table 'oo-abbrev-table-main
-  '(("bytecomp" "byte-compilation")
+  '(("advoid" "avoid")
+    ("bytecomp" "byte-compilation")
     ("dupl" "duplicate")
     ("err" "error")
     ("repeateadly" "repeatedly")
