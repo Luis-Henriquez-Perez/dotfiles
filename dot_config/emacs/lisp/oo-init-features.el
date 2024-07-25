ae57e4ad5ac7498279ba05f82b79ce3fa90dbedc
Luis Henriquez-Perez <luis@luishp.xyz>
2024-07-25 15:37:52 -0400

    Thu Jul 25 15:37:52 2024
    D	dot_config/emacs/lisp/oo-after-load-outline.el
    M	dot_config/emacs/lisp/oo-init-features.el

--- dot_config/emacs/lisp/oo-init-features.el
+++ dot_config/emacs/lisp/oo-init-features.el
@@ -51,6 +51,15 @@
 (defhook! text-mode-hook&set-captain-local-vars ()
   (setq-local captain-predicate #'always)
   (setq-local captain-sentence-start-function #'captain--default-sentence-start))
+;;;; chezmoi
+;; I need the command to write the source from the target.  The command
+;; =chezmoi-apply= does this but I would like it to do it automatically if I am
+;; already editing a target-file.
+(defhook! after-save-hook&chezmoi-write-maybe (&rest _)
+  (when (aand (require 'chezmoi nil t)
+              (buffer-file-name)
+              (chezmoi-target-file it))
+    (with-demoted-errors "error:%S" (chezmoi-write))))
 ;;;; consult
 (opt! consult-preview-key nil)
 (opt! consult-fontify-preserve nil)
@@ -95,15 +104,6 @@
 (opt! corfu-quick1 "ajskdlghty")
 (opt! corfu-quick2 "ajskdlghty")
 ;; https://github.com/minad/corfu/issues/12
-;;;; chezmoi
-;; I need the command to write the source from the target.  The command
-;; =chezmoi-apply= does this but I would like it to do it automatically if I am
-;; already editing a target-file.
-(defhook! after-save-hook&chezmoi-write-maybe (&rest _)
-  (when (aand (require 'chezmoi nil t)
-              (buffer-file-name)
-              (chezmoi-target-file it))
-    (with-demoted-errors "error:%S" (chezmoi-write))))
 ;;;; dashboard
 (require 'dashboard)
 
