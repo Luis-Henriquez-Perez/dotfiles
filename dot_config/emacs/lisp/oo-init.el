f93120b04199b9c132a8582c8041fde7ff207a97
Luis Henriquez-Perez <luis@luishp.xyz>
2024-07-27 18:24:33 -0400

    Update.

--- dot_config/emacs/lisp/oo-init.el
+++ dot_config/emacs/lisp/oo-init.el
@@ -26,24 +26,6 @@
 ;;
 ;;; Code:
 ;;;; requirements
-(require 'oo-base)
-(require 'oo-init-keybindings)
-;;;; disable old themes before enabling new ones
-;; We end up with remants of the faces of old themes when we load a new
-;; one.  For this reason, I make sure to disable any enabled themes before applying
-;; a new theme.
-
-;; When you load a theme you'll end up with quite a surprise.  And it
-;; stacks as well when working on a big configuration change I didn't
-;; have this code and I could literally backtrack the themes.
-
-;; Don't know why deleting the previous theme before enabling a new
-;; one isn't the default behavior.  When would anyone want to layer
-;; the colors of one theme on top of an older one.
-(defadvice! load-theme@ARdisable-old-themes (orig-fn &rest args)
-  "Disable old themes before loading new ones."
-  (mapc #'disable-theme custom-enabled-themes)
-  (apply orig-fn args))
 ;;;; set initial font
 (alet (or (font-spec :name "Martian Mono Nerd Font"
                      :weight 'normal
