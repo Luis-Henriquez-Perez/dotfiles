7e954f1252632c49d939abe9e010d55c3fb65757
Luis Henriquez-Perez <luis@luishp.xyz>
2024-07-29 03:12:45 -0400

    Add more abbreviations.

--- dot_config/emacs/lisp/oo-abbrev-table-main.el
+++ dot_config/emacs/lisp/oo-abbrev-table-main.el
@@ -28,7 +28,8 @@
 (require 'abbrev)
 
 (define-abbrev-table 'oo-abbrev-table-main
-  '(("shoud" "should")
+  '(("reqs" "requirements")
+    ("shoud" "should")
     ("upate" "update")
     ("autolaods" "autoloads")
     ("wndow" "window")
