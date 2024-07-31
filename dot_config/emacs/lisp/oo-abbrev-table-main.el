46ef848e7ba11da994ba845646619bb73aedec06
Luis Henriquez-Perez <luis@luishp.xyz>
2024-07-31 08:56:13 -0400

    Add more abbreviations.

--- dot_config/emacs/lisp/oo-abbrev-table-main.el
+++ dot_config/emacs/lisp/oo-abbrev-table-main.el
@@ -28,7 +28,8 @@
 (require 'abbrev)
 
 (define-abbrev-table 'oo-abbrev-table-main
-  '(("orners" "corners")
+  '(("cnfig" "configuration")
+    ("orners" "corners")
     ("sometiems" "sometimes")
     ("abstractioins" "abstractions")
     ("capt" "captain")
