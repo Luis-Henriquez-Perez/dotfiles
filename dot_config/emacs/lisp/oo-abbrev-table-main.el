5864eb71cc5bd8bdbbc5344dddd4ba7f867cb5c3
Luis Henriquez-Perez <luis@luishp.xyz>
2024-08-01 15:26:14 -0400

    Add more abbreviations.

--- dot_config/emacs/lisp/oo-abbrev-table-main.el
+++ dot_config/emacs/lisp/oo-abbrev-table-main.el
@@ -28,7 +28,8 @@
 (require 'abbrev)
 
 (define-abbrev-table 'oo-abbrev-table-main
-  '(("deps" "dependencies")
+  '(("rnadom" "random")
+    ("deps" "dependencies")
     ("req" "requirement")
     ("udate" "update")
     ("cnfig" "configuration")
