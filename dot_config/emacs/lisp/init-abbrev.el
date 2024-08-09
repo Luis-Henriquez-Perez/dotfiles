e582704e28a0e193e928dbc4ab8e8ead52c8739f
Luis Henriquez-Perez <luis@luishp.xyz>
2024-08-08 22:19:13 -0400

    Update init-abbreviation.

--- dot_config/emacs/lisp/init-abbrev.el
+++ dot_config/emacs/lisp/init-abbrev.el
@@ -1,4 +1,4 @@
-;;; oo-init-abbrevs.el --- abbrev configuration -*- lexical-binding: t; -*-
+;;; init-abbrev.el --- initialize abbrev-mode -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (c) 2024 Free Software Foundation, Inc.
 ;;
@@ -22,47 +22,26 @@
 ;;
 ;;; Commentary:
 ;;
-;; This is my configuration for abbrev.
+;; Initialize abbrev-mode.
 ;;
 ;;; Code:
-(require 'abbrev)
 (require 'base)
-(require 'oo-abbrev-table-main)
-(require 'oo-abbrev-table-wikipedia-misspellings)
-;;;; abbrevs
-;;;;; set abbrevs my way
-(defun oo-enable-text-abbrev-p ()
-  "Return non-nil when text abbrevs should be enabled.
-This is when the current major-mode is derived from text-mode or point is in a
-string or comment."
-  (or (derived-mode-p 'text-mode)
-      (oo-in-string-or-comment-p)))
-;;;;; all abbrevs
-(abbrev-table-put oo-abbrev-table-main :enable-function #'oo-enable-text-abbrev-p)
-(abbrev-table-put oo-abbrev-table-wikipedia-misspellings :enable-function #'oo-enable-text-abbrev-p)
-
-(alet (-snoc (abbrev-table-get global-abbrev-table :parents) oo-abbrev-table-main oo-abbrev-table-wikipedia-misspellings)
-  (abbrev-table-put global-abbrev-table :parents it))
-;;;;; automatically add period
-;; I do not like manually adding periods to the end of sentences.  Having moved
-;; from using one space after a sentence to two, I find it particularl daunting
-;; to type period, space, space whenever I am ending one sentence and starting a
-;; new one.  With this customization when I type space, space, following a word
-;; it is converted into period space space.  Additionally, if I end a sentence
-;; line with two spaces and I press ESC, the trailing two spaces are replaced
-;; with a period.
-(defadvice! auto-add-periods (around abbrev--default-expand expand-fn)
-  "Add a period when necessary."
-  (prog1 (funcall expand-fn)
-    (when (or (derived-mode-p 'text-mode) (oo-in-string-or-comment-p))
-      (set! eol (line-beginning-position -1))
-      (set! rx "\\([[:word:]]\\)\\([[:space:]][[:space:]]\\)\\([^[:space:]]+\\)")
-      (cond ((looking-back rx eol)
-             (replace-match "\\1.\\2\\3" nil nil nil 0))
-            ((looking-back "\\([[:word:]]\\)[[:space:]]\\{2,\\}" eol)
-             (replace-match "\\1."))))))
-;; The behavior I want is if I type two spaces then replace with period
-;; followed by two spaces.
+;;;; hooks
+(oo-add-hook 'prog-mode-hook #'abbrev-mode)
+(oo-add-hook 'text-mode-hook #'abbrev-mode)
+;; Emacs loads abbreviation-mode automatically so instead of using something
+;; like `with-eval-after-load' I am loading it in `abbrev-mode-hook'.
+(defhook! require-abbrev-config (abbrev-mode-hook)
+  (require 'config-abbrev))
+;;;; bindings
+(bind! "C-c j" #'oo-add-new-abbrev)
+(bind! "C-c k" #'unexpand-abbrev)
+(autoload #'oo-add-new-abbrev "oo-commands" nil t 'function)
+;;;; do not save abbrevs to a file
+(oo-add-advice #'read-abbrev-file :around #'ignore)
+(oo-add-advice #'write-abbrev-file :around #'ignore)
+(oo-add-advice #'abbrev--possibly-save :around #'ignore)
+(oo-add-advice #'quietly-read-abbrev-file :around #'ignore)
 ;;; provide
 (provide 'init-abbrev)
 ;;; init-abbrev.el ends here
