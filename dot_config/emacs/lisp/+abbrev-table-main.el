95031b189c5e971fd0e72e4943299804ff70e7c9
Luis Henriquez-Perez <luis@luishp.xyz>
2024-08-11 06:42:54 -0400

    Add new abbreviation.

--- dot_config/emacs/lisp/+abbrev-table-main.el
+++ dot_config/emacs/lisp/+abbrev-table-main.el
@@ -28,7 +28,8 @@
 (require 'abbrev)
 
 (define-abbrev-table '+abbrev-table-main
-  '(("ppress" "press")
+  '(("commentar" "commentary")
+	("ppress" "press")
 	("keybs" "keybindings")
 	("reqed" "required")
 	("examinging" "examining")
