dc38a81e2cfabeea5a549f7aee6e5d1910e0accc
Luis Henriquez-Perez <luis@luishp.xyz>
2024-08-12 11:55:03 -0400

    Only enable lispyville in emacs-lisp-mode.

--- dot_config/emacs/lisp/init-lispyville.el
+++ dot_config/emacs/lisp/init-lispyville.el
@@ -28,7 +28,7 @@
 (require 'base)
 
 ;; Do not bind any keys by default.
-(hook! prog-mode-hook lispyville-mode)
+(hook! emacs-lisp-mode-hook lispyville-mode)
 (oo-add-advice #'lispyville-normal-state :after #'@exit-everything)
 (opt! lispyville-key-theme nil)
 
