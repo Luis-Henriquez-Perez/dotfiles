456e82e414a583d02129341a492957b576eb8892
Luis Henriquez-Perez <luis@luishp.xyz>
2024-08-15 00:25:34 -0400

    Add new abbreviations.

--- dot_config/emacs/lisp/+abbrev-table-main.el
+++ dot_config/emacs/lisp/+abbrev-table-main.el
@@ -28,7 +28,8 @@
 (require 'abbrev)
 
 (define-abbrev-table '+abbrev-table-main
-  '(("advisig" "advising")
+  '(("wa" "way")
+	("advisig" "advising")
 	("inadvertedly" "inadvertently")
 	("aamof" "as a matter of fact")
 	("eamcs" "emacs")
