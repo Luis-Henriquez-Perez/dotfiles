e96afbaf958751e0be217412e3140811a1e96f6d
Luis Henriquez-Perez <luis@luishp.xyz>
2024-08-25 19:29:19 -0400

    Add another abbrev.

--- dot_config/emacs/lisp/+abbrev-plain-text-abbrevs.el
+++ dot_config/emacs/lisp/+abbrev-plain-text-abbrevs.el
@@ -32,6 +32,7 @@
 (require '+abbrev-enable-functions)
 (require '+abbrev-wikipedia-typo-abbrevs)
 
+(define-abbrev global-abbrev-table "remov"          "remove"                       nil :enable-function #'+abbrev-enable-plain-text-abbrevs-p)
 (define-abbrev global-abbrev-table "iddnt"          "did not"                       nil :enable-function #'+abbrev-enable-plain-text-abbrevs-p)
 (define-abbrev global-abbrev-table "expressoins"    "expressions"                   nil :enable-function #'+abbrev-enable-plain-text-abbrevs-p)
 (define-abbrev global-abbrev-table "regexes"        "regular expressions"           nil :enable-function #'+abbrev-enable-plain-text-abbrevs-p)
