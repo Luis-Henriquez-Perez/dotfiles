2d4f6744aa88227f94a4d777fbd9592d8b2816ba
Luis Henriquez-Perez <luis@luishp.xyz>
2024-11-16 10:52:42 -0500

    Sat Nov 16 10:52:41 2024
    M	dot_config/emacs/lisp/base-bind.el
    M	dot_config/emacs/test/base-bind-test.el

--- dot_config/emacs/test/base-bind-test.el
+++ dot_config/emacs/test/base-bind-test.el
@@ -66,10 +66,10 @@
   (should (equal '(oo-kbd :states '(?n ?m ?v) :keymap org-mode-map :key "d" :def #'foo)
                  (oo--kbd-forms '((n m v) org-mode-map "d" #'foo))))
   ;; 13 (bind! (normal motion visual) org-mode-map "d" #'foo)
-  (should (equal `(oo-kbd :states ',states :keymap ,keymap :key ,key :def ,def)
+  (should (equal '(oo-kbd :states '(normal motion visual) :keymap org-mode-map :key "d" :def #'foo)
                  (oo--kbd-forms '((normal motion visual) org-mode-map "d" #'foo))))
   ;; 14 (bind! "d" #'foo)
-  (should (equal `(oo-kbd :states ',states :keymap ,keymap :key ,key :def ,def)
+  (should (equal '(oo-kbd :states ',states :keymap ,keymap :key ,key :def ,def)
                  (oo--kbd-forms '(org-mode-map (normal motion visual) "d" #'foo)))))
 ;;; provide
 (provide 'base-bind-test)
