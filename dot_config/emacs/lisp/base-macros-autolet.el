205fa3f5805e94842f706f735e521f0e46df627d
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-01 12:06:58 -0500

    dot_config/emacs/lisp/base-macros-autolet.el

--- dot_config/emacs/lisp/base-macros-autolet.el
+++ dot_config/emacs/lisp/base-macros-autolet.el
@@ -232,9 +232,9 @@ SETTER, KEY, TEST, TEST-NOT are the same as in `adjoining!'."
              (t
               (push `(,symbol nil) bindings)))
            (setf (caar stack) t))
-          (`((nil ,fn (,(and loop (pred loop-symbol-p)) ,pred . ,(and body (guard t)))) . ,(guard t))
+          (`((nil ,fn (,(and loop (pred loop-symbol-p)) ,pred . ,(and rest (guard t)))) . ,(guard t))
            (setq stack (cdr stack))
-           (push (list nil fn body) stack)
+           (push (list nil fn rest) stack)
            (push (list nil (loop-join-fn loop pred) nil) stack))
           (`((nil ,_ (set! ,pattern ,_ . ,(guard t))) . ,(guard t))
            (if (symbolp pattern)
