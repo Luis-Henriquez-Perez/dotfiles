4459f4fc724b78367dae05b5cf810ed34058302c
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-01 04:49:25 -0500

    dot_config/emacs/lisp/base-macros-autolet.el

--- dot_config/emacs/lisp/base-macros-autolet.el
+++ dot_config/emacs/lisp/base-macros-autolet.el
@@ -166,6 +166,8 @@ SETTER, KEY, TEST, TEST-NOT are the same as in `adjoining!'."
                  (pop ,sym))
                 (`(:init (,(pred symbolp) . ,(guard t)) . ,(guard t))
                  (push (list (pop ,sym) nil) ,inits))
+                (`(:init (((,(pred symbolp))) . ,(guard t)) . ,(guard t))
+                 (push (list (pop ,sym) nil) ,inits))
                 (`(:init))
                 ((setq ,inits (append ,inits (pop ,sym))))
                 (:noinit
