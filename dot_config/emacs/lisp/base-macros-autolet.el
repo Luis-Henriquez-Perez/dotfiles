943fa6c2925318e9d0fe675f1746577fba98baef
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-03 09:10:30 -0500

    dot_config/emacs/lisp/base-macros-autolet.el

--- dot_config/emacs/lisp/base-macros-autolet.el
+++ dot_config/emacs/lisp/base-macros-autolet.el
@@ -42,11 +42,13 @@
 (require 'base-macros-lef)
 ;;;; control flow macros
 (defmacro return! (&optional value)
-  "Exit `autolet!' and return VALUE."
+  "Exit `autolet!' and return VALUE.
+Throw a `return!' signal, immediately terminating the `autolet!' execution and
+returning VALUE from the surrounding context."
   `(throw 'return! ,value))
 
 (defmacro done! ()
-  "Same as `return!' but always throw nil.
+  "A shorthand for `(return! nil)'.
 This is meant to be used within an `autolet!' body."
   `(return! nil))
 
@@ -56,20 +58,23 @@ This is meant to be used inside of loop from within an `autolet!' body."
   `(throw 'break! ,value))
 
 (defmacro continue! ()
-  "Skip the current iteration of loop."
+  "Skip the current iteration of loop.
+Throw a `continue!' signal to end the current iteration and move to the next.
+Must be used within an `autolet!' body."
   `(throw 'continue! nil))
+
 (defalias 'skip! 'continue!)
 ;;;; let-binding stubs
-;; This wrapper is mainly for.
 (defmacro stub! (name args &rest body)
-  "Define a local function definition with `cl-flet'.
-NAME, ARGS and BODY are the same as in `defun'.
-Must be used in `autolet!'."
+  "Indicator for defining local functions via `cl-flet' in `autolet!' forms."
   (declare (indent defun))
   (ignore name args body))
-(defalias 'flet! 'stub!)
-(defalias 'noflet! 'stub!)
-(defalias 'nflet! 'stub!)
+(defalias 'macrolet! 'stub! "Indicator for defining local macros via
+`cl-macrolet' in `autolet!' forms.")
+(defalias 'mlet! 'macrolet!)
+(defalias 'flet! 'stub! "Same as `stub!'.")
+(defalias 'noflet! 'stub! "Indicator for defining.")
+(defalias 'nflet! 'stub! "Same as `noflet!'")
 ;;;; accumulation macros
 (cl-defmacro appending! (place list &key (setter 'setf))
   "Append LIST to the end of PLACE.
@@ -153,110 +158,130 @@ SETTER, KEY, TEST, TEST-NOT are the same as in `adjoining!'."
 (defalias 'summing! 'cl-incf)
 (defalias 'subtracting! 'cl-decf)
 (defalias 'minusing! 'cl-decf)
+;;;; helpers
+(defmacro oo--autolet-inits (bodysym)
+  "Produce a list of init and noinits from BODYSYM.
+Extract initialization and exclusion bindings from BODYSYM.
+
+This macro processes BODYSYM to identify `:init` and `:noinit` forms.
+- `:init BINDINGS`: A list of symbol bindings, formatted like `let*`.
+- `:noinit SYMS`: A list of symbols to exclude from let-bindings."
+  (let ((inits (gensym "inits"))
+        (noinits (gensym "noinits"))
+        (letbind (gensym "letbind")))
+    `(let (,inits ,noinits)
+       (while (pcase ,bodysym
+                (`(:init ,(pred listp) . ,(guard t))
+                 (pop ,bodysym)
+                 (dolist (,letbind (pop ,bodysym))
+                   (pcase ,letbind
+                     ((pred symbolp)
+                      (push (list ,letbind nil) ,inits))
+                     (`(,_)
+                      (push (append ,letbind (list nil)) ,inits))
+                     (`(,_ ,_)
+                      (push ,letbind ,inits))))
+                 t)
+                (`(:noinit ,(pred listp) . ,(guard t))
+                 (pop ,bodysym)
+                 (setq ,noinits (append ,noinits (pop ,bodysym)))
+                 t)))
+       (list ,inits ,noinits))))
+
+(defun oo--autolet-data (body)
+  "Return let-bindings and processed forms used in `autolet!'.
+Identify and collect symbols needed for let-binding."
+  (pcase-let ((`(,init ,noinit) (oo--autolet-inits body))
+              (bindings nil)
+              (lets '((mlet! . cl-macrolet)
+                      (macrolet! . cl-macrolet)
+                      (nflet! . lef!)
+                      (noflet! . lef!)
+                      (flet! . cl-flet)
+                      (stub! . cl-flet)
+                      (label! . cl-labels)
+                      (labels! . cl-labels))))
+    (cl-flet ((quote-symbol-p (x) (memq x '(quote function backquote cl-function)))
+              (loop-symbol-p (x) (memq x '(while dolist dotimes for! dolist!)))
+              (ing-symbol-p (x) (and (symbolp x) (string-match-p "ing!$" (symbol-name x))))
+              (letbind-symbol-p (x) (assoc x lets))
+              (should-remove-p (x) (or (member (car x) noinit) (assoc (car x) init))))
+      (cl-labels ((process-form (form)
+                    (pcase form
+                      ;; Leave quoted forms as-is.
+                      (`(,(pred quote-symbol-p) . ,_)
+                       form)
+                      ;; Match `(set! VAR VALUE)` and collect VARIABLE.
+                      (`(set! ,pattern ,_ . ,(guard t))
+                       (if (symbolp pattern)
+                           (cl-pushnew (list pattern nil) bindings :key #'car)
+                         (dolist (symbol (reverse (oo--set-flatten pattern)))
+                           (cl-pushnew (list symbol nil) bindings :key #'car)))
+                       form)
+                      ;; Surround loops with a catch.
+                      (`(,(and loop (pred loop-symbol-p)) ,pred . ,(and body (guard t)))
+                       `(catch 'break! (,loop ,pred (catch 'continue! ,@(process-form body)))))
+                      ;; Properly initialize variables in ingmacro declarations.
+                      ;; Just for brevity I use string-match to check instead
+                      ;; of listing all my ing macros but there has been a clash
+                      ;; with org-ml that uses some macros that end in "ing!".
+                      (`(,(and name (pred ing-symbol-p)) ,symbol . ,(guard t))
+                       (cl-case name
+                         ((maxing! maximizing!)
+                          (cl-pushnew `(,symbol most-negative-fixnum) bindings))
+                         ((minning! minimizing!)
+                          (cl-pushnew `(,symbol most-positive-fixnum) bindings))
+                         ((summing! adding! counting!)
+                          (cl-pushnew `(,symbol 0) bindings))
+                         (t
+                          (cl-pushnew `(,symbol nil) bindings :key #'car)))
+                       form)
+                      ;; Handle special let shortcuts.
+                      (`((,(and macro (pred letbind-symbol-p)) . ,args) . ,(and rest (guard t)))
+                       `((,(alist-get macro lets) ((,@args)) ,@(process-form rest))))
+                      ((pred null)
+                       form)
+                      ;; Recurse into lists.
+                      ((pred listp)
+                       (cons (process-form (car form)) (process-form (cdr form))))
+                      ;; Leave other forms untouched.
+                      (_
+                       form))))
+        (setq body (process-form body)))
+      (setq bindings (append init (cl-remove-if #'should-remove-p bindings))))
+    (list bindings body)))
 ;;;; main macro
 (defmacro autolet! (&rest body)
-  "Let-bind symbols and transform forms based on indicators in BODY.
+  "Dynamically let-bind symbols and modify forms in BODY.
 
-Process BODY, recognizing special forms and keywords to dynamically bind
-variables or modify expressions.
+Process BODY by recognizing special forms and keywords for dynamically
+let-binding symbols, automatically wrapping forms and enhancing the control flow
+of loops.
 
-:init BINDINGS    Predefine symbol bindings.  BINDINGS follow the same format as `let*'.
-:noinit SYMS      Do not let bind any symbols in SYMS.  This takes precedence.
+Keywords:
+:init BINDINGS    Predefine symbol bindings.  Follow the same format as `let*'.
+This takes precedence over other let-binding indicators.
+:noinit SYMS      Do not let bind any symbols in SYMS, regardless of whether
+those symbols where specified by dynamic let-binding indicators.
 
+Dynamic let-binding:
 (set! SYM _)      Let bind SYM to nil.
 (maxing! SYM _)   Let bind SYM to `most-negative-fixnum'.
 (minning! SYM _)  Let bind SYM to `most-positive-fixnum'.
 (counting! SYM _) Let bind SYM to 0.
 (...ing! SYM VAL) Let bind SYM to nil.
 
-(stub!|flet! NAME ARGS . BODY) Wrap subsequent forms with `(cl-flet ((NAME ARGS . BODY)))'.
-(nflet!|noflet! NAME ARGS . BODY) Same as `stub!' but use `lef!'.
-(label!|labels! NAME ARGS . BODY) Same as `stub!' but use `cl-labels'.
+Wrapping forms:
+(mlet!|macrolet! NAME ARGS . BODY) Wrap subsequent forms with
+`(cl-macrolet ((NAME ARGS . BODY)))'.
+(stub!|flet! NAME ARGS . BODY)     Same as macrolet but use `cl-letf'.
+(nflet!|noflet! NAME ARGS . BODY)  Same as `stub!' but use `lef!'.
+(label!|labels! NAME ARGS . BODY)  Same as `stub!' but use `cl-labels'.
 
-(LOOP CONDITION . BODY) Replace with `(catch 'return! (LOOP CONDITION (catch 'break! BODY)))'."
-  (let ((bindings nil)
-        (temp nil)
-        (noinit nil)
-        (init nil)
-        (forms body)
-        (quote-constructs '(quote function backquote cl-function))
-        (lets '((nflet! . lef!)
-                (noflet! . lef!)
-                (flet! . cl-flet)
-                (stub! . cl-flet)
-                (label! . cl-labels)
-                (labels! . cl-labels)))
-        (loops '(for! loop! dolist! while dolist dotimes)))
-    ;; Process initial settings in beginning of body.  Special keywords will be
-    ;; :noinit and :init.
-    (while (member (car forms) '(:noinit :init :let))
-      (pcase (car forms)
-        ((or :init :let)
-         (pop forms)
-         (setq init (append init (pop forms))))
-        (:noinit
-         (pop forms)
-         (setq noinit (append noinit (ensure-list (pop forms)))))))
-    (cl-labels ((process-form (form)
-                  (pcase form
-                    ;; Leave quoted forms as-is.
-                    (`(,(pred (lambda (x) (memq x quote-constructs))) . ,_)
-                     form)
-                    ;; Match `(set! VAR VALUE)` and collect VARIABLE.
-                    (`(set! ,(and symbol (pred symbolp)) ,_ . ,(guard t))
-                     (cl-pushnew (list symbol nil) bindings :key #'car)
-                     form)
-                    ;; Handle set forms with mutiple values.
-                    (`(set! ,(and pattern (or (pred listp) (pred vectorp))) ,_ . ,(guard t))
-                     (dolist (sym (reverse (oo--set-flatten pattern)))
-                       (cl-pushnew (list sym nil) bindings :key #'car))
-                     form)
-                    ;; Surround loops with a catch.
-                    (`(,(and loop (pred (lambda (x) (memq x loops)))) ,pred . ,(and body (guard t)))
-                     `(catch 'break! (,loop ,pred (catch 'continue! ,@(process-form body)))))
-                    ;; Properly initialize variables in ingmacro declarations.
-                    ;; Just for brevity I use string-match to check instead
-                    ;; of listing all my ing macros but there has been a clash
-                    ;; with org-ml that uses some macros that end in "ing!".
-                    (`(,(and name (pred (lambda (x) (and (symbolp x) (string-match-p "ing!$" (symbol-name x)))))) ,symbol . ,(guard t))
-                     (cl-case name
-                       ((maxing! maximizing!)
-                        (push `(,symbol most-negative-fixnum) bindings))
-                       ((minning! minimizing!)
-                        (push `(,symbol most-positive-fixnum) bindings))
-                       ((summing! adding! counting!)
-                        (push `(,symbol 0) bindings))
-                       (t
-                        (push `(,symbol nil) bindings)))
-                     form)
-                    ;; Handle special let shortcuts.
-                    (`((,(and macro (pred (lambda (x) (assoc x lets)))) . ,args) . ,(and rest (guard t)))
-                     `((,(alist-get macro lets) ((,@args)) ,@(process-form rest))))
-                    ((pred null)
-                     form)
-                    ;; Recurse into lists.
-                    ((pred listp)
-                     (cons (process-form (car form)) (process-form (cdr form))))
-                    ;; Recurse into vectors.
-                    ((pred vectorp)
-                     (vconcat (process-form (append form nil))))
-                    ;; Leave other forms untouched.
-                    (_
-                     form))))
-      (setq body (process-form forms)))
-    ;; Normalize init format.
-    (setq init (mapcar (lambda (x) (cond ((symbolp x)
-                                          (list x nil))
-                                         ((not (nthcdr 1 x))
-                                          (append x (list nil)))
-                                         (t
-                                          x)))
-                       init))
-    (dolist (binding bindings)
-      (when (and (not (member (car-safe binding) noinit))
-                 (not (assoc (car-safe binding) init)))
-        (push binding temp)))
-    (setq bindings (append init temp))
+Enhanced looping control flow:
+(while|dotimes|dolist CONDITION . BODY) Replace with
+`(catch \='return! (LOOP CONDITION (catch \='break! BODY)))'."
+  (pcase-let ((`(,bindings ,body) (oo--autolet-data body)))
     `(let ,bindings (catch 'return! ,@body))))
 ;;; provide
 (provide 'base-macros-autolet)
