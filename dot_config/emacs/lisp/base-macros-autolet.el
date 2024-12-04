15564e99d4afbd6dd4bc12adbf701f54def6bb4d
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-04 11:47:28 -0500

    dot_config/emacs/lisp/base-macros-autolet.el

--- dot_config/emacs/lisp/base-macros-autolet.el
+++ dot_config/emacs/lisp/base-macros-autolet.el
@@ -26,7 +26,9 @@
 ;; certain indicators in its body (among other things).  This macro was created
 ;; for multiple reasons.
 ;;
-;; -
+;; - automate let bind
+;; - automate wrapping forms
+;; - enhance control flow
 ;;
 ;; Sometimes you do not want symbol to be auto let-bound to nil, you actually
 ;; want to just modify the original symbol without let-binding it at all.  In
