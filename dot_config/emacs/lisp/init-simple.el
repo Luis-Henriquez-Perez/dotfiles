3f75dfd42a3c9a9f4c627cba0dc61ab57e6a946f
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-05 08:20:32 -0500

    Prevent byte-comp warning with unsed variable.

--- dot_config/emacs/lisp/init-simple.el
+++ dot_config/emacs/lisp/init-simple.el
@@ -46,7 +46,7 @@
   (oo-add-hook 'before-save-hook #'delete-trailing-whitespace :local t))
 ;;;; less confusing kill buffer
 ;; https://christiantietze.de/posts/2023/09/kill-unsaved-buffer-ux-action-labels/
-(defun! oo--prompt-clearly (_ buffer &rest args)
+(defun! oo--prompt-clearly (_ buffer &rest _)
   "Ask user in the minibuffer whether to save before killing.
 Replace `kill-buffer--possibly-save' as advice."
   (set! prompt (format "Buffer %s modified." (buffer-name)))
