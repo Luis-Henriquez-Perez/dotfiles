9ce899b17f619a1ac4f708df125b14ab459c2c9d
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-07 13:08:21 -0500

    Move contents of oo-base-utils to oo-base-lib.

--- dot_config/emacs/lisp/base-lib.el
+++ dot_config/emacs/lisp/base-lib.el
@@ -41,6 +41,80 @@
 
 (defvar evil-state-properties)
 (declare-function evil-define-key* "evil")
+;;;; converting types
+;; These functions try to "do what I mean" when converting from one type to another.
+(defun oo-into-string (&rest args)
+  "Return ARGS as a string."
+  (declare (pure t) (side-effect-free t))
+  (with-output-to-string (mapc #'princ args)))
+(defalias 'oo-to-string 'oo-into-string)
+
+(defun oo-into-symbol (&rest args)
+  "Return an interned symbol from ARGS."
+  (declare (pure t) (side-effect-free t))
+  (intern (apply #'oo-into-string args)))
+(defalias 'oo-to-symbol 'oo-into-symbol)
+
+(defun oo-into-keyword (&rest args)
+  "Return ARGS as a keyword."
+  (declare (pure t) (side-effect-free t))
+  (apply #'oo-into-symbol ":" args))
+(defalias 'oo-to-keyword 'oo-into-keyword)
+;;;; miscellaneous
+(defun oo-wrap-forms (wrappers forms)
+  "Return FORMS wrapped by WRAPPERS.
+FORMS is a list of forms to be wrapped.  WRAPPERS are a list of forms
+representing the wrappers to apply.  If WRAPPERS is empty, `progn' is added to
+ensure the result is syntactically valid."
+  (declare (pure t) (side-effect-free t))
+  (unless wrappers (push '(progn) wrappers))
+  (setq wrappers (reverse wrappers))
+  (setq forms (append (pop wrappers) forms))
+  (dolist (wrapper wrappers)
+    (setq forms (append wrapper (list forms))))
+  forms)
+;;;; oo-in-string-or-comment-p
+;; This function is used by captain and abbrev.
+(defun oo-in-string-or-comment-p ()
+  "Return non-nil if point is in a string or comment.
+Specifically, return the symbol `string' if point is in a string, the symbol
+`comment' if in a comment and nil otherwise."
+  (declare (pure t) (side-effect-free t))
+  (let ((ppss (syntax-ppss)))
+    (cond ((nth 3 ppss) 'string)
+          ((nth 4 ppss) 'comment)
+          (t nil))))
+;;;; if-not!
+;; More often than not when I am using `if', the default else clause is simpler than
+;; the then clause.  And in that case I end up having to wrap the then clause in
+;; a `progn'. I want to invert the else clause and the if clause so I do not
+;; need to include the extra `progn' in that case.  I also considered just
+;; writing a macro that expands to an `if' with the then and else reversed, but
+;; I think it might be confusing.
+(defmacro if-not! (cond then &rest else)
+  (declare (indent 2))
+  `(if (not ,cond) ,then ,@else))
+
+(defalias 'nif! 'if-not!)
+;;;; oo-funcall-silently
+(defun oo-funcall-silently (fn &rest args)
+  "Call FN with ARGS without producing any output."
+  (shut-up (apply fn args)))
+;;;; oo-first-success
+;; This function is very similar to dash's [[file:snapshots/_helpful_function__-first_.png][-first]] or cl-lib's [[file:snapshots/_helpful_function__cl-find-if_.png][cl-find-if]].
+;; These functions take a predicate and a list and they return the first element of
+;; the list for which ~(pred element)~ returns non-nil.  The function =oo-first-success= also takes a
+;; predicate and the list, but instead it returns the first non-nil return value of
+;; ~(pred element)~.  For example, ~(oo-first-sucess 'numberp '(a t 0))~ would return
+;; =t= instead of =0= as it would for =-first= or =cl-find-if= because ~(numberp 0)~ evaluates
+;; to =t=. The name of this function is inspired by a similar function designed for
+;; hooks [[file:snapshots/_helpful_function__run-hooks-with-args-until-success_.png][run-hook-with-args-until-success]].
+(defun oo-first-success (fn list)
+  "Return the first non-nil result of applying FN to an element in LIST."
+  (declare (pure t) (side-effect-free t))
+  (let (success)
+    (--each-while list (not (setq success (funcall fn it))))
+    success))
 ;;;; logging
 (defvar oo-logger (lgr-get-logger "main")
   "Object used for logging.")
@@ -264,7 +338,7 @@ SYMBOL and FN in `oo-after-load-hash-table'."
 ;;     (setq ,def (list 'menu-item "" ,alt :filter #'oo-alternate-command-choose-fn))
 ;;     (push ,(oo--lambda-form alt '(&rest ) `(when ,condition ,alt)) (gethash ,orig oo-alternate-commands))
 ;;     ,@(oo--bind-generate-body metadata steps)))
-;;;; foo
+;;;; alt!
 (defmacro alt! (old new feature)
   `(progn (push (lambda (&rest _) (when (or (featurep ',feature) (require ',feature nil t)) ',new))
                 (gethash ',old oo-alternate-commands))
