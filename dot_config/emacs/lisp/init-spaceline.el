bfb8e7e6d5fff01594cad296bdf7c41491d958e2
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-08 00:14:21 -0500

    dot_config/emacs/lisp/init-spaceline.el

--- dot_config/emacs/lisp/init-spaceline.el
+++ dot_config/emacs/lisp/init-spaceline.el
@@ -169,7 +169,7 @@
     '((+evil-state :face (spaceline-highlight-face-evil-state))
       ((+narrow +kbd-macro +buffer-read-only +buffer-modified buffer-id remote-host) :priority 98)
       (+version-control :face 'powerline-active0))
-    '((+pomodoro :face 'powerline-active0) major-mode (+current-time :face )))
+    '((+pomodoro :face 'powerline-active0) major-mode (+current-time :face ())))
   (setq-default mode-line-format '("%e" (:eval (spaceline-ml-main)))))
 ;;; provide
 (provide 'init-spaceline)
