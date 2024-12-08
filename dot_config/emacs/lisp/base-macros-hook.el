83f44266b6830133407d0b99cfb7ea0652e1d4e9
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-08 01:16:46 -0500

    dot_config/emacs/lisp/base-macros-hook.el

--- dot_config/emacs/lisp/base-macros-hook.el
+++ dot_config/emacs/lisp/base-macros-hook.el
@@ -55,7 +55,6 @@
         (let ((fill-column len))
           (fill-region (point-min) (point-max)))
         (buffer-substring (point-min) (point-max)))))
-  ;; (oo-condition-case-fn fn action)
   `(progn
      (declare-function ,function nil)
      (defun ,fname (&rest args)
