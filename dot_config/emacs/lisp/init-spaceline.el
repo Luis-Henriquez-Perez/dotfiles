a2fc1f4615d76594f980372b1a9b010032a482e6
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-08 00:14:06 -0500

    dot_config/emacs/lisp/init-spaceline.el

--- dot_config/emacs/lisp/init-spaceline.el
+++ dot_config/emacs/lisp/init-spaceline.el
@@ -163,11 +163,11 @@
   (message "set separator to %s" powerline-default-separator))
 ;;;; initialize modeline at startup
 (opt! spaceline-highlight-face-func #'spaceline-highlight-face-evil-state)
-(spaceline-highlight-face-evil-state)
+
 (defhook! oo-initialize-modeline-h (after-init-hook :depth 90)
   (spaceline-compile
     'main
-    '((+evil-state)
+    '((+evil-state :face )
       ((+narrow +kbd-macro +buffer-read-only +buffer-modified buffer-id remote-host) :priority 98)
       (+version-control :face 'powerline-active0))
     '((+pomodoro :face 'powerline-active0) major-mode (+current-time)))
