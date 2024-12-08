01d9bd450b74e075912897a15557579e426c7f82
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-08 01:12:44 -0500

    dot_config/emacs/lisp/base-macros-hook.el

--- dot_config/emacs/lisp/base-macros-hook.el
+++ dot_config/emacs/lisp/base-macros-hook.el
@@ -44,7 +44,6 @@
   (set! fname (intern (format "%s&%s" hook function)))
   (set! depth (plist-get args :depth))
   (set! local (plist-get args :local))
-  ;; Ignore arguments.
   ;; This is taken directly from the `s' library.  Right now, it is the only
   ;; function from there I use.  Not wanting to require s for just one short
   ;; function, I copied it is body here.
