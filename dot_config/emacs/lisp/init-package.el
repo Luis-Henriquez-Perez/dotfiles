76c77a9a353800b4c7ad760605b47da9ccc26ce9
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-11 08:02:52 -0500

    dot_config/emacs/lisp/init-package.el

--- dot_config/emacs/lisp/init-package.el
+++ dot_config/emacs/lisp/init-package.el
@@ -220,7 +220,7 @@
            (message "Installing package `%s'" package)
            (quietly! (package-install package 'dont-select))
            (unless (package-installed-p package)
-             (message "Successfully installed package `%s'")
+             (message "Successfully installed package `%s'" package)
              (message "Failed to install package")))
           (t
            (message "Package %s is not available." package)))))
