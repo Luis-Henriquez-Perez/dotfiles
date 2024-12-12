141310eaa249d4647dcc1f69065a3d0204b8e8a1
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-12 09:37:03 -0500

    dot_config/emacs/lisp/base-lib.el

--- dot_config/emacs/lisp/base-lib.el
+++ dot_config/emacs/lisp/base-lib.el
@@ -71,7 +71,11 @@ A \"true list\" is a list whose CDR is also a list."
   "Return non-nil if NUMBER is greater than zero."
   (> number 0))
 
-
+(defsubst oo-contains-all-p (object)
+  "Return non-nil if OBJECT is a true list.
+A \"true list\" is a list whose CDR is also a list."
+  (declare (pure t) (side-effect-free error-free))
+  (and (listp object) (listp (cdr-safe object))))
 ;;;; macros
 ;;;;; nif!
 ;; More often than not when I am using `if', the default else clause is simpler than
