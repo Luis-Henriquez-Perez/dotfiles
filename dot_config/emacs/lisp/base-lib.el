7abf20397d3bbd223b87ecb6517e3e2799921092
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-12 09:34:25 -0500

    dot_config/emacs/lisp/base-lib.el

--- dot_config/emacs/lisp/base-lib.el
+++ dot_config/emacs/lisp/base-lib.el
@@ -55,6 +55,11 @@
   "Return non-nil if OBJECT."
   )
 
+(defun oo-true-list-p (object)
+  "Return non-nil if OBJECT is a true list.
+A \"true list\" is a list whose CDR is also a list."
+  (declare (pure t) (side-effect-free error-free))
+  (and (listp object) (listp (cdr-safe object))))
 ;;;; macros
 ;;;;; nif!
 ;; More often than not when I am using `if', the default else clause is simpler than
