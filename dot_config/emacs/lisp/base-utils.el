f77819110c92b87b0ee5f01bd76242137a962c12
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-13 16:10:04 -0500

    dot_config/emacs/lisp/base-utils.el

--- dot_config/emacs/lisp/base-utils.el
+++ dot_config/emacs/lisp/base-utils.el
@@ -252,6 +252,61 @@ Specifically, return the symbol `string' if point is in a string, the symbol
     (cond ((nth 3 ppss) 'string)
           ((nth 4 ppss) 'comment)
           (t nil))))
+;;;; oo-call-after-load
+(defun oo--call-after-load (expr fn)
+  "Call FN after EXPR is met."
+  (pcase expr
+    ((pred null)
+     (funcall fn nil))
+    (`(:or . ,exprs)
+     (dolist (expr exprs)
+       (oo-call-after-load expr fn)))
+    (`(:and . ,exprs)
+     (oo--call-after-load exprs fn))
+    ((or `(,(and feature (pred symbolp))) (and feature (pred symbolp)))
+     (if (featurep feature)
+         (funcall fn feature)
+       (eval-after-load feature (apply-partially #'oo--call-after-load feature fn))))
+    (`(,expr . ,exprs)
+     (oo--call-after-load expr `(lambda (_) (oo--call-after-load ',exprs #',fn))))
+    (_
+     (error "invalid expression `%S'" expr))))
+
+;; This macro is designed with the following goals in mind.
+;; 1 - use one generic macro for most binding needs
+;; 2 - log the variables I set and when they are being set
+;; You'll get a warning when trying to bind a symbol that hasn't been defined yet.
+;; So it's best to bind a package symbol only after the package has been loaded.
+;; 3 - stop worrying about variables that haven't been bound
+;; 4 - stop worrying about whether a variable is a custom variable or not
+;; Some variables are custom variables.  Meaning they have some function that.
+(defun oo-call-after-load (expr fn)
+  "Call FN with ARGS after EXPR resolves.
+EXPR can be a feature (symbol), a list of CONDITIONS, a list whose CAR is
+either `:or' or `:and' and whose CDR is a list of EXPRS.  If CONDITION is a
+feature, call FN with ARGS if feature has already been provided; otherwise,
+behave similarly to `eval-after-load'.  If EXPR is a list of
+EXPRS, call FN with ARGS only after all CONDITIONS have been met.  If
+EXPR is a list whose CAR is `:and' behave the same way as (CDR CONDITION).
+If EXPR is a list whose CAR is `:or', call FN with ARGS after any of
+EXPRS in (CDR CONDITION) is met."
+  (alet! (eval `(let ((first-call-p t))
+                  (lambda (&optional feature)
+                    (when first-call-p
+                      (setq first-call-p nil)
+                      (info! "AFTER-LOAD: %s -> %s" feature #',fn)
+                      (condition-case err
+                          (funcall #',fn)
+                        (error
+                         (cond (oo-debug-p
+                                (signal (car err) (cdr err)))
+                               (t
+                                (error! "Error calling %s in %s because of %s"
+                                        ',fn
+                                        (car err)
+                                        (cdr err)))))))))
+               t)
+    (oo--call-after-load expr it)))
 ;;; provide
 (provide 'base-utils)
 ;;; base-utils.el ends here
