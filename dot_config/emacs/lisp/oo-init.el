a20cc7e33ef1e1f7c8b351c7d0de24334f65641d
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-14 09:22:21 -0500

    whatever change

--- dot_config/emacs/lisp/oo-init.el
+++ dot_config/emacs/lisp/oo-init.el
@@ -45,8 +45,7 @@
       (1 font-lock-keyword-face nil t)
       (2 font-lock-function-name-face nil t))
      ("\\_<\\(it\\|this-fn\\)\\_>"
-      (1 font-lock-constant-face nil t))))
-  )
+      (1 font-lock-constant-face nil t)))))
 ;;;;; oo-override-map
 ;; To ensure that =oo-override-mode-map= takes priority over evil states, we need
 ;; to make it an intercept map for all evil states.  In evil, intercept maps are
