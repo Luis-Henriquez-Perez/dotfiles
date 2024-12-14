3061db5783f2608b17a060aa0eced1788c5bf6f8
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-13 20:37:35 -0500

    Remove evil-magit because it is now apart of evil-collection.

--- dot_config/emacs/lisp/init-package.el
+++ dot_config/emacs/lisp/init-package.el
@@ -199,11 +199,10 @@
 
 (cl-pushnew '(evil-easymotion :url "https://github.com/Luis-Henriquez-Perez/evil-easymotion" :branch "master") package-vc-selected-packages)
 (cl-pushnew '(spaceline :url "https://github.com/Luis-Henriquez-Perez/spaceline" :branch "add-evil-operator-state-face") package-vc-selected-packages)
-(cl-pushnew '(evil-magit :url "https://github.com/emacs-evil/evil-magit") package-vc-selected-packages)
 (cl-pushnew '(outli :url "https://github.com/jdtsmith/outli") package-vc-selected-packages)
 (cl-pushnew '(on :url "https://github.com/ajgrf/on.el") package-vc-selected-packages)
 (cl-pushnew '(zone-matrix :url "https://github.com/ober/zone-matrix" :branch "master") package-vc-selected-packages)
-(cl-pushnew '(grid :url "https://github.com/ichernyshovvv/grid.el" :branch "master") package-vc-selected-packages)
+;; (cl-pushnew '(grid :url "https://github.com/ichernyshovvv/grid.el" :branch "master") package-vc-selected-packages)
 
 ;; The function `package-install-selected-packages' does not activate the
 ;; packages which causes a problem fo rme.
