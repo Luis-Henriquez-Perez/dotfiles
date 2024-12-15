4cc8ac5793875e7a5af9d83f7d664161d4cf6e6f
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-15 09:31:21 -0500

    dot_config/emacs/lisp/init-startup.el

--- dot_config/emacs/lisp/init-startup.el
+++ dot_config/emacs/lisp/init-startup.el
@@ -53,7 +53,6 @@
       (run-with-timer 5 nil #'oo--timer--lower-garbage-collection)
     (info! "Running timer for lowering garbage collection...")
     (set! reduction (/ (get-register :gc-cons-threshold) 10))
-    ;; (info! "reduction = %f MB of %f MB" (mb reduction) (get-register :gc-cons-threshold))
     (set! gc-floor (* 8 1024 1024))
     (set! gcp-default 0.2)
     (when (/= gc-cons-threshold gc-floor)
