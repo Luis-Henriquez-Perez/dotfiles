fc685aaae16d13656582865c7095e8b5a98f18f8
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-16 11:12:24 -0500

    Fix problem

--- dot_config/emacs/lisp/config-auto-insert.el
+++ dot_config/emacs/lisp/config-auto-insert.el
@@ -172,10 +172,18 @@
                    "</body>" > n
                    "</html>")))
 
-(defun! oo-auto-insert-python-file-header ()
+(defun oo-auto-insert-python-file-header ()
   "Insert python file header."
   (require 'tempel)
-  (tempel-insert '("# Filename: " (file-name-non-directory (directory-file-name (buffer-file-name))) > n
+  (tempel-insert '("# Filename: " (file-name-nondirectory (directory-file-name (buffer-file-name))) > n
+                   "# Author: " user-full-name " <" user-mail-address ">" > n
+                   "# Created: " (format-time-string "%Y-%m-%d %H:%M:%S") > n
+                   "# Description: " p > n)))
+
+(defun oo-auto-insert-bash-file-header ()
+  "Insert bash file header."
+  (require 'tempel)
+  (tempel-insert '("# Filename: " (file-name-nondirectory (directory-file-name (buffer-file-name))) > n
                    "# Author: " user-full-name " <" user-mail-address ">" > n
                    "# Created: " (format-time-string "%Y-%m-%d %H:%M:%S") > n
                    "# Description: " p > n)))
