8d14df82d5b98ccb536308331e773bfc99f13b7b
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-16 21:11:02 -0500

    /home/luis/.config/emacs/lisp/oo-commands.el

--- .config/emacs/lisp/oo-commands.el
+++ .config/emacs/lisp/oo-commands.el
@@ -270,8 +270,10 @@ With prefix argument, run as dry-run (do not actually move any files)."
   (when diff
     (set! command (format "%s add %s && %s commit -m %S %s" git fname git msg fname))
     (call-process-shell-command command)
-    (trace! "hello")
-
+    ;; (let ((default-directory (file-name-directory (buffer-file-name buffer))))
+    ;;   (let ((proc (start-process "git" "*git-auto-push*" "git" "push")))
+    ;;     (set-process-sentinel proc 'gac-process-sentinel)
+    ;;     (set-process-filter proc 'gac-process-filter)))
     ;; (shell-command-to-string (format "%s push" git))
     ))
 
