a2be1ffd34bfad785d084dcaf6591dc2eafec08e
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-17 05:38:57 -0500

    /home/luis/.config/emacs/lisp/oo-commands.el

--- .config/emacs/lisp/oo-commands.el
+++ .config/emacs/lisp/oo-commands.el
@@ -279,7 +279,6 @@ changes and push them."
     ;; TODO: fix keeps asking me for password
     (set! (program arg1 arg2) (split-string git))
     (set! proc (start-process "git" "*git-auto-push*" program arg1 arg2 "push"))
-    ;; (set! proc (start-process "git" "*git-auto-push*" git "push"))
     (set-process-sentinel proc #'status)
     ;; (set-process-filter proc 'gac-process-filter)
     ))
