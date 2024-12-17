f7462069c7d24e6b0d86607275b15718d9bff79b
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-16 21:25:12 -0500

    /home/luis/.config/emacs/lisp/oo-commands.el

--- .config/emacs/lisp/oo-commands.el
+++ .config/emacs/lisp/oo-commands.el
@@ -272,7 +272,7 @@ changes and push them."
   (when diff
     (set! command (format "%s add %s && %s commit -m %S %s" git fname git msg fname))
     (call-process-shell-command command)
-    (set! (program arg1 arg2))
+    (set! (program arg1 arg2) (split-string ))
     (set! proc (start-process "git" "*git-auto-push*"  ( "push")))
     (set-process-sentinel proc (lambda (_ status) (message "git %s" (substring status 0 -1))))))
 
