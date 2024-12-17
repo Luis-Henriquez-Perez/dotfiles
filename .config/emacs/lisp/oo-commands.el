361778ad1543cbed4cad1b409a36b768f877bdd2
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-17 03:50:48 -0500

    /home/luis/.config/emacs/lisp/oo-commands.el

--- .config/emacs/lisp/oo-commands.el
+++ .config/emacs/lisp/oo-commands.el
@@ -288,7 +288,7 @@ changes and push them."
   (set! home-dir (expand-file-name "~/"))
   (set! worktree (format "--work-tree=%s" home-dir))
   (set! dir (format "--git-dir=%s" dotfile-dir))
-  (set! magit-git-global-arguments )
+  (set! magit-git-global-arguments `(,dir ,worktree ,@magit-git-global-arguments))
   (set! fn (lambda ()
              (setq-local magit-git-global-arguments `(,dir ,worktree ,@magit-git-global-arguments))))
   (add-hook 'magit-status-mode-hook fn nil t)
