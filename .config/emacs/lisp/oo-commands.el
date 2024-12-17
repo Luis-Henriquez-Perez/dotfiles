f4cf004d7a22f4c527395f89b2135251d6ee1d10
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-17 03:39:19 -0500

    /home/luis/.config/emacs/lisp/oo-commands.el

--- .config/emacs/lisp/oo-commands.el
+++ .config/emacs/lisp/oo-commands.el
@@ -283,7 +283,7 @@ changes and push them."
   "Open Magit status for the bare Git dotfiles repository."
   (interactive)
   :init ((magit-git-global-arguments magit-git-global-arguments))
-  (setq-hook! magit-status-mode-hook )
+  (setq-hook! magit-status-mode-hook magit-git-global-arguments)
   (set! dotfile-dir (expand-file-name "~/dotfiles/"))
   (set! home-dir (expand-file-name "~/"))
   (pushing! magit-git-global-arguments (format "--work-tree=%s" home-dir))
