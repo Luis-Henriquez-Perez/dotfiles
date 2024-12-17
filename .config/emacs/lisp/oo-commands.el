a236d8b7cc9079a2a39aafca41242dfde472d568
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-16 22:22:12 -0500

    /home/luis/.config/emacs/lisp/oo-commands.el

--- .config/emacs/lisp/oo-commands.el
+++ .config/emacs/lisp/oo-commands.el
@@ -275,7 +275,9 @@ changes and push them."
     (set! (program arg1 arg2) (split-string git))
     (message "%S" (split-string git))
     (set! proc (start-process "git" "*git-auto-push*" program arg1 arg2 "push"))
-    (set-process-sentinel proc (lambda (_ status) (message "git %s" (substring status 0 -1))))))
+    (set-process-sentinel proc (lambda (_ status) (message "git %s" (substring
+                                                                     status 0 -1))))
+    ))
 
 (defun! oo-magit-status-dotfiles ()
   "Open Magit status for the bare Git dotfiles repository."
