f3f713817a0f5504daf757b9ff70f6a1ede99caa
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-17 13:01:14 -0500

    Delete unneeded files.

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-### Install chezmoi if it is not already
-echo "Install chezmoi..."
-# pacman -S chezmoi
-### Setup everything
-# This script is designed to bootstrap my dotfiles its taken from [[][chezmoi site]].
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply Luis-Henriquez-Perez
