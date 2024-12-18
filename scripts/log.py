ba34c7cdef09319e2685992950c3c713c46ee987
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-17 23:07:48 -0500

    /home/luis/scripts/log.py Tue Dec 17 23:07:48 2024

--- scripts/log.py
+++ scripts/log.py
@@ -11,7 +11,7 @@ import os
 
 logfile = os.path.abspath(os.path.expanduser("~/.cache/systemlog.txt"))
 logformat = '%(asctime)s - %(levelname)s - %(message)s'
-logformat = ''
+logformat = '%(asctime)s - %(name)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s'
 # Configure the logging module
 logging.basicConfig(filename=logfile, level=logging.INFO, format=logformat)
 
