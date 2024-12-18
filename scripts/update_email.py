3c0c9a528409f1d3865e2c581b7b2f95d6af2964
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-17 20:08:18 -0500

    Add update_email.py.

---
+++ scripts/update_email.py
@@ -0,0 +1,14 @@
+# Filename: update_email.py
+# Author: Luis Henriquez <luis@luishp.xyz>
+# Created: 2024-12-17 20:02:52
+# Description: Pull email from the cloud and index it.
+
+from log import get_logger
+
+logger = get_logger(__name__)
+
+# mbsync -a
+# mu index
+# notmuch new
+logger.info("")
+logger.info("")
