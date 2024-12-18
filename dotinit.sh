7287ea6521a5f16e1ade2835e9fb2dffffb96caa
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-18 17:14:32 -0500

    /home/luis/dotinit.sh Wed Dec 18 17:14:32 2024

--- dotinit.sh
+++ dotinit.sh
@@ -19,7 +19,9 @@ git clone --bare $DOTFILES_URL $DOTFILES_DIR
 if dot checkout; then
   echo "Successfully Checked out dotfiles.";
 else
-
+  dot checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | while read -r file; do
+    mv "$HOME/$file" "$BACKUP_DIRECTORY/" || echo "Warning: Could not move $file"
+  done
     mkdir -p "$BACKUP_DIRECTORY"
     printf "Backing up pre-existing dotfiles into %s\n" "$BACKUP_DIRECTORY"
     dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
