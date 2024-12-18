0f2d903aa3cd35261f806ae6c437b588fc9640b9
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-17 23:50:08 -0500

    /home/luis/scripts/update_email.py Tue Dec 17 23:50:08 2024

--- scripts/update_email.py
+++ scripts/update_email.py
@@ -64,7 +64,7 @@ def update_email():
     run_command(["mbsync", "-a"], "pulling email")
 
     # Run mu index
-    run_command(["mu", "index"], "Indexing emails with mu")
+    run_command(["mu", "index"], "indexing email")
 
     # Run notmuch new
     run_command(["notmuch", "new"], "Updating notmuch database")
