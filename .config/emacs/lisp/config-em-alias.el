84dc6beeb672c3076b366b65ff229080da7888c3
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-19 17:19:59 -0500

    /home/luis/.config/emacs/lisp/config-em-alias.el Thu Dec 19 17:19:59 2024

--- .config/emacs/lisp/config-em-alias.el
+++ .config/emacs/lisp/config-em-alias.el
@@ -68,9 +68,9 @@
 (eshell/alias "dotadd" "dot add $1 && dot commit -m \"Add $1.\" $1 && git push")
 ;; Define a commit-undo
 (eshell/alias "add" "dot add $1 && dot commit -m \"Add $1.\" $1 && dot push $1")
-(eshell/alias "dot status" "dot status --porcelain")
 (eshell/alias "dot" (format "%s --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $*"
                             (executable-find "git")))
+(eshell/alias "dot status" "dot status --porcelain")
 ;; (eshell/alias "dots" (format "%s --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $*"
 ;;                              (executable-find "git")))
 ;; (eshell/alias "dotadd" "/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME $*")
