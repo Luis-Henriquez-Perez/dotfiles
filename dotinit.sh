8a0e1a44d5c62e094bc2a169c67df8bbf5bf63f0
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-19 11:45:52 -0500

    /home/luis/dotinit.sh Thu Dec 19 11:45:52 2024

--- dotinit.sh
+++ dotinit.sh
@@ -22,7 +22,7 @@ else
   mkdir -p "$BACKUP_DIRECTORY"
 
   for file in $(dot checkout 2>&1 | grep -E "^\s+.+" | awk '{print $1}'); do
-   | while read -r file; do
+      e
     mv "$HOME/$file" "$BACKUP_DIRECTORY/" || echo "Warning: Could not move $file"
   done
 
