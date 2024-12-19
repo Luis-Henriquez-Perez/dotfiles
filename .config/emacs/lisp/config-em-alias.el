b0d6a978c20892a4ac5630bb84381c598bc2c3f6
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-19 07:34:30 -0500

    /home/luis/.config/emacs/lisp/config-em-alias.el Thu Dec 19 07:34:30 2024

--- .config/emacs/lisp/config-em-alias.el
+++ .config/emacs/lisp/config-em-alias.el
@@ -66,7 +66,7 @@
 ;; TODO: Allow arguments to commands.  I ommited them for the sake of.
 ;;;; emacs maintenance
 (pop eshell-command-aliases-list)
-(eshell/alias "dot add" "dot add $1 && dot commit -m \"Add $1.\" && dot push $1")
+(eshell/alias "dot add" "dot add $1 && dot commit")
 (eshell/alias "add" "dot add $1 && dot commit -m \"Add $1.\" && dot push $1")
 (eshell/alias "dot" (format "%s --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $*"
                             (executable-find "git")))
