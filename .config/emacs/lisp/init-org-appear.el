ab49876f3e625237e0b5bb8fd53a273765a16cc0
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-19 07:23:45 -0500

    /home/luis/.config/emacs/lisp/init-org-appear.el Thu Dec 19 07:23:45 2024

--- .config/emacs/lisp/init-org-appear.el
+++ .config/emacs/lisp/init-org-appear.el
@@ -30,6 +30,7 @@
 
 (defhook! ensure-proper (org-appear-mode-hook)
   (when (and org-appear-autoemphasis (not org-hide-emphasis-markers))
+    (setq )
     (warn! ))
   (when (and org-appear-autoemphasis (not org-hide-emphasis-markers))
     (warn! ))
