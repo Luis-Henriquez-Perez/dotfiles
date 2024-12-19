63ec836509690fba6f2ee3b41d5b5832ad656f64
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-19 11:57:12 -0500

    /home/luis/dotinit.sh Thu Dec 19 11:57:12 2024

--- dotinit.sh
+++ dotinit.sh
@@ -22,7 +22,7 @@ else
   mkdir -p "$BACKUP_DIR"
 
   for file in $(dot checkout 2>&1 | grep -E "^\s+.+" | awk '{print $1}'); do
-    echo "moving $HOME/$file to $BACKUP_DIR"
+    echo "moving $HOME/$file to $BACKUP_DIR...."
     mv "$HOME/$file" "$BACKUP_DIR/" || echo "Warning: Could not move $file"
   done
 
