19b60aca113487c7e9fec1803f739e177837b96b
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-20 10:43:31 -0500

    /home/luis/.config/emacs/lisp/init-recentf.el Fri Dec 20 10:43:31 2024

--- .config/emacs/lisp/init-recentf.el
+++ .config/emacs/lisp/init-recentf.el
@@ -53,6 +53,7 @@
 (recentf-push (recentf-expand-file-name "~/.config/init.el"))
 (recentf-push (recentf-expand-file-name "~/.config/qtile/config.py"))
 (recentf-push (recentf-expand-file-name "~/.local/share/qtile/qtile.log"))
+(recentf-push )
 ;;; provide
 (provide 'init-recentf)
 ;;; init-recentf.el ends here
