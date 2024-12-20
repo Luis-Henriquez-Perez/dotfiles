7227438c7a5c5d27d2d8555db595f043b0840efd
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-19 21:11:31 -0500

    /home/luis/.config/qtile/config.py Thu Dec 19 21:11:31 2024

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -12,6 +12,7 @@ from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
+from libqtile.log_utils import logger
 #### declare variables
 mod = "mod4"
 # mod1 = alt
@@ -144,6 +145,7 @@ def rotate_windows(qtile, forward=True):
     - forward (bool): Direction of rotation. True for forward, False for backward.
     """
     current_group = qtile.current_group
+    logger.info("hello")
     print(f"current group -> {current_group}")
     print(f"current group -> {current_group}")
     # if not current_group:
