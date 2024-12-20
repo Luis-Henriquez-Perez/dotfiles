7ce448f820c3797a65910a78a413b2f7921878b7
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-20 06:58:47 -0500

    /home/luis/.config/emacs/lisp/init-spaceline.el Fri Dec 20 06:58:47 2024

--- .config/emacs/lisp/init-spaceline.el
+++ .config/emacs/lisp/init-spaceline.el
@@ -112,7 +112,11 @@
 
 (+spaceline-define-segment! +version-control
   "Display current git branch."
-
+  (set! fname (shell-quote-argument (convert-standard-filename (buffer-file-name))))
+  (set! default-directory (file-name-directory fname))
+  (set! dots (expand-file-name "~/.dotfiles/"))
+  (set! worktree (expand-file-name "~"))
+  (set! git (format "%s --git-dir=%s --work-tree=%s" (executable-find "git") dots worktree))
   (when (and (buffer-file-name)
              (or (locate-dominating-file (buffer-file-name) ".git")
                  (shell-command-to-string (format "%s diff %s" git fname))))
@@ -127,8 +131,8 @@
     (if (display-graphic-p)
         (format "%s %s" (all-the-icons-octicon "git-branch" :face face :v-adjust -0.01) branch)
       branch))
-  (set! diff (shell-command-to-string (format "%s diff %s" git fname)))
-  (locate-dominating-file buffer-file-name ".git"))
+  ;; (locate-dominating-file buffer-file-name ".git")
+  )
 
 (+spaceline-define-segment! +evil-state
   "Display the current evil state if evil-mode is enabled."
