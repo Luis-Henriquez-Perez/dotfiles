d66643022a7bd054b64bb4b062ba35b988130187
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-19 21:08:16 -0500

    /home/luis/.config/qtile/config.py Thu Dec 19 21:08:16 2024

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -12,9 +12,6 @@ from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
-import logging
-
-logger
 #### declare variables
 mod = "mod4"
 # mod1 = alt
