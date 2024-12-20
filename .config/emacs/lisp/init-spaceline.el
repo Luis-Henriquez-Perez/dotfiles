a803de65f3951e6c35e40b2e06481787dba312ba
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-20 06:38:12 -0500

    /home/luis/.config/emacs/lisp/init-spaceline.el Fri Dec 20 06:38:12 2024

--- .config/emacs/lisp/init-spaceline.el
+++ .config/emacs/lisp/init-spaceline.el
@@ -112,22 +112,22 @@
 
 (+spaceline-define-segment! +version-control
   "Display current git branch."
-  (let ((default-directory (or (and buffer-file-name
-                                    (locate-dominating-file buffer-file-name ".git"))
-                               default-directory)))
-    (cond ((not (buffer-file-name)))
-          ((and default-directory (file-directory-p (concat default-directory ".git")))
-           ;; TODO: each segment should get passed in their face so I do not have to
-           ;; hard-code it like this.  The face should have the background and
-           ;; foreground for the segment.  This way I can dynamically set the proper
-           ;; background for an icon.
-           (set! bg (face-attribute 'powerline-active0 :background nil 'default))
-           (set! fg (face-attribute 'warning :background nil 'default))
-           (set! branch (string-trim (shell-command-to-string "git rev-parse --abbrev-ref HEAD")))
-           (set! face `((t (:background ,bg :foreground ,fg))))
-           (if (display-graphic-p)
-               (format "%s %s" (all-the-icons-octicon "git-branch" :face face :v-adjust -0.01) branch)
-             branch)))))
+  (locate-dominating-file buffer-file-name ".git")
+  default-directory
+  (cond ((not (buffer-file-name)))
+        ((and default-directory (file-directory-p (concat default-directory ".git")))
+         ;; TODO: each segment should get passed in their face so I do not have to
+         ;; hard-code it like this.  The face should have the background and
+         ;; foreground for the segment.  This way I can dynamically set the proper
+         ;; background for an icon.
+         (set! bg (face-attribute 'powerline-active0 :background nil 'default))
+         (set! fg (face-attribute 'warning :background nil 'default))
+         (set! branch (string-trim (shell-command-to-string "git rev-parse --abbrev-ref HEAD")))
+         (set! face `((t (:background ,bg :foreground ,fg))))
+         (if (display-graphic-p)
+             (format "%s %s" (all-the-icons-octicon "git-branch" :face face :v-adjust -0.01) branch)
+           branch))
+        ()))
 
 (+spaceline-define-segment! +evil-state
   "Display the current evil state if evil-mode is enabled."
