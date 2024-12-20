4923f10df71e1bfdeeef9f149ec57cbfb24af109
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-20 08:52:22 -0500

    /home/luis/.config/emacs/lisp/init-spaceline.el Fri Dec 20 08:52:22 2024

--- .config/emacs/lisp/init-spaceline.el
+++ .config/emacs/lisp/init-spaceline.el
@@ -125,6 +125,7 @@
 (+spaceline-define-segment! +version-control
   "Display current git branch.
 If file is a dotfile managed by my git bare repo, display that branch."
+  (unless (buffer-file-name))
   (set! git (or (executable-find "git")))
   (when (and (buffer-file-name)
              (or (locate-dominating-file (buffer-file-name) ".git")
