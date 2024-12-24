643f5e61c1cbfe195103590b0cb6beb66a117488
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-23 21:02:45 -0500

    Make dotinit.sh script work.

--- scripts/dotinit.sh
+++ scripts/dotinit.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
-# Filename: dotfiles_init.sh
+# Filename: dotinit.sh
 # Author: Luis Henriquez-Perez <luis@luishp.xyz>
 # Created: 2024-12-23 16:33:00
 # Description: Initialize dotfiles from git repo.
 
-BACKUP_DIR=$(mktemp -d ~/backup_dir_XXXXXX)
-OUTPUT=$(git checkout develop 2>&1)
+REPO="https://github.com/Luis-Henriquez-Perez/dotfiles.git"
+# THROWAWAY_DIR=$(mktemp -u "throwaway_dir_XXXXXX")
+
+# Clone the repository with a separate git directory
+cd "$HOME" || { echo "Error: Unable to change to HOME directory."; exit 1; }
+echo "Cloning $REPO..."
+git clone -n --separate-git-dir="$HOME/.git" "$REPO" throwaway || { echo "Error: Failed to clone $REPO."; exit 1; }
+rm -rf throwaway
+
+# Configure Git to hide untracked files
+if ! git config --local status.showUntrackedFiles no; then
+    echo "Error: Failed to configure Git to hide untracked files.";
+    exit 1;
+fi
+
+echo "Attempting to checkout files..."
+OUTPUT=$(git checkout 2>&1)
 
 while true; do
-    # I do not check for the exit code because it seems that git produces a 0
-    # excited code for checking out even if it was unsuccessful.  So I actually
-    # have to check for the specific error message.
     if ! echo "$OUTPUT" | grep -q "error: The following untracked"; then
         echo "Checkout succeeded."
         break
     fi
+    # Create a backup directory for conflicting files
+    BACKUP_DIR=$(mktemp -d "$HOME/backup_dir_XXXXXX")
     CONFLICTING_FILES=$(echo "$OUTPUT" | grep -E "^\s+.+" | awk '{print $1}')
 
-    # When the number of conflicting files exceeds a certain amount the git
-    # checkout message does not display them all.  That is why I need to use a
-    # while loop to get them all.  Also sometimes the file name is cut
-    # off--which is why I check to see if the file exists.
+    # Move conflicting files to the backup directory
     for file in $CONFLICTING_FILES; do
         FILE_PATH="$HOME/$file"
-        if [[ -f $FILE_PATH ]]; then
+        if [[ -f "$FILE_PATH" ]]; then
             if mv "$FILE_PATH" "$BACKUP_DIR"; then
                 echo "Moved $FILE_PATH -> $BACKUP_DIR"
             else
@@ -31,9 +42,8 @@ while true; do
                 exit 1
             fi
         else
-            echo "Error: $FILE_PATH does not exist or is not a regular file."
+            echo "Warning: $FILE_PATH does not exist or is not a regular file."
         fi
     done
-
-    OUTPUT=$(git checkout develop 2>&1)
+    OUTPUT=$(git checkout 2>&1)
 done
