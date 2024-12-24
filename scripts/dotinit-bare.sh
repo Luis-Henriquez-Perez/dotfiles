10681162ec922ef818f0d2182c5442340386911a
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-23 20:29:52 -0500

    Move git bare script to `dotinit-bar.sh`; replace with new script.

---
+++ scripts/dotinit-bare.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Filename: dotfiles_init.sh
+# Author: Luis Henriquez <luis@luishp.xyz>
+# Created: 2024-12-16 12:26:13
+# Description: Initialize dotfiles from bare git repo.
+
+DOTFILES_URL="https://github.com/Luis-Henriquez-Perez/dotfiles.git"
+DOTFILES_DIR="$HOME/.dotfiles"
+BACKUP_DIR="$HOME/.dotfiles_backup"
+
+dot () {
+  git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" "$@"
+}
+
+echo "Cloning bare Git repository from $DOTFILES_URL"
+git clone --branch master--bare-repo --bare $DOTFILES_URL $DOTFILES_DIR
+
+if dot checkout; then
+  echo "Successfully Checked out dotfiles."
+else
+  echo "Conflicts detected! Backing up pre-existing dotfiles to $BACKUP_DIRECTORY..."
+  mkdir -p "$BACKUP_DIR"
+
+  for file in $(dot checkout 2>&1 | grep -E "^\s+.+" | awk '{print $1}'); do
+    echo "moving $HOME/$file to $BACKUP_DIR..."
+    mv "$HOME/$file" "$BACKUP_DIR/" || echo "Warning: Could not move $file"
+  done
+
+  dot checkout
+fi;
+
+dot config --local status.showUntrackedFiles no
+
+echo "Dotfiles initialized successfully."
--- scripts/dotinit.sh
+++ scripts/dotinit.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 # Filename: dotfiles_init.sh
-# Author: Luis Henriquez <luis@luishp.xyz>
-# Created: 2024-12-16 12:26:13
-# Description: Initialize dotfiles from bare git repo.
+# Author: Luis Henriquez-Perez <luis@luishp.xyz>
+# Created: 2024-12-23 16:33:00
+# Description: Initialize dotfiles from git repo.
 
-DOTFILES_URL="https://github.com/Luis-Henriquez-Perez/dotfiles.git"
-DOTFILES_DIR="$HOME/.dotfiles"
-BACKUP_DIR="$HOME/.dotfiles_backup"
+BACKUP_DIR=$(mktemp -d ~/backup_dir_XXXXXX)
+OUTPUT=$(git checkout develop 2>&1)
 
-dot () {
-  git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" "$@"
-}
+while true; do
+    # I do not check for the exit code because it seems that git produces a 0
+    # excited code for checking out even if it was unsuccessful.  So I actually
+    # have to check for the specific error message.
+    if ! echo "$OUTPUT" | grep -q "error: The following untracked"; then
+        echo "Checkout succeeded."
+        break
+    fi
+    CONFLICTING_FILES=$(echo "$OUTPUT" | grep -E "^\s+.+" | awk '{print $1}')
 
-echo "Cloning bare Git repository from $DOTFILES_URL"
-git clone --branch master--bare-repo --bare $DOTFILES_URL $DOTFILES_DIR
+    # When the number of conflicting files exceeds a certain amount the git
+    # checkout message does not display them all.  That is why I need to use a
+    # while loop to get them all.  Also sometimes the file name is cut
+    # off--which is why I check to see if the file exists.
+    for file in $CONFLICTING_FILES; do
+        FILE_PATH="$HOME/$file"
+        if [[ -f $FILE_PATH ]]; then
+            if mv "$FILE_PATH" "$BACKUP_DIR"; then
+                echo "Moved $FILE_PATH -> $BACKUP_DIR"
+            else
+                echo "Error: Unable to move $FILE_PATH to $BACKUP_DIR"
+                exit 1
+            fi
+        else
+            echo "Error: $FILE_PATH does not exist or is not a regular file."
+        fi
+    done
 
-if dot checkout; then
-  echo "Successfully Checked out dotfiles."
-else
-  echo "Conflicts detected! Backing up pre-existing dotfiles to $BACKUP_DIRECTORY..."
-  mkdir -p "$BACKUP_DIR"
-
-  for file in $(dot checkout 2>&1 | grep -E "^\s+.+" | awk '{print $1}'); do
-    echo "moving $HOME/$file to $BACKUP_DIR..."
-    mv "$HOME/$file" "$BACKUP_DIR/" || echo "Warning: Could not move $file"
-  done
-
-  dot checkout
-fi;
-
-dot config --local status.showUntrackedFiles no
-
-echo "Dotfiles initialized successfully."
+    OUTPUT=$(git checkout develop 2>&1)
+done
