973669587fe23c18bb5afd171f7dde8398aaa104
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 16:06:48 -0500

    ~/.config/awesome/rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -289,6 +289,7 @@ function fn4 () awful.tag.incnmaster(-1, nil, true) end
 function fn5 () awful.tag.incncol( 1, nil, true) end
 
 function fn6 () awful.tag.incncol(-1, nil, true) end
+function () awful.layout.inc( 1) end
 
 globalkeys = gears.table.join(
     -- Standard program
