5f8077f19f8a0b1e2c153010f7f3e979326da654
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 12:24:44 -0500

    ~/.config/qtile/config.py

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -98,7 +98,7 @@ for i in groups:
 layouts = [
     layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
     layout.Max(),
-    layout.floating.Floating()
+    layout.floating.Floating(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
     # layout.Bsp(),
