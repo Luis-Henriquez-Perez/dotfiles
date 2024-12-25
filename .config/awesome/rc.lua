e9f7b3ec5c46c32d0d2cc3712557016c6194a63b
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 17:18:37 -0500

    ~/.config/awesome/rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -303,12 +303,12 @@ globalkey({ modkey, "Control" }, "h", fn5, {description = "increase the number o
 globalkey({ modkey, "Control" }, "l", fn6, {description = "decrease the number of columns", group = "layout"})
 globalkey({ modkey }, "space", fn7, {description = "select next", group = "layout"})
 globalkey({ modkey, "Shift" }, "space", function () awful.layout.inc(-1) end, {description = "select previous", group = "layout"}),
-globalkey({ modkey, "Control" }, "n", function () local c = awful.client.restore() -- Focus restored client if c then c:emit_signal( "request::activate", "key.unminimize", {raise = true} ) end end, {description = "restore minimized", group = "client"})
-globalkey({ modkey }, "r", function () awful.screen.focused().mypromptbox:run() end, {description = "run prompt", group = "launcher"})
+(globalkey({ modkey }, "r", function () awful.screen.focused().mypromptbox:run() end, {description = "run prompt", group = "launcher"})
 globalkey({ modkey }, "x", function () awful.prompt.run { prompt = "Run Lua code: ", textbox = awful.screen.focused().mypromptbox.widget, exe_callback = awful.util.eval, history_path = awful.util.get_cache_dir() .. "/history_eval" } end, {description = "lua execute prompt", group = "awesome"})
 globalkey({ modkey }, "p", function() menubar.show() end, {description = "show the menubar", group = "launcher"})
 
 clientkeys = gears.table.join(
+globalkey{ modkey, "Control" }, "n", function () local c = awful.client.restore() -- Focus restored client if c then c:emit_signal( "request::activate", "key.unminimize", {raise = true} ) end end, {description = "restore minimized", group = "client"})
     awful.key({ modkey }, "f",
         function (c)
             c.fullscreen = not c.fullscreen
