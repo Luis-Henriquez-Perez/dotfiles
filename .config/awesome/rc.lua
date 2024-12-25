625907c0d7b4b609cd437a7d123a9782cad462f4
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 17:21:13 -0500

    ~/.config/awesome/rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -304,7 +304,7 @@ function f8 () menubar.show() end
     globalkey({ modkey, "Control" }, "h", fn5, {description = "increase the number of columns", group = "layout"})
     globalkey({ modkey, "Control" }, "l", fn6, {description = "decrease the number of columns", group = "layout"})
     globalkey({ modkey }, "space", fn7, {description = "select next", group = "layout"})
-    globalkey({ modkey }, "r", function () awful.screen.focused().mypromptbox:run() end, {description = "run prompt", group = "launcher"})
+    globalkey({ modkey }, "r", {, group = "launcher"})
     globalkey({ modkey }, "x", function () awful.prompt.run { prompt = "Run Lua code: ", textbox = awful.screen.focused().mypromptbox.widget, exe_callback = awful.util.eval, history_path = awful.util.get_cache_dir() .. "/history_eval" } end, {description = "lua execute prompt", group = "awesome"})
     globalkey({ modkey }, "p", f8, {description = "show the menubar", group = "launcher"})
 
