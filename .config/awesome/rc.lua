38c2fb2041f5cf8db0c2174991436949a2f28165
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 18:26:59 -0500

    ~/.config/awesome/rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -234,7 +234,7 @@ globalkeys = {}
 
 function oo_set_previous_layout () awful.layout.inc(-1) end
 function oo_decrease_number_of_masters () awful.tag.incnmaster(-1, nil, true) end
-function oo_swap () awful.client.swap.byidx( -1) end
+function oo_swap_with_previous_client () awful.client.swap.byidx( -1) end
 function fn19 () awful.client.swap.byidx( 1) end
 function fn18 () awful.tag.incnmaster( 1, nil, true) end
 function fn17 () local c = awful.client.restore() if c then c:emit_signal( "request::activate", "key.unminimize", {raise = true} ) end
@@ -278,7 +278,7 @@ globalkey({ modkey, "Control" }, "n", fn17, description = "restore minimized", g
 globalkey({ modkey, "Control" }, "r", awesome.restart, description = "reload awesome", group = "awesome")
 globalkey({ modkey, "Shift  " }, "h", fn18, description = "increase the number of master clients", group = "layout")
 globalkey({ modkey, "Shift  " }, "j", fn19, description = "swap with next client by index", group = "client")
-globalkey({ modkey, "Shift  " }, "k", oo_swap, description = "swap with previous client by index", group = "client")
+globalkey({ modkey, "Shift  " }, "k", oo_swap_with_previous_client, description = "swap with previous client by index", group = "client")
 globalkey({ modkey, "Shift  " }, "l", oo_decrease_number_of_masters, description = "decrease the number of master clients", group = "layout")
 globalkey({ modkey, "Shift  " }, "q", awesome.quit, description = "quit awesome", group = "awesome")
 globalkey({ modkey, "Shift  " }, "space", oo_set_previous_layout, description = "select previous", group = "layout")
