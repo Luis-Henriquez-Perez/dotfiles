e12a8007f9b1eaf590b05d6a5b244ec3383e766b
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 12:35:23 -0500

    ~/.config/qtile/config.py

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -17,7 +17,7 @@ def define_key (modifier, key, fn, desc):
 # Switch between windows
 # define_key([mod], "space", lazy.layout.next(), desc="Move window focus to other window")
 # define_key([mod], "h", lazy.layout.left(), desc="Move focus to left")
-define_key([mod], "l", lazy.layout.right(), desc="Move focus to right")
+de# fine_key([mod], "l", lazy.layout.right(), desc="Move focus to right")
 define_key([mod], "j", lazy.layout.next(), desc="Move focus to next window")
 define_key([mod], "k", lazy.layout.previous(), desc="Move focus to previous window")
 # Move windows between left/right columns or move up/down in current stack.
