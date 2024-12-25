1c30ec0fef8d3ac6c2ff7202f6e8c809f9c88d25
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-24 20:47:30 -0500

    ~/.config/emacs/lisp/oo-commands.el

--- .config/emacs/lisp/oo-commands.el
+++ .config/emacs/lisp/oo-commands.el
@@ -248,10 +248,10 @@ repository and if it is, commit and push all changes.  Otherwise, do nothing."
 If FILE is not in registered in dotfile repo, register it.  In any case commit
 the file.  Additionally, push the file but only if the battery is charging or
 the battery percentage is greater than 60%."
-  (interactive)
+  ;; (interactive (buffer-file-name))
   (set! default-directory (expand-file-name "~/"))
   (trace! "Adding dotfile %s" file)
-  (set! fname (expand-file-name (convert-standard-filename file) oo-dotfile-dir))
+  (set! fname (expand-file-name (convert-standard-filename file) "~/"))
   (set! tracked-p (not (string-empty-p (shell-command-to-string (format "git ls-files %s" (shell-quote-argument fname))))))
   (set! modified-p (not (string-empty-p (shell-command-to-string (format "git diff %s" (shell-quote-argument fname))))))
   ;; Do not do anything if the file is already tracked and has no changes.
