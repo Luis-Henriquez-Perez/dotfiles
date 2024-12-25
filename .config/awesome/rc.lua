c63f8ba3ca5633d8edd83a8b3385cafc793fdc91
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 16:06:45 -0500

    ~/.config/awesome/rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -301,7 +301,7 @@ globalkeys = gears.table.join(
     globalkey({ modkey, "Shift" }, "l", fn4, description = "decrease the number of master clients", group = "layout")
     globalkey({ modkey, "Control" }, "h", fn5, {description = "increase the number of columns", group = "layout"}),
     globalkey({ modkey, "Control" }, "l", fn6, {description = "decrease the number of columns", group = "layout"}),
-    globalkey({ modkey }, "space", function () awful.layout.inc( 1) end, {description = "select next", group = "layout"}),
+    globalkey({ modkey }, "space", fn7, {description = "select next", group = "layout"}),
     globalkey({ modkey, "Shift" }, "space", function () awful.layout.inc(-1) end, {description = "select previous", group = "layout"}),
     globalkey({ modkey, "Control" }, "n", function () local c = awful.client.restore() -- Focus restored client if c then c:emit_signal( "request::activate", "key.unminimize", {raise = true} ) end end, {description = "restore minimized", group = "client"}),
     awful.key({ modkey }, "r", function () awful.screen.focused().mypromptbox:run() end, {description = "run prompt", group = "launcher"}),
