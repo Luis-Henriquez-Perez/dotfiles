011d847f1f621030dbe057ca21c4752163413bd3
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 07:31:04 -0500

    ~/.config/awesome/rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -230,7 +230,8 @@ root.buttons(gears.table.join(
 -- {{{ Key bindings
 globalkeys = gears.table.join(
     awful.key({ modkey,},
-        "s",      hotkeys_popup.show_help,
+        "s",
+        hotkeys_popup.show_help,
         {description="show help", group="awesome"}),
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev,
               {description = "view previous", group = "tag"}),
