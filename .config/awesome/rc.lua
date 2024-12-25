2ddcc935ff89425a3d5edf7ac1295b99f11ef78f
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 17:22:54 -0500

    ~/.config/awesome/rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -295,6 +295,7 @@ function fn7 () awful.layout.inc( 1) end
 function f8 () menubar.show() end
 
 function f9 () awful.screen.focused().mypromptbox:run() end
+
 function () awful.prompt.run { prompt = "Run Lua code: ", textbox = awful.screen.focused().mypromptbox.widget, exe_callback = awful.util.eval, history_path = awful.util.get_cache_dir() .. "/history_eval" } end
 
     globalkey({ modkey }, "Return", oo_open_terminal, description = "open a terminal", group = "launcher")
