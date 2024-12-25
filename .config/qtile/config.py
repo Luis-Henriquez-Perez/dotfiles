ab0259ce7f24f106419db5c5b8775961246de50a
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 12:46:53 -0500

    ~/.config/qtile/config.py

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -18,7 +18,6 @@ def define_key (modifier, key, fn, desc):
 # define_key([mod], "space", lazy.layout.next(), desc="Move window focus to other window")
 define_key([mod], "h", lazy.layout.left(), desc="Move focus to left")
 define_key([mod], "l", lazy.layout.right(), desc="Move focus to right")
-# I want these commands to do what I mean instead of being so restrictive.
 define_key([mod], "j", lazy.layout.next(), desc="Move focus to next window")
 define_key([mod], "k", lazy.layout.previous(), desc="Move focus to previous window")
 # Move windows between left/right columns or move up/down in current stack.
