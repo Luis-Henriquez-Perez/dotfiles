e879aeb1b7523ee9662b5f772848399c006ed2a5
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 08:01:35 -0500

    ~/.config/awesome/rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -229,15 +229,15 @@ root.buttons(gears.table.join(
 
 -- {{{ Key bindings
 globalkeys = gears.table.join(
-    awful.key {
-        modifiers = { modkey },
-        key = 's',
-        on_press = hotkeys_popup.show_help,
-        description="show help",
-        group="awesome"
-    },
-    -- awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
-    --     {description="show help", group="awesome"}),
+    -- awful.key {
+    --     modifiers = { modkey },
+    --     key = 's',
+    --     on_press = hotkeys_popup.show_help,
+    --     description="show help",
+    --     group="awesome"
+    -- },
+    awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
+        {description="show help", group="awesome"}),
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev,
         {description = "view previous", group = "tag"}),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext,
