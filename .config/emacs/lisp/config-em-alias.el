e040db07d3b0d193a1a3366b37518d364665d25e
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 14:11:56 -0500

    ~/.config/emacs/lisp/config-em-alias.el

--- .config/emacs/lisp/config-em-alias.el
+++ .config/emacs/lisp/config-em-alias.el
@@ -49,7 +49,7 @@
 (eshell/alias "remotes" "git --no-pager branch -r")
 (eshell/alias "checkout" "git checkout $*")
 (eshell/alias "check" "git checkout $*")
-(eshell/alias "rename" "git ")
+(eshell/alias "rename" "git branch -m <old-branch-name> <new-branch-name>")
 ;;;; rsync
 ;; Communicate with my desktop.
 ;; (eshell/alias "rpush" "rsync -a $1 luis@yadira:$2")
