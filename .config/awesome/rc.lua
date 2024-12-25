cf4cf7e1793cf105bba677202f80e2e18b74270a
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 07:50:00 -0500

    ~/.config/awesome/rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -253,18 +253,19 @@ globalkeys = gears.table.join(
     --     description="show help",
     --     group="awesome"
     -- },
-    awful.key {
-        modifiers = {modkey},
-        key = 's',
-        -- on_press = hotkeys_popup.show_help,
-        on_press = function ()
-            print("The Mod4 + Shift + a combo is pressed")
-        end,
-        on_release = function ()
-            print("The Mod4 + Shift + a combo is released")
-        end
-    },
-
+    -- awful.key {
+    --     modifiers = {modkey},
+    --     key = 's',
+    --     -- on_press = hotkeys_popup.show_help,
+    --     on_press = function ()
+    --         print("The Mod4 + Shift + a combo is pressed")
+    --     end,
+    --     on_release = function ()
+    --         print("The Mod4 + Shift + a combo is released")
+    --     end
+    -- },
+    awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
+        {description="show help", group="awesome"}),
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev,
         {description = "view previous", group = "tag"}),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext,
