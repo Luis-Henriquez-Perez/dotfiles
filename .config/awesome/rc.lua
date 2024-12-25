32fb058998ccf75b82211b312527cc65dbeae58a
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 07:33:50 -0500

    ~/.config/awesome/rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -239,10 +239,10 @@ globalkeys = gears.table.join(
     --         print("The Mod4 + Shift + a combo is released")
     --     end
     -- },
-    awful.key({ modkey,},
+    awful.key{{ modkey,},
         "s",
         hotkeys_popup.show_help,
-        {description="show help", group="awesome"}),
+        {description="show help", group="awesome"}},
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev,
         {description = "view previous", group = "tag"}),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext,
