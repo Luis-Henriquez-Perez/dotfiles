5e2fd58c2a9bdabbc356b8dc693c71d23635dc54
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 18:21:18 -0500

    Set gap between windows to 15 pixels.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -59,6 +59,8 @@ editor_cmd = terminal .. " -e " .. editor
 -- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
 
+beautiful.useless_gap = 15
+
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     awful.layout.suit.floating,
