7a7e8b1db6efa946e8cddfca7de72f1381e23646
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 21:43:07 -0500

    ~/.config/awesome/rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -245,8 +245,7 @@ end
 local function oo_decrease_number_of_masters ()
    awful.tag.incnmaster(-1, nil, true)
 end
-local function oo_swap_with_previous_client ()
-   awful.client.swap.byidx( -1) end
+local function oo_swap_with_previous_client () awful.client.swap.byidx( -1) end
 local function oo_swap_with_next_client () awful.client.swap.byidx( 1) end
 local function oo_increase_number_of_masters () awful.tag.incnmaster( 1, nil, true) end
 local function oo_restore_minimized () local c = awful.client.restore() if c then c:emit_signal( "request::activate", "key.unminimize", {raise = true} ) end
