48520d2a574e9f93986f2648915d3f7fabaa1acc
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 21:43:00 -0500

    ~/.config/awesome/rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -249,8 +249,7 @@ local function oo_swap_with_previous_client ()
    awful.client.swap.byidx( -1)
 end
 local function oo_swap_with_next_client ()
-   awful.client.swap.byidx( 1)
-end
+   awful.client.swap.byidx( 1) end
 local function oo_increase_number_of_masters () awful.tag.incnmaster( 1, nil, true) end
 local function oo_restore_minimized () local c = awful.client.restore() if c then c:emit_signal( "request::activate", "key.unminimize", {raise = true} ) end
 local function oo_decrease_number_of_columns () awful.tag.incncol(-1, nil, true) end
