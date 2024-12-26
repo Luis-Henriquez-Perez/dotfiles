e7786f4bc0541625382e4f288a0be97158931b07
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 21:22:03 -0500

    ~/.config/awesome/rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -239,7 +239,7 @@ function oo_swap_with_next_client () awful.client.swap.byidx( 1) end
 function oo_increase_number_of_masters () awful.tag.incnmaster( 1, nil, true) end
 function fn17 () local c = awful.client.restore() if c then c:emit_signal( "request::activate", "key.unminimize", {raise = true} ) end
 function oo_decrease_number_of_columns () awful.tag.incncol(-1, nil, true) end
-function fn15 () awful.screen.focus_relative(-1) end
+function oo_focus_previous_screen () awful.screen.focus_relative(-1) end
 function fn14 () awful.screen.focus_relative( 1) end
 function oo_increase_number_of_columns () awful.tag.incncol( 1, nil, true) end
 function fn12 () menubar.show() end
@@ -272,7 +272,7 @@ globalkey({ modkey }, "x", oo_prompt_lua_code, description = "lua execute prompt
 globalkey({ modkey }, "p", fn12, description = "show the menubar", group = "launcher")
 globalkey({ modkey, "Control" }, "h", oo_increase_number_of_columns, description = "increase the number of columns", group = "layout")
 globalkey({ modkey, "Control" }, "j", fn14, description = "focus the next screen", group = "screen")
-globalkey({ modkey, "Control" }, "k", fn15, description = "focus the previous screen", group = "screen")
+globalkey({ modkey, "Control" }, "k", oo_focus_previous_screen, description = "focus the previous screen", group = "screen")
 globalkey({ modkey, "Control" }, "l", oo_decrease_number_of_columns, description = "decrease the number of columns", group = "layout")
 globalkey({ modkey, "Control" }, "n", fn17, description = "restore minimized", group = "client")
 globalkey({ modkey, "Control" }, "r", awesome.restart, description = "reload awesome", group = "awesome")
