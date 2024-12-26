8b6fd7060558e5e99e3eb3a10af8057e2d3013b9
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 23:20:04 -0500

    Add spacing.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -230,6 +230,7 @@ root.buttons(gears.table.join(
 -- }}}
 
 -- {{{ Key bindings
+
 globalkeys = gears.table.join(
     awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
               {description="show help", group="awesome"}),
