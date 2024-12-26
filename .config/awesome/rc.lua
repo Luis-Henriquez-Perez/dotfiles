4007297f47da77561a9a381b2352bb99ff597436
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-25 21:47:25 -0500

    ~/.config/awesome/rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -239,27 +239,27 @@ local function globalkey(modifiers, key, action, description, group)
     globalkeys = gears.table.join(globalkeys, newkey)
 end
 
-local function oo_set_previous_layout () awful.layout.inc(-1) end
-local function oo_decrease_number_of_masters () awful.tag.incnmaster(-1, nil, true) end
-local function oo_swap_with_previous_client () awful.client.swap.byidx( -1) end
-local function oo_swap_with_next_client () awful.client.swap.byidx( 1) end
-local function oo_increase_number_of_masters () awful.tag.incnmaster( 1, nil, true) end
--- local function oo_restore_minimized () local c = awful.client.restore() if c then c:emit_signal( "request::activate", "key.unminimize", {raise = true} ) end
-local function oo_decrease_number_of_columns () awful.tag.incncol(-1, nil, true) end
-local function oo_focus_previous_screen () awful.screen.focus_relative(-1) end
-local function oo_focus_next_screen () awful.screen.focus_relative( 1) end
-local function oo_increase_number_of_columns () awful.tag.incncol( 1, nil, true) end
-local function oo_show_menubar () menubar.show() end
-local function oo_prompt_lua_code () awful.prompt.run { prompt = "Run Lua code: ", textbox = awful.screen.focused().mypromptbox.widget, exe_callback = awful.util.eval, history_path = awful.util.get_cache_dir() .. "/history_eval" } end
-local function oo_run_prompt () awful.screen.focused().mypromptbox:run() end
-local function oo_select_next_layout () awful.layout.inc( 1) end
-local function oo_decrease_master_width_factor () awful.tag.incmwfact(-0.05) end
-local function oo_increase_master_width_factor () awful.tag.incmwfact( 0.05) end
-local function oo_launch_terminal () awful.spawn(terminal) end
-local function oo_focus_last_client () awful.client.focus.history.previous() if client.focus then client.focus:raise() end
-local function oo_show_mainmenu () mymainmenu:show() end
-local function oo_focus_previous_client () awful.client.focus.byidx(-1) end
-local function oo_focus_next_client () awful.client.focus.byidx( 1) end
+-- local function oo_set_previous_layout () awful.layout.inc(-1) end
+-- local function oo_decrease_number_of_masters () awful.tag.incnmaster(-1, nil, true) end
+-- local function oo_swap_with_previous_client () awful.client.swap.byidx( -1) end
+-- local function oo_swap_with_next_client () awful.client.swap.byidx( 1) end
+-- local function oo_increase_number_of_masters () awful.tag.incnmaster( 1, nil, true) end
+-- -- local function oo_restore_minimized () local c = awful.client.restore() if c then c:emit_signal( "request::activate", "key.unminimize", {raise = true} ) end
+-- local function oo_decrease_number_of_columns () awful.tag.incncol(-1, nil, true) end
+-- local function oo_focus_previous_screen () awful.screen.focus_relative(-1) end
+-- local function oo_focus_next_screen () awful.screen.focus_relative( 1) end
+-- local function oo_increase_number_of_columns () awful.tag.incncol( 1, nil, true) end
+-- local function oo_show_menubar () menubar.show() end
+-- local function oo_prompt_lua_code () awful.prompt.run { prompt = "Run Lua code: ", textbox = awful.screen.focused().mypromptbox.widget, exe_callback = awful.util.eval, history_path = awful.util.get_cache_dir() .. "/history_eval" } end
+-- local function oo_run_prompt () awful.screen.focused().mypromptbox:run() end
+-- local function oo_select_next_layout () awful.layout.inc( 1) end
+-- local function oo_decrease_master_width_factor () awful.tag.incmwfact(-0.05) end
+-- local function oo_increase_master_width_factor () awful.tag.incmwfact( 0.05) end
+-- local function oo_launch_terminal () awful.spawn(terminal) end
+-- local function oo_focus_last_client () awful.client.focus.history.previous() if client.focus then client.focus:raise() end
+-- local function oo_show_mainmenu () mymainmenu:show() end
+-- local function oo_focus_previous_client () awful.client.focus.byidx(-1) end
+-- local function oo_focus_next_client () awful.client.focus.byidx( 1) end
 
 -- globalkey({ modkey }, "s", hotkeys_popup.show_help, description="show help", group="awesome")
 -- globalkey({ modkey }, "Left", awful.tag.viewprev, description = "view previous", group = "tag")
