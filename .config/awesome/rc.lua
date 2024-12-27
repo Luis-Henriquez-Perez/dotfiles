8836bb1dc4eb5b0c724c91a646f777f2fea70af2
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-27 16:18:25 -0500

    Open emacsclient as the editor when offering to edit config.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -51,8 +51,7 @@ revelation.init()
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"
-editor = os.getenv("EDITOR") or "nano"
-editor_cmd = terminal .. " -e " .. editor
+editor_cmd = "emacsclient -r -a \"\""
 
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
