2ea0418243feda6aae98ac20a0d34d0da1b2de49
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-27 17:24:44 -0500

    Fix indentation of screenshot function.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -339,8 +339,8 @@ local function client_rotate_backward()
 end
 
 local function system_take_screenshot()
-   awful.spawn.with_shell("maim ~/Pictures/screenshot_$(date +%Y%m%d_%H%M%S).png")
-   naughty.notify({title = "maim", text = "take screenshot"})
+    awful.spawn.with_shell("maim ~/Pictures/screenshot_$(date +%Y%m%d_%H%M%S).png")
+    naughty.notify({title = "maim", text = "take screenshot"})
 end
 
 local function system_launch_emacs ()
