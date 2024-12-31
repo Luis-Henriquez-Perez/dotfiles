73a304306f0836a53ad126950843644e984ef9be
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-31 04:15:07 -0500

    ~/.config/emacs/lisp/init-spaceline.el

--- .config/emacs/lisp/init-spaceline.el
+++ .config/emacs/lisp/init-spaceline.el
@@ -32,7 +32,7 @@
 ;;;; settings
 (opt! spaceline-highlight-face-func #'spaceline-highlight-face-evil-state)
 (opt! powerline-height 33)
-(opt! powerline-default-separator 'curve)
+(opt! powerline-default-separator 'arrow)
 (setq spaceline-separator-dir-left '(left . left))
 (setq spaceline-separator-dir-right '(right . right))
 ;; Although this saves time the longer you use the modeline, it means that the
