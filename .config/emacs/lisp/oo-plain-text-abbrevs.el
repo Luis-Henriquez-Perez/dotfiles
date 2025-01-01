6dea51f5c18b2a2439cb9287499d70497e378712
Luis Henriquez-Perez <luis@luishp.xyz>
2024-12-31 20:57:24 -0500

    ~/.config/emacs/lisp/oo-plain-text-abbrevs.el

--- .config/emacs/lisp/oo-plain-text-abbrevs.el
+++ .config/emacs/lisp/oo-plain-text-abbrevs.el
@@ -54,7 +54,7 @@ string or comment."
 ;; modliens -> modelines
 ;; narrowd -> narrowed
 ;; narowd -> narowed
-(define-abbrev global-abbrev-table "narowd" "customizability"               nil :enable-function #'oo--use-plain-text-abbrev-p)
+(define-abbrev global-abbrev-table "narowd" "narrowed"               nil :enable-function #'oo--use-plain-text-abbrev-p)
 (define-abbrev global-abbrev-table "customizeability" "customizability"               nil :enable-function #'oo--use-plain-text-abbrev-p)
 (define-abbrev global-abbrev-table "reproducability"  "reproducibility"               nil :enable-function #'oo--use-plain-text-abbrev-p)
 (define-abbrev global-abbrev-table "reproduceability" "reproducibility"               nil :enable-function #'oo--use-plain-text-abbrev-p)
