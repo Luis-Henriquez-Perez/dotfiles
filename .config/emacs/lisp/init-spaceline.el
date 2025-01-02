3429d54536b38969eb0251bb4444bd161997a6eb
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-02 08:08:01 -0500

    ~/.config/emacs/lisp/init-spaceline.el

--- .config/emacs/lisp/init-spaceline.el
+++ .config/emacs/lisp/init-spaceline.el
@@ -81,7 +81,7 @@
 (spaceline-compile
   'main
   '((+evil-state :face (spaceline-highlight-face-evil-state))
-    (+buffer-name :face 'powerline-active0)
+    (+buffer-modified +buffer-name :face 'powerline-active0)
     (+version-control :face 'powerline-active1))
   '((+pomodoro :face 'powerline-active1)
     +emms
