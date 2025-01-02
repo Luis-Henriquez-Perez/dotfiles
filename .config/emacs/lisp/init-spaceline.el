ed6ff86a68b87e38f4d3f585be969c7e7ae8de3a
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-02 08:02:48 -0500

    ~/.config/emacs/lisp/init-spaceline.el

--- .config/emacs/lisp/init-spaceline.el
+++ .config/emacs/lisp/init-spaceline.el
@@ -80,7 +80,7 @@
 ;;;; define main modeline
 (spaceline-compile
   'main
-  '((+evil-state :face (spaceline-highlight-face-evil-state))
+  '((+evil-state)
     (+narrow +kbd-macro +buffer-read-only +buffer-modified +buffer-name :face 'powerline-active0)
     (+version-control :face 'powerline-active1))
   '((+pomodoro :face 'powerline-active1)
