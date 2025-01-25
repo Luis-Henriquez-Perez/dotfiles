ab8651b7d5bbc8e8acd045c686817ad157d8e930
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-25 11:14:39 -0500

    .config/qutebrowser/config.py

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -16,8 +16,24 @@ c = c  # type: ConfigContainer
 c.url.searchengines = {'DEFAULT' : '' 'https://www.qwant.com/?l=en={}',
                        # '!a'      : 'https://www.amazon.com/s?k={}',
                        'dg'      : 'https://duckduckgo.com/?ia=web&q={}',
+                       # '!dd'     : 'https://thefreedictionary.com/{}',
+                       # '!e'      : 'https://www.ebay.com/sch/i.html?_nkw={}',
+                       # '!fb'     : 'https://www.facebook.com/s.php?q={}',
+                       # '!gh'     : 'https://github.com/search?o=desc&q={}&s=stars',
+                       # '!gist'   : 'https://gist.github.com/search?q={}',
+                       # '!gi'     : 'https://www.google.com/search?tbm=isch&q={}&tbs=imgo:1',
+                       # '!gn'     : 'https://news.google.com/search?q={}',
+                       # '!ig'     : 'https://www.instagram.com/explore/tags/{}',
+                       # '!m'      : 'https://www.google.com/maps/search/{}',
+                       # '!p'      : 'https://pry.should have/{}',
+                       # '!r'      : 'https://www.reddit.com/search?q={}',
+                       # '!sd'     : 'https://slickdeals.net/newsearch.php?q={}&searcharea=deals&searchin=first',
+                       # '!t'      : 'https://www.thesaurus.com/browse/{}',
+                       # '!tw'     : 'https://twitter.com/search?q={}',
+                       # '!w'      : 'https://en.wikipedia.org/wiki/{}',
+                       # '!yelp'   : 'https://www.yelp.com/search?find_description={}',
+                       # '!yt'     : 'https://www.youtube.com/results?search_query={}'
                        }
-
 
 config.load_autoconfig(False)
 config.set('content.cookies.accept', 'no-3rdparty', 'chrome-devtools://*')
