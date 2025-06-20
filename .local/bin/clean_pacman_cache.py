ea4ca7af918a7d4ade9cf0f91a25c890b5007eec
Luis Henriquez-Perez <luis@luishp.xyz>
2025-06-20 08:38:25 -0400

    Remove several outdated scripts.

--- .local/bin/clean_pacman_cache.py
+++
@@ -1,6 +0,0 @@
-# Filename: clean_pacman_cache.py
-# Author: Luis Henriquez <luis@luishp.xyz>
-# Created: 2024-12-18 09:47:48
-# Description: Clean pacman cache.
-
-pacman -Scc
--- .local/bin/trash_wallpaper.py
+++
@@ -1,4 +0,0 @@
-# Luis Henriquez
-# Deduce the current wallpaper from the wallpaper file.
-
-import os
