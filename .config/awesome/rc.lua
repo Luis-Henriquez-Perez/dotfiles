9965c38e6bc02c8bdbdca8e426f785d76e91c1f8
Luis Henriquez-Perez <luis@luishp.xyz>
2025-06-24 23:38:23 -0400

    Add bind for reboot.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -437,7 +437,7 @@ globalkey({ modkey }, "x", awesome_run_lua_code, "lua execute prompt", "awesome"
 globalkey({ modkey }, "g", awesome_toggle_wibox, "toggle wibox", "awesome")
 
 -- system
-globalkey({ modkey }, "u", system_reboot, "Reboot", "reboot")
+globalkey({ modkey , "Shift"}, "r", system_reboot, "Reboot", "reboot")
 globalkey({ modkey }, "u", system_launch_qutebrowser, "Launch qutebrowser", "system")
 globalkey({ modkey }, "Return", system_launch_terminal, "Launch terminal", "system")
 globalkey({ modkey }, "i", system_launch_firefox, "Launch firefox", "system")
