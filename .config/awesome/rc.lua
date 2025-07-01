7ac2bfba54189ac0c7d439f959c6ed20ba361270
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-01 18:22:08 -0400

    I prefer tile-bottom.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -69,8 +69,8 @@ beautiful.useless_gap = 10
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.tile,
     awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile,
     awful.layout.suit.corner.nw,
     awful.layout.suit.magnifier,
     -- awful.layout.suit.tile.left,
