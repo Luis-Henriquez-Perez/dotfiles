c23a79e7be7c774503b89fc1e8e4f290040ccd86
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-03 12:55:39 -0400

    Delete unnecessary files.

--- Archive/draft_snapshot-webpage-with-monolith.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-URL=$QUTE_URL
-
-FILENAME="$HOME/Downloads/$(date +%Y%m%d%H%M%S)-page.html"
-
-monolith $URL > $FILENAME
-
-echo "message-info 'Saved page to $FILENAME'" >> $QUTE_FIFO
